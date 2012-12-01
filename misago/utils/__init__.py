793de430faee2764d3669b479f9463b5f6176ffc
Ralfp <kontakt@rpiton.com>
2012-12-02 00:36:45 +0100

    Build list of formats.

--- misago/utils/__init__.py
+++ misago/utils/__init__.py
@@ -33,31 +33,18 @@ def get_msgid(gettext):
 
 """
 Date formats
-"""    
-from django.conf import settings
-from django.utils.importlib import import_module
-from misago import get_version
+"""               
+from django.utils.formats import get_format
 
-try:
-    locale_formats = import_module('django.conf.locale.%s.formats' % settings.LANGUAGE_CODE)
-    formats = {
-               'DATE_FORMAT': locale_formats.DATE_FORMAT,
-               'TIME_FORMAT': locale_formats.TIME_FORMAT,
-               'DATETIME_FORMAT': locale_formats.DATETIME_FORMAT,
-               'SHORT_DATE_FORMAT': locale_formats.SHORT_DATE_FORMAT,
-               'SHORT_DATETIME_FORMAT': locale_formats.SHORT_DATETIME_FORMAT,
-               }
-except (ImportError, AttributeError):
-    formats = {
-               'DATE_FORMAT': settings.DATE_FORMAT,
-               'TIME_FORMAT': settings.TIME_FORMAT,
-               'DATETIME_FORMAT': settings.DATETIME_FORMAT,
-               'SHORT_DATE_FORMAT': settings.SHORT_DATE_FORMAT,
-               'SHORT_DATETIME_FORMAT': settings.SHORT_DATETIME_FORMAT,
-               }
+formats = {
+    'DATE_FORMAT': '',
+    'DATETIME_FORMAT': '',
+    'TIME_FORMAT': '',
+    'YEAR_MONTH_FORMAT': '',
+    'MONTH_DAY_FORMAT': '',
+    'SHORT_DATE_FORMAT': '',
+    'SHORT_DATETIME_FORMAT': '',
+}
 
-formats['DATE_FORMAT'] = unicode(formats['DATE_FORMAT'].replace('P', 'g:i a'))
-formats['TIME_FORMAT'] = unicode(formats['TIME_FORMAT'].replace('P', 'g:i a'))
-formats['DATETIME_FORMAT'] = unicode(formats['DATETIME_FORMAT'].replace('P', 'g:i a'))
-formats['SHORT_DATE_FORMAT'] = unicode(formats['SHORT_DATE_FORMAT'].replace('P', 'g:i a'))
-formats['SHORT_DATETIME_FORMAT'] = unicode(formats['SHORT_DATETIME_FORMAT'].replace('P', 'g:i a'))
+for key in formats:
+    formats[key] = get_format(key).replace('P', 'g:i a')
