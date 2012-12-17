58d52a8fc419b77eef8bb0e6dc08fe2a49e15cb3
Ralfp <kontakt@rpiton.com>
2012-12-17 23:48:20 +0100

    Fixed sessions cron commands

--- misago/sessions/management/commands/clearsessions.py
+++ misago/sessions/management/commands/clearsessions.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
