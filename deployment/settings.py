d58732f5d57ac819016a8c1105ceea57ae1cd1ab
Ralfp <kontakt@rpiton.com>
2012-12-29 03:48:13 +0100

    Updated blank config

--- deployment/settings.py
+++ deployment/settings.py
@@ -20,7 +20,7 @@ ADMINS = ()
 # Database connection
 DATABASES = {
     'default': {
-        'ENGINE': '',                    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -58,6 +58,7 @@ TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en_US'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
+# Always use forward slashes, even on Windows.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
 
@@ -68,6 +69,7 @@ MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
+# Always use forward slashes, even on Windows.
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ''
