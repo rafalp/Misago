fced45ef9742892c1a623cc77afe7e135edfb722
Ralfp <kontakt@rpiton.com>
2012-12-31 05:29:38 +0100

    Made loadfixtures work only on empty database

--- misago/setup/management/commands/loadfixtures.py
+++ misago/setup/management/commands/loadfixtures.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from misago.setup.fixtures import load_app_fixtures
+from misago.monitor.models import Item 
 from optparse import make_option
 
 class Command(BaseCommand):
@@ -10,9 +11,12 @@ class Command(BaseCommand):
     """
     help = 'Load Misago fixtures'
     def handle(self, *args, **options):
-        fixtures = 0
-        for app in settings.INSTALLED_APPS:
-            if load_app_fixtures(app):
-                fixtures += 1
-                print 'Loading fixtures from %s' % app
-        self.stdout.write('\nLoaded fixtures from %s applications.\n' % fixtures)
+        if Item.objects.count() > 0:
+            self.stdout.write("\nIt appears that fixters have been loaded already. Use updatefixtures if you want to update database data.\n")
+        else:
+            fixtures = 0
+            for app in settings.INSTALLED_APPS:
+                if load_app_fixtures(app):
+                    fixtures += 1
+                    print 'Loading fixtures from %s' % app
+            self.stdout.write('\nLoaded fixtures from %s applications.\n' % fixtures)
