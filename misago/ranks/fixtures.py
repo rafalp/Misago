b897dc62deb7523e4476e3f108388a19213a6a6a
Ralfp <kontakt@rpiton.com>
2013-01-07 19:33:18 +0100

    Updated fixtures to contain missing slugs.

--- misago/ranks/fixtures.py
+++ misago/ranks/fixtures.py
@@ -5,7 +5,7 @@ from misago.utils import get_msgid
 def load_fixtures():
     Rank.objects.create(
                         name=_("Forum Team").message,
-                        name_slug='forum_team',
+                        name_slug='forum-team',
                         title=_("Forum Team").message,
                         style='rank-team',
                         special=True,
@@ -16,6 +16,7 @@ def load_fixtures():
     
     Rank.objects.create(
                         name=_("Most Valuable Posters").message,
+                        name_slug='most-valuable-posters',
                         title=_("MVP").message,
                         style='rank-mvp',
                         special=True,
@@ -25,18 +26,21 @@ def load_fixtures():
     
     Rank.objects.create(
                         name=_("Lurkers").message,
+                        name_slug='lurkers',
                         order=1,
                         criteria="100%"
                         )
     
     Rank.objects.create(
                         name=_("Members").message,
+                        name_slug='members'
                         order=2,
                         criteria="75%"
                         )
     
     Rank.objects.create(
                         name=_("Active Members").message,
+                        name_slug='active-members'
                         style='rank-active',
                         order=3,
                         criteria="10%",
