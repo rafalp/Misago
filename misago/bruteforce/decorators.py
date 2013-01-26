b56a79aeb7df1b671e7789b3f4df6c202d383255
Ralfp <kontakt@rpiton.com>
2013-01-26 09:17:35 +0100

    Don't crash on crawler jam check.

--- misago/bruteforce/decorators.py
+++ misago/bruteforce/decorators.py
@@ -4,7 +4,10 @@ from misago.views import error403
 def block_jammed(f):
     def decorator(*args, **kwargs):
         request = args[0]
-        if not request.firewall.admin and request.jam.is_jammed():
-            return error403(request, _("You have used up allowed attempts quota and we temporarily banned you from accessing this page."))
+        try:
+            if not request.firewall.admin and request.jam.is_jammed():
+                return error403(request, _("You have used up allowed attempts quota and we temporarily banned you from accessing this page."))
+        except AttributeError:
+            pass
         return f(*args, **kwargs)
     return decorator
