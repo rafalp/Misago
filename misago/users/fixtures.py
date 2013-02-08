1ecfc05fefffe0c6c83238f557f03a6bcebaf074
Ralfp <kontakt@rpiton.com>
2013-02-08 02:42:45 +0100

    Fixture cleanup

--- misago/users/fixtures.py
+++ misago/users/fixtures.py
@@ -1,6 +1,4 @@
 from misago.monitor.fixtures import load_monitor_fixture
-from misago.utils import ugettext_lazy as _
-from misago.utils import get_msgid
 
 monitor_fixtures = {
                   'users': 0,
@@ -10,7 +8,6 @@ monitor_fixtures = {
                   'last_user_name': None,
                   'last_user_slug': None,
                   }
-
 
 def load_fixtures():
     load_monitor_fixture(monitor_fixtures)
