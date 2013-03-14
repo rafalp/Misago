1be443b5d96509271963dafc53083d8b845a57d9
Ralfp <kontakt@rpiton.com>
2013-03-14 19:30:15 +0100

    Update threads read data when threads are moved to other forums

--- misago/readstracker/models.py
+++ misago/readstracker/models.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from misago.forums.signals import move_forum_content
+from misago.threads.signals import move_thread
 
 class ThreadRecord(models.Model):
     user = models.ForeignKey('users.User')
@@ -21,3 +23,15 @@ class ForumRecord(models.Model):
         for thread in ThreadRecord.objects.filter(user=self.user, forum=self.forum, updated__gte=(timezone.now() - timedelta(days=settings.READS_TRACKER_LENGTH))):
             threads[thread.thread_id] = thread
         return threads
+
+
+def move_forum_content_handler(sender, **kwargs):
+    ForumRecord.objects.filter(forum=sender).delete()
+
+move_forum_content.connect(move_forum_content_handler, dispatch_uid="move_forum_threads_reads")
+
+
+def move_thread_handler(sender, **kwargs):
+    ThreadRecord.objects.filter(thread=sender).delete()
+
+move_thread.connect(move_thread_handler, dispatch_uid="move_thread_reads")
