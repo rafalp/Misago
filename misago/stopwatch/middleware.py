bc4aec8d8908965282275d01bc09c9c64bb32423
Ralfp <kontakt@rpiton.com>
2013-03-14 12:58:49 +0100

    Small change in stopwatch middleware

--- misago/stopwatch/middleware.py
+++ misago/stopwatch/middleware.py
@@ -7,10 +7,9 @@ class StopwatchMiddleware(object):
 
     def process_response(self, request, response):
         try:
-            time = request.stopwatch.time()
             if settings.STOPWATCH_LOG:
                 stat_file = open(settings.STOPWATCH_LOG, 'a')
-                stat_file.write("%s %s s\n" % (request.path_info, time))
+                stat_file.write("%s %s s\n" % (request.path_info, request.stopwatch.time()))
                 stat_file.close()
         except AttributeError:
             pass
