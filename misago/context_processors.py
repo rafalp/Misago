eb91909439cde45b2af33dc0669e860d80ff4ab1
Ralfp <kontakt@rpiton.com>
2013-03-16 23:24:59 +0100

    Fail in template instead of context processor

--- misago/context_processors.py
+++ misago/context_processors.py
@@ -1,9 +1,13 @@
 from misago import __version__
 
 def misago(request):
-    return {
-        'version': __version__,
-        'monitor': request.monitor,
-        'settings': request.settings,
-        'stopwatch': request.stopwatch.time(),
-    }
+    try:
+        return {
+            'version': __version__,
+            'monitor': request.monitor,
+            'settings': request.settings,
+            'stopwatch': request.stopwatch.time(),
+        }
+    except AttributeError:
+        # If request lacks required service, let template crash in context processor's stead
+        return  {}
