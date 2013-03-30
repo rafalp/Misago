24526bef563e014dc01bcd18c8ce83f27046b71c
Ralfp <kontakt@rpiton.com>
2013-03-30 23:57:46 +0100

    Don't prune announcements and stickies.

--- misago/management/commands/pruneforums.py
+++ misago/management/commands/pruneforums.py
@@ -12,11 +12,11 @@ class Command(BaseCommand):
         for forum in Forum.objects.all():
             deleted = 0
             if forum.prune_start:
-                for thread in forum.thread_set.filter(start__lte=timezone.now() - timedelta(days=forum.prune_start)):
+                for thread in forum.thread_set.filter(weight=0).filter(start__lte=timezone.now() - timedelta(days=forum.prune_start)):
                     thread.delete()
                     deleted += 1
             if forum.prune_last:
-                for thread in forum.thread_set.filter(start__lte=timezone.now() - timedelta(days=forum.prune_last)):
+                for thread in forum.thread_set.filter(weight=0).filter(start__lte=timezone.now() - timedelta(days=forum.prune_last)):
                     thread.delete()
                     deleted += 1
             if deleted:
