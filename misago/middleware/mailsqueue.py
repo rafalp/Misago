77346547c425856bbf958a409e04134a908ffa20
Ralfp <kontakt@rpiton.com>
2013-04-16 15:51:21 +0200

    Don't blurb exception pages from mails queue middleware.

--- misago/middleware/mailsqueue.py
+++ misago/middleware/mailsqueue.py
@@ -6,9 +6,12 @@ class MailsQueueMiddleware(object):
         request.mails_queue = []
 
     def process_response(self, request, response):
-        if request.mails_queue:
-            connection = mail.get_connection(fail_silently=settings.DEBUG)
-            connection.open()
-            connection.send_messages(request.mails_queue)
-            connection.close()
+        try:
+            if request.mails_queue:
+                connection = mail.get_connection(fail_silently=settings.DEBUG)
+                connection.open()
+                connection.send_messages(request.mails_queue)
+                connection.close()
+        except AttributeError:
+            pass
         return response
