4d0d879b945269ac822e1f27f7908a366b5560c4
Ralfp <kontakt@rpiton.com>
2013-06-20 19:43:06 +0200

    Fixed syntax error.

--- misago/search_indexes.py
+++ misago/search_indexes.py
@@ -3,7 +3,7 @@ from misago.models import Post
 
 class PostIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    forum = indexes.IntegerField(model_attr='forum')
+    forum = indexes.IntegerField(model_attr='forum_id')
     thread = indexes.CharField(model_attr='thread')
     user = indexes.CharField(model_attr='user_name')
     date = indexes.DateTimeField(model_attr='date')
@@ -12,14 +12,14 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
         return Post
 
     def prepare_thread(self, obj):
-        return thread.name
+        return obj.thread.name
 
     def get_updated_field(self):
         return 'current_date'
 
     def should_update(self, instance, **kwargs):
         if (instance.deleted or instance.moderated
-                or instance.thread.deletedor instance.thread.moderated):
+                or instance.thread.deletedor or instance.thread.moderated):
             self.remove_object(instance, **kwargs)
             return False
         return True
