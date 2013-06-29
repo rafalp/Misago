2b13dfaf11235bda4d5d0d0c27e5fdc77f54fe91
Ralfp <kontakt@rpiton.com>
2013-06-30 00:34:48 +0200

    Clear up TLS after request processing

--- misago/thread.py
+++ misago/thread.py
@@ -4,3 +4,9 @@ _thread_local = threading.local()
 
 def local():
     return _thread_local
+
+
+def clear():
+    for attr in _thread_local.__dict__.keys():
+        if attr[0] != '_':
+            del _thread_local.__dict__[attr]
