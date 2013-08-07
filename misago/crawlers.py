ecf28336b38f67793317f6c4c0de86e48b9369f9
Rafał Pitoń <kontakt@rpiton.com>
2013-08-07 16:59:46 +0200

    Detect unidentified crawlers. #43

--- misago/crawlers.py
+++ misago/crawlers.py
@@ -3,6 +3,7 @@ CRAWLERS_NAMES = {
     'google': 'Googlebot',
     'yahoo': 'Yahoo! Slurp',
     'yahooch': 'Yahoo! Slurp China',
+    'unidentified': 'Unidentified',
 }
 
 CRAWLERS_AGENTS = {
@@ -10,6 +11,9 @@ CRAWLERS_AGENTS = {
     'Googlebot/': 'google',
     'Yahoo! Slurp China': 'yahooch',
     'Yahoo! Slurp': 'yahoo',
+    'bot': 'unidentified',
+    'Bot': 'unidentified',
+    'BOT': 'unidentified',
 }
 
 CRAWLERS_HOSTS = {
@@ -20,20 +24,22 @@ class Crawler(object):
     crawler = False
     host = None
     username = None
-    
+
     def __init__(self, agent = None, ip = None):
         if agent is not None:
             for item in CRAWLERS_AGENTS.keys():
             	if agent.find(item) != -1:
                     self.crawler = True
                     self.username = CRAWLERS_AGENTS[item]
-                    
+                    break
+
         if ip is not None:
             for item in CRAWLERS_HOSTS.keys():
             	if ip == item:
                     self.crawler = True
                     self.username = CRAWLERS_HOSTS[item]
-                    
+                    break
+
         if self.crawler:
             self.username = CRAWLERS_NAMES[self.username]
             self.host = ip
