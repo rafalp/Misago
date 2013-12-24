defd471a96e46ee58851c45323cab55d5f6d439b
Rafał Pitoń <kontakt@rpiton.com>
2013-12-24 21:34:58 +0100

    Warning levels admin. #37

--- misago/models/warnlevelmodel.py
+++ misago/models/warnlevelmodel.py
@@ -6,8 +6,8 @@ class WarnLevel(models.Model):
     description = models.TextField(null=True, blank=True)
     warning_level = models.PositiveIntegerField(default=1, db_index=True)
     expires_after_minutes = models.PositiveIntegerField(default=0)
-    inhibit_posting_threads = models.PositiveIntegerField(default=0)
     inhibit_posting_replies = models.PositiveIntegerField(default=0)
+    inhibit_posting_threads = models.PositiveIntegerField(default=0)
 
     class Meta:
         app_label = 'misago'
