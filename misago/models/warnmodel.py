5574a17f5ec46a83b4ae0f30a09df594ae8aebe3
Rafał Pitoń <kontakt@rpiton.com>
2013-12-29 22:48:31 +0100

    Store canceler on user model. #37

--- misago/models/warnmodel.py
+++ misago/models/warnmodel.py
@@ -4,7 +4,7 @@ from misago.signals import rename_user
 class Warn(models.Model):
     user = models.ForeignKey('User')
     giver = models.ForeignKey('User', null=True, blank=True,
-                              on_delete=models.SET_NULL, related_name="warnings_given_set")
+        on_delete=models.SET_NULL, related_name="warnings_given_set")
     giver_name = models.CharField(max_length=255)
     giver_slug = models.SlugField(max_length=255)
     date = models.DateTimeField()
@@ -13,6 +13,10 @@ class Warn(models.Model):
     reason = models.TextField(null=True, blank=True)
     reason_preparsed = models.TextField(null=True, blank=True)
     canceled = models.BooleanField(default=False)
+    canceler = models.ForeignKey('User', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name="warnings_canceled_set")
+    canceler_name = models.CharField(max_length=255)
+    canceler_slug = models.SlugField(max_length=255)
 
     class Meta:
         app_label = 'misago'
@@ -22,6 +26,10 @@ def rename_user_handler(sender, **kwargs):
     Warn.objects.filter(giver=sender).update(
                                              giver_name=sender.username,
                                              giver_slug=sender.username_slug,
+                                             )
+    Warn.objects.filter(canceler=sender).update(
+                                             canceler_name=sender.username,
+                                             canceler_slug=sender.username_slug,
                                              )
 
 rename_user.connect(rename_user_handler, dispatch_uid="rename_user_warnings")
