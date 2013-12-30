b91434fbf5d21deed7056ce6d6356d41f15782d3
Rafał Pitoń <kontakt@rpiton.com>
2013-12-30 19:38:22 +0100

    Use misago's lazy gettext wrapper. #37

--- misago/apps/warnuser/alerts.py
+++ misago/apps/warnuser/alerts.py
@@ -1,3 +1,6 @@
+from misago.utils.translation import ugettext_lazy
+
+
 def you_have_been_warned(giver, receiver, warning):
     alert = receiver.alert(ugettext_lazy("%(username)s has increased your warning level.").message)
     alert.profile('username', giver)
