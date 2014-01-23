b22d52877e93d2f4434dc5023e21b11f44f90d52
Rafał Pitoń <kontakt@rpiton.com>
2014-01-23 03:21:47 +0100

    misago.views.utils test suite

--- misago/views/tests/test_utils.py
+++ misago/views/tests/test_utils.py
@@ -1,7 +1,37 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import RequestFactory
 from misago.views.utils import is_request_to_misago
 
-class MisagoExceptionHandlerTests(TestCase):
+
+VALID_PATHS = (
+    "/",
+    "/threads/",
+)
+
+INVALID_PATHS = (
+    "",
+    "somewhere/",
+)
+
+
+class IsRequestToMisagoTests(TestCase):
     def test_is_request_to_misago(self):
-        """is_request_to_misago correctly detects requests directed at Misago"""
-        self.fail("Not yet implemented")
+        """
+        is_request_to_misago correctly detects requests directed at Misago
+        """
+        misago_prefix = reverse('forum_index')
+
+        for path in VALID_PATHS:
+            request = RequestFactory().get('/')
+            request.path_info = path
+            self.assertTrue(
+                is_request_to_misago(request),
+                '"%s" is not overlapped by "%s"' % (path, misago_prefix))
+
+        for path in INVALID_PATHS:
+            request = RequestFactory().get('/')
+            request.path_info = path
+            self.assertFalse(
+                is_request_to_misago(request),
+                '"%s" is overlapped by "%s"' % (path, misago_prefix))
--- testproject/urls.py
+++ testproject/urls.py
@@ -1,12 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'testproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('misago.urls')),
 )
