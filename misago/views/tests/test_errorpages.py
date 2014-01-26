2a4aec4f97022c567403ec26bf4bbd5a427cd58a
Rafał Pitoń <kontakt@rpiton.com>
2014-01-26 22:53:33 +0100

    Dropper unneeded import and fixed status codes.

--- misago/views/tests/test_errorpages.py
+++ misago/views/tests/test_errorpages.py
@@ -1,20 +1,19 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
 from misago.views import errorpages
 
 
 class ErrorPageViewsTests(TestCase):
     def setUp(self):
-        c = Client()
-        response = c.get(reverse('forum_index'))
+        response = self.client.get(reverse('forum_index'))
         self.request = response.request
 
-    def test_permission_denied_returns_200(self):
+    def test_permission_denied_returns_403(self):
         """permission_denied error page has no show-stoppers"""
         response = errorpages.permission_denied(self.request)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 403)
 
-    def test_page_not_found_returns_200(self):
+    def test_page_not_found_returns_404(self):
         """page_not_found error page has no show-stoppers"""
         response = errorpages.page_not_found(self.request)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
--- misago/views/tests/test_views.py
+++ misago/views/tests/test_views.py
@@ -1,11 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
 
 
 class ForumIndexViewTests(TestCase):
     def test_forum_index_returns_200(self):
         """forum_index view has no show-stoppers"""
-        c = Client()
-        response = c.get(reverse('forum_index'))
+        response = self.client.get(reverse('forum_index'))
         self.assertEqual(response.status_code, 200)
