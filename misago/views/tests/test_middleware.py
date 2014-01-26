b465749df8013175e967754fba899ca624a7ca9b
Rafał Pitoń <kontakt@rpiton.com>
2014-01-26 22:35:09 +0100

    Covered middleware with tests.

---
+++ misago/views/tests/test_middleware.py
@@ -0,0 +1,22 @@
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from django.test import TestCase
+from django.test.client import RequestFactory
+from misago.views.middleware import MisagoExceptionHandlerMiddleware
+
+
+class MisagoExceptionHandlerMiddlewareTests(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get(reverse('forum_index'))
+
+    def test_middleware_returns_none_for_non_supported_exception(self):
+        exception = TypeError()
+        middleware = MisagoExceptionHandlerMiddleware()
+
+        self.assertFalse(middleware.process_exception(self.request, exception))
+
+    def test_middleware_returns_response_for_supported_exception(self):
+        exception = Http404()
+        middleware = MisagoExceptionHandlerMiddleware()
+
+        self.assertTrue(middleware.process_exception(self.request, exception))
