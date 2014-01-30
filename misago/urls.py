7f6a5ef18c72dc4ddfb8927968dbaae4f2cdcdb2
Rafał Pitoń <kontakt@rpiton.com>
2014-01-30 01:24:33 +0100

    Fixed link to forum index.

--- misago/urls.py
+++ misago/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 
-urlpatterns = patterns('misago.views',
+urlpatterns = patterns('misago.core',
     # forum_index link symbolises "root" of Misago links space
     # any request with path that falls below this one is assumed to be directed
     # at Misago and will be handled by misago.views.exceptionhandler if it
