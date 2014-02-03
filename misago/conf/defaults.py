9791d8201d7be70940152cde22f2c9f7ac6b37e1
Rafał Pitoń <kontakt@rpiton.com>
2014-02-03 03:52:09 +0100

    Add site address to template contex.

--- misago/conf/defaults.py
+++ misago/conf/defaults.py
@@ -56,6 +56,17 @@ MIDDLEWARE_CLASSES = (
     'misago.core.middleware.exceptionhandler.ExceptionHandlerMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'misago.core.context_processors.site_address',
+)
+
 # Register Misago directories
 
 LOCALE_PATHS = (
---
+++ misago/core/context_processors.py
@@ -0,0 +1,6 @@
+def site_address(request):
+    if request.is_secure():
+        address_template = 'https://%s'
+    else:
+        address_template = 'http://%s'
+    return {'SITE_ADDRESS': address_template % request.get_host(),}
