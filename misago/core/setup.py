b2181c56585340e3800561dc57457e282db787da
Rafał Pitoń <kontakt@rpiton.com>
2014-02-13 22:26:35 +0100

    Made tests for setup.py util

--- misago/core/setup.py
+++ misago/core/setup.py
@@ -1,6 +1,31 @@
+import re
 import os
 from optparse import OptionParser
 from django.core import management
+
+
+def validate_project_name(parser, project_name):
+    if project_name[0].isdigit():
+        parser.error("project_name cannot start with digit")
+
+    if project_name.startswith("-"):
+        parser.error("project_name cannot start with '-'")
+
+    if re.search("[^0-9a-zA-Z]", project_name):
+        parser.error("project_name cannot contain special characters")
+
+    # Ensure the given directory name doesn't clash with an existing
+    # Python package/module.
+    try:
+        __import__(project_name)
+    except ImportError:
+        pass
+    else:
+        parser.error("'%s' conflicts with the name of an existing "
+                     "Python module and cannot be used as a project "
+                     "name. Please try another name." % project_name)
+
+    return project_name
 
 
 def get_misago_project_template():
@@ -14,20 +39,8 @@ def start_misago_project():
 
     if len(args) != 1:
         parser.error("project_name must be specified")
-    project_name = args[0]
-    if project_name.startswith("-"):
-        parser.error("project_name cannot start with '-'")
 
-    # Ensure the given directory name doesn't clash with an existing
-    # Python package/module.
-    try:
-        __import__(project_name)
-    except ImportError:
-        pass
-    else:
-        parser.error("'%s' conflicts with the name of an existing "
-                     "Python module and cannot be used as a project "
-                     "name. Please try another name." % project_name)
+    project_name = validate_project_name(parser, args[0])
 
     argv = ['start-misago.py', 'startproject', project_name,
             '--template=%s' % get_misago_project_template()]
