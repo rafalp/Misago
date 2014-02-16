0a4ffbafe3fe2d1c2594edbc14c32e81203f8c03
Rafał Pitoń <kontakt@rpiton.com>
2014-02-16 23:00:44 +0100

    Navbar CSS updated.

---
+++ uiframework/misago/js/misago.js
@@ -0,0 +1,7 @@
+// Register tooltips
+$(function() {
+  $('.tooltip-top').tooltip({placement: 'top'});
+  $('.tooltip-bottom').tooltip({placement: 'bottom'});
+  $('.tooltip-left').tooltip({placement: 'left'});
+  $('.tooltip-right').tooltip({placement: 'right'});
+});
