ae159c6d4e025d80d0afc0e2d0ae00db73904343
Rafał Pitoń <kontakt@rpiton.com>
2014-02-20 02:38:02 +0100

    Moar styles for navbar, emptied flavor for custom overrides

--- uiframework/misago/js/misago.js
+++ uiframework/misago/js/misago.js
@@ -1,7 +1,7 @@
 // Register tooltips
 $(function() {
-  $('.tooltip-top').tooltip({placement: 'top'});
-  $('.tooltip-bottom').tooltip({placement: 'bottom'});
-  $('.tooltip-left').tooltip({placement: 'left'});
-  $('.tooltip-right').tooltip({placement: 'right'});
+  $('.tooltip-top').tooltip({placement: 'top', container: 'body'});
+  $('.tooltip-bottom').tooltip({placement: 'bottom', container: 'body'});
+  $('.tooltip-left').tooltip({placement: 'left', container: 'body'});
+  $('.tooltip-right').tooltip({placement: 'right', container: 'body'});
 });
