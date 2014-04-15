284eab9887dde9c2cd6c367a146f51ad9f691a9a
Rafał Pitoń <kontakt@rpiton.com>
2014-04-15 23:28:16 +0200

    Signin form

--- misago/users/views/auth.py
+++ misago/users/views/auth.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from misago.core.decorators import require_POST
 from misago.users.decorators import deny_authenticated, deny_guests
+from misago.users.forms.auth import AuthenticationForm
 
 
 @sensitive_post_parameters()
@@ -11,7 +13,14 @@ from misago.users.decorators import deny_authenticated, deny_guests
 @csrf_protect
 @never_cache
 def login(request):
-    return render(request, 'misago/login.html')
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            pass
+
+    return render(request, 'misago/login.html', {'form': form})
 
 
 @deny_guests
