126a509059d83a32897526d565fe418f78895f3e
Rafał Pitoń <kontakt@rpiton.com>
2014-04-18 00:32:29 +0200

    Added extremly basic user menu

---
+++ misago/users/templatetags/misago_avatars.py
@@ -0,0 +1,11 @@
+import hashlib
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='avatar')
+def avatar(user, size=200):
+    gravatar_hex = hashlib.md5(user.email).hexdigest()
+    return '//www.gravatar.com/avatar/%s?s=%s' % (gravatar_hex, size)
