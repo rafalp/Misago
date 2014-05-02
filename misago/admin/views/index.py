cfcc7878858eae36947588ea308c6e7929ff8ba9
Rafał Pitoń <kontakt@rpiton.com>
2014-05-02 02:56:03 +0200

    Dropped debug code

--- misago/admin/views/index.py
+++ misago/admin/views/index.py
@@ -1,8 +1,6 @@
-from django.contrib import messages
 from misago.admin.views import render
 
 
 def admin_index(request):
-    messages.success(request, 'Hello bro, I hope ya liking Miasgo!')
     namespace = request.resolver_match.namespace
     return render(request, 'misago/admin/index.html', {'namespace': namespace})
