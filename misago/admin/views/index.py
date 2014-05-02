1996a6fed0ae0ef27ce0859ac694cd862e190d6b
Rafał Pitoń <kontakt@rpiton.com>
2014-05-02 02:54:41 +0200

    Better alerts in admin

--- misago/admin/views/index.py
+++ misago/admin/views/index.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from misago.admin.views import render
 
 
 def admin_index(request):
+    messages.success(request, 'Hello bro, I hope ya liking Miasgo!')
     namespace = request.resolver_match.namespace
     return render(request, 'misago/admin/index.html', {'namespace': namespace})
