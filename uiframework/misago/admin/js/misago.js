bac8bd047a989827f9cd638fead8f94ff39f077e
Rafał Pitoń <kontakt@rpiton.com>
2014-05-07 00:09:10 +0200

    Checkboxes for selecting rows.

--- uiframework/misago/admin/js/misago.js
+++ uiframework/misago/admin/js/misago.js
@@ -5,3 +5,34 @@ $(function() {
   $('.tooltip-left').tooltip({placement: 'left', container: 'body'});
   $('.tooltip-right').tooltip({placement: 'right', container: 'body'});
 });
+
+
+// Tables
+$(function() {
+  $('.table tr').each(function() {
+    var $row = $(this);
+    var $checkbox = $row.find('input[type=checkbox]');
+    var $label = $checkbox.parent();
+
+    $label.addClass('ninja-label');
+    $label.parent().append('<a href="#"><span class="fa fa-check"></span></a>');
+    var $check = $label.parent().find('a');
+
+    if ($checkbox.prop("checked")) {
+      $check.toggleClass('active');
+      $row.addClass('active');
+    }
+
+    $check.click(function() {
+      $(this).toggleClass('active');
+      if ($(this).hasClass('active')) {
+        $(this).parent().find('input').prop("checked", true);
+        $row.addClass('active');
+      } else {
+        $(this).parent().find('input').prop("checked", false);
+        $row.removeClass('active');
+      }
+      return false;
+    });
+  });
+});
