cdee366688ef43364baf9788b41fef1bcac6a27b
Rafał Pitoń <kontakt@rpiton.com>
2014-05-22 01:21:20 +0200

    More randomness in user randomness

--- misago/faker/management/commands/createfakeusers.py
+++ misago/faker/management/commands/createfakeusers.py
@@ -1,8 +1,10 @@
+import random
 from faker import Factory
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
+from misago.users.models import Rank
 
 class Command(BaseCommand):
     help = 'Creates plenty of random fakey users for testing purposes'
@@ -12,6 +14,8 @@ class Command(BaseCommand):
         fake = Factory.create()
         User = get_user_model()
 
+        ranks = [r for r in Rank.objects.all()]
+
         message = 'Attempting to create %s fake user accounts!'
         self.stdout.write(message % fake_users_to_create)
 
@@ -20,8 +24,12 @@ class Command(BaseCommand):
         created_count = 0
         for i in xrange(fake_users_to_create):
             try:
+                kwargs = {
+                    'rank': random.choice(ranks),
+                }
+
                 User.objects.create_user(fake.first_name(), fake.email(),
-                                         'pass123')
+                                         'pass123', **kwargs)
             except (ValidationError, IntegrityError):
                 pass
             else:
