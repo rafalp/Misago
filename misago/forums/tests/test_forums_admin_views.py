20dc97513bbc58ee5f5773cea3e7b96a137de7e0
Rafał Pitoń <kontakt@rpiton.com>
2014-06-06 21:15:18 +0200

    Move forums views.

--- misago/forums/tests/test_forums_admin_views.py
+++ misago/forums/tests/test_forums_admin_views.py
@@ -6,8 +6,7 @@ from misago.forums.models import Forum
 class ForumAdminViewsTests(AdminTestCase):
     def test_link_registered(self):
         """admin nav contains forums link"""
-        response = self.client.get(
-            reverse('misago:admin:forums:nodes:index'))
+        response = self.client.get(reverse('misago:admin:forums:nodes:index'))
 
         self.assertIn(reverse('misago:admin:forums:nodes:index'),
                       response.content)
@@ -70,3 +69,75 @@ class ForumAdminViewsTests(AdminTestCase):
             reverse('misago:admin:forums:nodes:edit',
                     kwargs={'forum_id': private_threads.pk}))
         self.assertEqual(response.status_code, 302)
+
+    def test_move_views(self):
+        """move up view has no showstoppers"""
+        root = Forum.objects.root_category()
+
+        self.client.post(reverse('misago:admin:forums:nodes:new'),
+                         data={
+                             'name': 'Category A',
+                             'new_parent': root.pk,
+                             'role': 'category',
+                             'prune_started_after': 0,
+                             'prune_replied_after': 0,
+                         })
+        self.client.post(reverse('misago:admin:forums:nodes:new'),
+                         data={
+                             'name': 'Category B',
+                             'new_parent': root.pk,
+                             'role': 'category',
+                             'prune_started_after': 0,
+                             'prune_replied_after': 0,
+                         })
+
+        category_a = Forum.objects.get(slug='category-a')
+        category_b = Forum.objects.get(slug='category-b')
+
+        response = self.client.post(
+            reverse('misago:admin:forums:nodes:up',
+                    kwargs={'forum_id': category_b.pk}))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.get(reverse('misago:admin:forums:nodes:index'))
+        response = self.client.get(reverse('misago:admin:forums:nodes:index'))
+        self.assertEqual(response.status_code, 200)
+        position_a = response.content.find('Category A')
+        position_b = response.content.find('Category B')
+        self.assertTrue(position_a > position_b)
+
+        response = self.client.post(
+            reverse('misago:admin:forums:nodes:up',
+                    kwargs={'forum_id': category_b.pk}))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.get(reverse('misago:admin:forums:nodes:index'))
+        response = self.client.get(reverse('misago:admin:forums:nodes:index'))
+        self.assertEqual(response.status_code, 200)
+        position_a = response.content.find('Category A')
+        position_b = response.content.find('Category B')
+        self.assertTrue(position_a > position_b)
+
+        response = self.client.post(
+            reverse('misago:admin:forums:nodes:down',
+                    kwargs={'forum_id': category_b.pk}))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.get(reverse('misago:admin:forums:nodes:index'))
+        response = self.client.get(reverse('misago:admin:forums:nodes:index'))
+        self.assertEqual(response.status_code, 200)
+        position_a = response.content.find('Category A')
+        position_b = response.content.find('Category B')
+        self.assertTrue(position_a < position_b)
+
+        response = self.client.post(
+            reverse('misago:admin:forums:nodes:down',
+                    kwargs={'forum_id': category_b.pk}))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.get(reverse('misago:admin:forums:nodes:index'))
+        response = self.client.get(reverse('misago:admin:forums:nodes:index'))
+        self.assertEqual(response.status_code, 200)
+        position_a = response.content.find('Category A')
+        position_b = response.content.find('Category B')
+        self.assertTrue(position_a < position_b)
