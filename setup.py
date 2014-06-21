5b177a78959175e7aadebb853be6c57452ce8455
Rafał Pitoń <kontakt@rpiton.com>
2014-06-22 01:37:12 +0200

    Fixing travis.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email='kontakt@rpiton.com',
     install_requires=REQUIREMENTS,
     dependency_links=[
-        'https://github.com/django/django/archive/stable/1.7.x.zip',
+        'https://github.com/django/django/archive/stable/1.7.x.zip#egg=django-1.7',
     ],
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     include_package_data=True,
