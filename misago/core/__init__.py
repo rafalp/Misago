3ac50fd86549cc21d5ca6d5a5ddb587f0b8581b2
Rafał Pitoń <kontakt@rpiton.com>
2014-06-24 18:06:28 +0200

    Require PostgreSQL to start.

--- misago/core/__init__.py
+++ misago/core/__init__.py
@@ -1,1 +1,20 @@
+from django.conf import settings
+from django.core.checks import register, Critical
+
+
+@register()
+def example_check(app_configs, **kwargs):
+    errors = []
+
+    try:
+        supported_driver = 'django.db.backends.postgresql_psycopg2'
+        if settings.DATABASES['default']['ENGINE'] != supported_driver:
+            raise ValueError()
+    except (AttributeError, KeyError, ValueError):
+        errors.append(Critical(msg='Misago requires PostgreSQL database.',
+                               id='misago.001'))
+
+    return errors
+
+
 default_app_config = 'misago.core.apps.MisagoCoreConfig'
