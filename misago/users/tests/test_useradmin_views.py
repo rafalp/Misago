5548b99ca287a80eb0b7e2b2c33c759d236ca924
Rafał Pitoń <kontakt@rpiton.com>
2014-06-27 00:32:49 +0200

    Added tests to ListView search feature

--- misago/users/tests/test_useradmin_views.py
+++ misago/users/tests/test_useradmin_views.py
@@ -20,3 +20,35 @@ class UserAdminViewsTests(AdminTestCase):
         response = self.client.get(response['location'])
         self.assertEqual(response.status_code, 200)
         self.assertIn('TestAdmin', response.content)
+
+    def test_list_search(self):
+        """users list is searchable"""
+        response = self.client.get(
+            reverse('misago:admin:users:accounts:index'))
+        self.assertEqual(response.status_code, 302)
+
+        link_base = response['location']
+        response = self.client.get(link_base)
+        self.assertEqual(response.status_code, 200)
+
+        User = get_user_model()
+        user_a = User.objects.create_user('Tyrael', 't123@test.com', 'pass123')
+        user_b = User.objects.create_user('Tyrion', 't321@test.com', 'pass123')
+
+        # Search both
+        response = self.client.get(link_base + '&username_slug=tyr')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(user_a.username, response.content)
+        self.assertIn(user_b.username, response.content)
+
+        # Search tyrion
+        response = self.client.get(link_base + '&username_slug=tyrion')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(user_a.username in response.content)
+        self.assertIn(user_b.username, response.content)
+
+        # Search tyrael
+        response = self.client.get(link_base + '&email=t123@test.com')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(user_a.username, response.content)
+        self.assertFalse(user_b.username in response.content)
