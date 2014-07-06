b0bd96f5c2b8344787e894f3f92fbaecc8afc2ba
Rafał Pitoń <kontakt@rpiton.com>
2014-07-06 03:01:45 +0200

    ajax_only decorator for API's

--- misago/core/decorators.py
+++ misago/core/decorators.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 
+def ajax_only(f):
+    def decorator(request, *args, **kwargs):
+        if not request.is_ajax():
+            response = render(request, 'misago/errorpages/wrong_way.html')
+            response.status_code = 405
+            return response
+        else:
+            return f(request, *args, **kwargs)
+    return decorator
+
+
 def require_POST(f):
     def decorator(request, *args, **kwargs):
         if not request.method == 'POST':
