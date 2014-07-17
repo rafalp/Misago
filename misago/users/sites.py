01bb07918390974605816cdbdecdf4fbd03752a2
Rafał Pitoń <kontakt@rpiton.com>
2014-07-17 22:22:52 +0200

    Remove space

--- misago/users/sites.py
+++ misago/users/sites.py
@@ -42,6 +42,6 @@ class Site(object):
             })
 
 
-usercp_actions = Site('usercp ')
+usercp_actions = Site('usercp')
 users_list_tabs = Site('users list')
 user_profile_tabs = Site('user profile')
