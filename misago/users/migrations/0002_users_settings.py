d98662c5437ad719d7efdc794febd12073368021
Rafał Pitoń <kontakt@rpiton.com>
2014-07-28 20:17:19 +0200

    More pic-friendly avatar limit

--- misago/users/migrations/0002_users_settings.py
+++ misago/users/migrations/0002_users_settings.py
@@ -123,7 +123,7 @@ def create_users_settings_group(apps, schema_editor):
                     'description': _("Enter maximum allowed file size "
                                      "(in KB) for avatar uploads"),
                     'python_type': 'int',
-                    'value': 512,
+                    'value': 750,
                     'field_extra': {
                         'min_value': 0,
                     },
