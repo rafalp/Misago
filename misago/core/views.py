13cd0dc46c83040029238a78f3225728cbf8d91b
Rafał Pitoń <kontakt@rpiton.com>
2014-08-26 22:07:14 +0200

    Add ACL's to forums on index

--- misago/core/views.py
+++ misago/core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
+from misago.acl import add_acl
 from misago.forums.lists import get_forums_list
-
 from misago.users.online.ranks import get_ranks_online
 
 
 def forum_index(request):
+    forums_list = get_forums_list(request.user)
+    add_acl(request.user, forums_list)
+
     return render(request, 'misago/index.html', {
-        'categories': get_forums_list(request.user),
+        'categories': forums_list,
         'ranks_online': get_ranks_online(request.user),
     })
