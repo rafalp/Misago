c268123378ba6e2dbcaaf6fefcd6ab66c07f7628
Rafał Pitoń <kontakt@rpiton.com>
2014-08-31 21:36:17 +0200

    Display DB size on admin index

--- misago/admin/views/index.py
+++ misago/admin/views/index.py
@@ -1,6 +1,18 @@
+from misago.threads.models import Thread, Post
+from misago.users.models import User, ACTIVATION_REQUIRED_NONE
+
 from misago.admin.views import render
 
 
 def admin_index(request):
-    namespace = request.resolver_match.namespace
-    return render(request, 'misago/admin/index.html', {'namespace': namespace})
+    inactive_users = {'requires_activation__gt': ACTIVATION_REQUIRED_NONE}
+    db_stats = {
+        'threads': Thread.objects.count(),
+        'posts': Post.objects.count(),
+        'users': User.objects.count(),
+        'inactive_users': User.objects.filter(**inactive_users)
+    }
+
+    return render(request, 'misago/admin/index.html', {
+        'db_stats': db_stats,
+    })
