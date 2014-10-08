f9b764e8ecc2f612ef39fde6cb9d11fdd0fc3eb2
Rafał Pitoń <kontakt@rpiton.com>
2014-10-08 20:02:26 +0200

    Fixed email hashes for gravatar.

--- misago/users/utils.py
+++ misago/users/utils.py
@@ -2,7 +2,4 @@ import hashlib
 
 
 def hash_email(email):
-    email = email.lower()
-    while len(email) < 15:
-        email *= 2
-    return hashlib.md5(email).hexdigest()
+    return hashlib.md5(email.lower()).hexdigest()
