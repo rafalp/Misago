58a177cb67230050b4c71f31bc2a1f69e7584428
Rafał Pitoń <kontakt@rpiton.com>
2014-10-09 23:04:45 +0200

    Removed outdated UI framework

--- uiframework/README.rst
+++
@@ -1,5 +0,0 @@
-===================
-Misago UI Framework
-===================
-
-This directory contains Misago UI Framework, which is defacto Bootstrap + extra UI elements Misago users.
--- uiframework/misago/js/misago.js
+++
@@ -1,7 +0,0 @@
-// Register tooltips
-$(function() {
-  $('.tooltip-top').tooltip({placement: 'top', container: 'body'});
-  $('.tooltip-bottom').tooltip({placement: 'bottom', container: 'body'});
-  $('.tooltip-left').tooltip({placement: 'left', container: 'body'});
-  $('.tooltip-right').tooltip({placement: 'right', container: 'body'});
-});
