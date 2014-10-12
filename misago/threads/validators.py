cf6d9ef47cb819310d87daefef97e81c8f4d33ef
Rafał Pitoń <kontakt@rpiton.com>
2014-10-12 23:54:12 +0200

    Fixed thread title validator

--- misago/threads/validators.py
+++ misago/threads/validators.py
@@ -9,7 +9,7 @@ def validate_title(title):
     title_len = len(title)
 
     if not title_len:
-        raise forms.ValidationError(_("Enter thread title."))
+        raise ValidationError(_("Enter thread title."))
 
     if title_len < settings.thread_title_length_min:
         message = ungettext(
@@ -17,7 +17,7 @@ def validate_title(title):
             "Thread title should be at least %(limit)s characters long.",
             settings.thread_title_length_min)
         message = message % {'limit': settings.thread_title_length_min}
-        raise forms.ValidationError(message)
+        raise ValidationError(message)
 
     if title_len > settings.thread_title_length_max:
         message = ungettext(
@@ -25,7 +25,7 @@ def validate_title(title):
             "Thread title can't be longer than %(limit)s characters.",
             settings.thread_title_length_max,)
         message = message % {'limit': settings.thread_title_length_max}
-        raise forms.ValidationError(message)
+        raise ValidationError(message)
 
     error_not_sluggable = _("Thread title should contain "
                             "alpha-numeric characters.")
