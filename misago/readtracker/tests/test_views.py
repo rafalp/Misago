9a7aff6a96d51f38a220059df86e2df7332c885b
Rafał Pitoń <kontakt@rpiton.com>
2014-10-18 20:24:19 +0200

    "read_forum" view tests added.

--- misago/readtracker/tests/test_views.py
+++ misago/readtracker/tests/test_views.py
@@ -19,7 +19,29 @@ class AuthenticatedTests(AuthenticatedUserTestCase):
         make_read_aware(self.user, [forum])
         self.assertFalse(forum.is_read)
 
-        self.client.post(reverse('misago:read_all'))
+        response = self.client.post(reverse('misago:read_all'))
+        self.assertEqual(response.status_code, 302)
+
+        forum = Forum.objects.get(id=forum.id)
+        user = get_user_model().objects.get(id=self.user.id)
+
+        make_read_aware(user, [forum])
+        self.assertTrue(forum.is_read)
+
+    def test_read_forum(self):
+        """read_forum view updates reads cutoff on forum tracker"""
+        forum = Forum.objects.all_forums().filter(role="forum")[:1][0]
+        threads = [testutils.post_thread(forum) for t in xrange(10)]
+
+        forum = Forum.objects.get(id=forum.id)
+        make_read_aware(self.user, [forum])
+        self.assertFalse(forum.is_read)
+
+        response = self.client.post(
+            reverse('misago:read_forum', kwargs={'forum_id': forum.id}))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            response['location'].endswith(forum.get_absolute_url()))
 
         forum = Forum.objects.get(id=forum.id)
         user = get_user_model().objects.get(id=self.user.id)
