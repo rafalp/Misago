71e8bdc49eeeb1d1d77308c723f030241e7a22a7
Rafał Pitoń <kontakt@rpiton.com>
2014-10-30 17:58:58 +0100

    #453: test misagodbrelations

---
+++ misago/core/tests/test_misagodbrelations.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.utils.six import StringIO
+
+from misago.core.management.commands import misagodbrelations
+
+
+class MisagoDBRelationsTests(TestCase):
+    def test_command_has_no_errors(self):
+        """command raises no errors during execution"""
+        command = misagodbrelations.Command()
+
+        command.execute(stdout=StringIO())
