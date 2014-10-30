0b03d565651e7aa19fe6fa9064e15183fab60c9d
Rafał Pitoń <kontakt@rpiton.com>
2014-10-30 16:18:05 +0100

    #453: createsuperuser tests

---
+++ misago/users/tests/test_createsuperuser.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.test import TestCase
+from django.utils.six import StringIO
+
+
+class CreateSuperuserTests(TestCase):
+    def test_create_superuser(self):
+        """command creates superuser"""
+        out = StringIO()
+        call_command(
+            "createsuperuser",
+            interactive=False,
+            username="joe",
+            email="joe@somewhere.org",
+            password="Pass.123",
+            stdout=out
+        )
+
+        new_user = get_user_model().objects.order_by('-id')[:1][0]
+
+        self.assertEqual(
+            out.getvalue().splitlines()[-1].strip(),
+            'Superuser #%s has been created successfully.' % new_user.pk)
+
+        self.assertEqual(new_user.username, 'joe')
+        self.assertEqual(new_user.email, 'joe@somewhere.org')
+        self.assertTrue(new_user.check_password("Pass.123"))
