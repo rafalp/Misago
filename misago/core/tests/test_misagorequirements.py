6e59e3a45bbd95baa77fbd62cfbdc3082c4db9e8
Rafał Pitoń <kontakt@rpiton.com>
2014-10-30 18:01:25 +0100

    #453: list misago requirments

---
+++ misago/core/tests/test_misagorequirements.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.utils.six import StringIO
+
+from misago.core.management.commands import misagorequirements
+
+
+class MisagoRequirementsTests(TestCase):
+    def test_list_misago_requirements(self):
+        """command returns requirements.txt contents"""
+        command = misagorequirements.Command()
+
+        out = StringIO()
+        command.execute(stdout=out)
+        command_output = out.getvalue()
+
+        self.assertIn("django==", command_output)
