3b7f6b76212304d29ba295c106efd15630221594
Rafał Pitoń <kontakt@rpiton.com>
2015-02-22 20:05:33 +0100

    cleaned imports

--- misago/users/api/auth.py
+++ misago/users/api/auth.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.contrib import auth
-from django.utils.translation import ugettext as _
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
@@ -9,8 +7,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from misago.users.decorators import (deny_authenticated, deny_guests,
-                                     deny_banned_ips)
+from misago.users.decorators import deny_authenticated, deny_banned_ips
 from misago.users.forms.auth import AuthenticationForm
 from misago.users.serializers import AuthenticatedUserSerializer
 
