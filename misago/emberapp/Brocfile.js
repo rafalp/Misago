4fd9be9a62894a28a4b6dc52e328384b65e6ff9c
Rafał Pitoń <kontakt@rpiton.com>
2015-03-01 23:15:35 +0100

    don't store config in meta
    note: this breaks /tests/, use  ember test --server for tests instead

--- misago/emberapp/Brocfile.js
+++ misago/emberapp/Brocfile.js
@@ -3,7 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
-  //storeConfigInMeta: false,
+  storeConfigInMeta: false,
   fingerprint: {
     enabled: false
   },
