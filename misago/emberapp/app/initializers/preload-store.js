1e82f7b1dfb71460abfa0394f9e69a3915025050
Rafał Pitoń <kontakt@rpiton.com>
2015-05-01 18:06:43 +0200

    tiny code formatting fix

--- misago/emberapp/app/initializers/preload-store.js
+++ misago/emberapp/app/initializers/preload-store.js
@@ -2,6 +2,7 @@ import PreloadStore from 'misago/services/preload-store';
 
 export function initialize(container, application) {
   application.register('service:preload-store', PreloadStore, { instantiate: false });
+
   application.inject('route', 'preloadStore', 'service:preload-store');
 }
 
