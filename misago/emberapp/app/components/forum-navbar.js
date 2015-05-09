d251536ccbd2beb0f529e0487e3b8b540cf75660
Rafał Pitoń <kontakt@rpiton.com>
2015-05-09 02:48:47 +0200

    simplified navbar affix

--- misago/emberapp/app/components/forum-navbar.js
+++ misago/emberapp/app/components/forum-navbar.js
@@ -4,20 +4,15 @@ export default Ember.Component.extend({
   classNames: 'navbar-spacer',
 
   affixNavbar: function() {
-    // take navbar's height in document
+    // reserve navbar's height in document
     this.$().height(this.$().height());
 
     // affix navbar
-    var offset = this.$('.navbar').offset().top;
-    if (offset === 0) {
-      // workaround around bootstrap affix mishandling elements
-      // with resting position on top of page
-      offset = 1;
-    }
-
     this.$('.navbar').affix({
       offset: {
-        top: offset
+        // workaround around bootstrap affix mishandling
+        // elements with resting position on top of page
+        top: this.$('.navbar').offset().top + 1
       }
     });
   }.on('didInsertElement')
