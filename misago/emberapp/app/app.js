99648fbbfccc928de11a0ebe286a83d25f298a1c
Rafał Pitoń <kontakt@rpiton.com>
2015-06-07 19:59:17 +0200

    updated Ember-CLI to 0.2.7

--- misago/emberapp/app/app.js
+++ misago/emberapp/app/app.js
@@ -3,9 +3,11 @@ import Resolver from './resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
+var App;
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-var App = Ember.Application.extend({
+App = Ember.Application.extend({
   rootElement: config.rootElement,
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
