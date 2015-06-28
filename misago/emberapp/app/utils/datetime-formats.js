0ec236d08ed6e909db24b43787a923906ade29a2
Rafał Pitoń <kontakt@rpiton.com>
2015-06-28 22:01:20 +0200

    dropped unused js file

--- misago/emberapp/app/utils/datetime-formats.js
+++
@@ -1,9 +0,0 @@
-import PreloadStore from 'misago/services/preload-store';
-
-export function local(datetime) {
-  return datetime.utcOffset(PreloadStore.get('utcOffset') / 60);
-}
-
-export default function datetimeFormats() {
-  return true;
-}
