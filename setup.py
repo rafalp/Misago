1fd1ddd90384e3d1b92e6e27016f8a812e7349cd
Mantas <sirexas@gmail.com>
2015-07-09 18:38:12 +0300

    setup.py for Misage 0.5.x
    One of the strengths of Python is that you can install python packages in many
    ways and use them in you project.
    
    Unfortunately that is not the case with Misango. In stead of using standard
    python tools and infrastructure one has to exception and for Misango and the
    deployment in a different way.
    
    This change fixes that by making Misago fully working Python package.
    
    The package also includes data files. Data files can be accessed using
    setuptools:
    
        import pkg_resources as pres
    
        MISAGO_DIR = pres.resource_filename('misago', '')
    
        STATICFILES_DIRS = (
            os.path.join(MISAGO_DIR, 'attachments')
            os.path.join(MISAGO_DIR, 'media')
            os.path.join(MISAGO_DIR, 'static')
        )

---
+++ setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+from setuptools import setup, find_packages
+from setuptools.command.sdist import sdist as _sdist
+from misago import __version__ as version
+
+here = os.path.dirname(__file__)
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+with open(os.path.join(here, 'requirements.txt')) as f:
+    REQUIREMENTS = [x.strip() for x in f.readlines()]
+
+
+def copy_data_files(paths):
+    for path in paths:
+        dest = os.path.join('misago', path)
+        if os.path.exists(dest):
+            shutil.rmtree(dest)
+        shutil.copytree(path, dest)
+
+
+class sdist(_sdist):
+
+    def run(self):
+        copy_data_files(['attachments', 'media', 'static', 'templates'])
+        _sdist.run(self)
+
+
+setup(
+    name='Misago',
+    version=version,
+    license='GNU General Public License v2 (GPLv2)',
+    description='Misago is complete, featured and modern forum solution.',
+    long_description=README,
+    url='http://www.misago-project.org/',
+    author=u'Rafał Pitoń',
+    author_email='kontakt@rpiton.com',
+    install_requires=REQUIREMENTS,
+    extras_require={
+        'postgresql': ['psycopg2'],
+    },
+    cmdclass={'sdist': sdist},
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
