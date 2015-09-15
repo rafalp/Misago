a5740afa0655f7145baa528eafffbd03c7554643
Rafał Pitoń <kontakt@rpiton.com>
2015-09-15 21:07:42 +0200

    changed title object to actual one

--- misago/frontend/misago/services/title.js
+++ misago/frontend/misago/services/title.js
@@ -1,35 +1,35 @@
 (function (Misago) {
   'use strict';
 
-  Misago.addService('page-title', function(_) {
-    _.title = {
-      forum_name: _.settings.forum_name,
-
-      set: function(title) {
-        if (title) {
-          this._set_complex(title);
-        } else {
-          document.title = this.forum_name;
-        }
-      },
-
-      _set_complex: function(title) {
-        if (typeof title === 'string') {
-          title = {title: title};
-        }
-
-        var completeTitle = title.title;
-
-        if (typeof title.page !== 'undefined' && title.page > 1) {
-          completeTitle += ' (' + interpolate(gettext('page %(page)s'), { page:title.page }, true) + ')';
-        }
-
-        if (typeof title.parent !== 'undefined') {
-          completeTitle += ' | ' + title.parent;
-        }
-
-        document.title = completeTitle + ' | ' + this.forum_name;
+  var PageTitle = function(forum_name) {
+    this.set = function(title) {
+      if (title) {
+        this._set_complex(title);
+      } else {
+        document.title = forum_name;
       }
     };
+
+    this._set_complex = function(title) {
+      if (typeof title === 'string') {
+        title = {title: title};
+      }
+
+      var completeTitle = title.title;
+
+      if (typeof title.page !== 'undefined' && title.page > 1) {
+        completeTitle += ' (' + interpolate(gettext('page %(page)s'), { page:title.page }, true) + ')';
+      }
+
+      if (typeof title.parent !== 'undefined') {
+        completeTitle += ' | ' + title.parent;
+      }
+
+      document.title = completeTitle + ' | ' + forum_name;
+    };
+  };
+
+  Misago.addService('page-title', function(_) {
+    _.title = new PageTitle(_.settings.forum_name)
   });
 }(Misago.prototype));
