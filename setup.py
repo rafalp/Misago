f07bb05b018dc18094c5f84067a8cb57190f24ca
Rafał Pitoń <kontakt@rpiton.com>
2016-02-07 21:52:51 +0100

    run against specified django version (for now)

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ from misago import __version__ as version
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
-requirements_path = os.path.join(os.path.dirname(__file__),
-                                 'misago/project_template/requirements.txt')
+requirements_path = os.path.join(
+    os.path.dirname(__file__), 'misago/project_template/requirements.txt')
 with open(requirements_path, "r") as f:
     REQUIREMENTS = [x.strip() for x in f.readlines()]
-
-REQUIREMENTS = REQUIREMENTS[1:]
 
 
 # allow setup.py to be run from any path
