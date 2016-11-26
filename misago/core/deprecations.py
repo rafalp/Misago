b49ad62beaa5efcb218bea7879d90286121bb8eb
Rafał Pitoń <kontakt@rpiton.com>
2016-11-26 01:45:57 +0100

    updated doc comment for deprecations utility

--- misago/core/deprecations.py
+++ misago/core/deprecations.py
@@ -1,3 +1,20 @@
+"""
+Utility module useful for refactors,
+
+Defines utility function for easy raising of warnings in deprecated functions.
+
+This function is set to use stacklevel 3 as default making following code in test.py:
+
+def depreacted_function():
+    warn("This function is deprecated")
+
+
+def other_function():
+    deprecated_function()
+
+
+Will raise warning about 1st line in other_function calling deprecated function.
+"""
 import warnings
 
 
