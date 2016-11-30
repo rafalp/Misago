be9c20b0d470bbfce429b98d57704feb09f16476
TheKit <nekit1000@gmail.com>
2016-11-30 10:44:06 +0000

    Change commands to use add_arguments

--- misago/core/management/commands/remakemisagochecksums.py
+++ misago/core/management/commands/remakemisagochecksums.py
@@ -8,12 +8,21 @@ from ...signals import secret_key_changed
 class Command(BaseCommand):
     help = 'Regenerates Misago checksums after SECRET_KEY changed.'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--force',
+            action='store_true',
+            dest='force',
+            default=False,
+            help='Do not ask for confirmation',
+        )
+
     def handle(self, *args, **options):
         message = force_str("This will replace all checksums "
                             "in database with new ones, marking "
                             "all data as trusted. Are you sure "
                             "you wish to continue? [Y/n]")
-        if '--force' in args or input(message).strip().lower() == "y":
+        if options['force'] or input(message).strip().lower() == "y":
             self.stdout.write("\nRegenerating checksums...")
             secret_key_changed.send(self)
             self.stdout.write("\nDone!")
