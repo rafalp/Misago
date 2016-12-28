83e304c2d97f6ad8c85fc2aa5796bb7e90eacfdc
Rafał Pitoń <kontakt@rpiton.com>
2016-12-29 00:34:49 +0100

    fix #630

--- misago/threads/paginator.py
+++ misago/threads/paginator.py
@@ -17,17 +17,14 @@ class PostsPaginator(Paginator):
         if self.count == 0 and not self.allow_empty_first_page:
             return 0
         hits = max(1, self.count - self.orphans)
-        hits += int(ceil(hits / float(self.per_page)))
-        return int(ceil(hits / float(self.per_page)))
+        return int(ceil(hits / float(self.per_page - 1)))
 
     def page(self, number):
         """
         Returns a Page object for the given 1-based page number.
         """
         number = self.validate_number(number)
-        bottom = (number - 1) * self.per_page
-        if number > 1:
-            bottom -= number - 1
+        bottom = (number - 1) * (self.per_page - 1)
         top = bottom + self.per_page
         if top + self.orphans >= self.count:
             top = self.count
