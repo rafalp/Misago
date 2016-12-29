c5ad4eb30e9652f22f594182a5efd41e935939b2
Rafał Pitoń <kontakt@rpiton.com>
2016-12-29 06:31:58 +0100

    deleted unsued markup.editor module

--- misago/markup/__init__.py
+++ misago/markup/__init__.py
@@ -1,7 +1,6 @@
 from .finalise import finalise_markup
 from .flavours import common as common_flavour, signature as signature_flavour
 from .parser import parse
-from .editor import Editor
 
 
 default_app_config = 'misago.markup.apps.MisagoMarkupConfig'
