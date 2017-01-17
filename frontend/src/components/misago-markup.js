78c8ade24aa4000a3b3d76ac7267b895a122927e
Rafał Pitoń <kontakt@rpiton.com>
2017-01-17 22:52:47 +0100

    fix #684: replace alone youtube urls with players

--- frontend/src/components/misago-markup.js
+++ frontend/src/components/misago-markup.js
@@ -1,8 +1,14 @@
 // jshint ignore:start
 import React from 'react';
+import onebox from 'misago/services/one-box';
+
 
 export default function(props) {
- return (
-    <article className="misago-markup" dangerouslySetInnerHTML={{__html: props.markup}} />
+  return (
+    <article
+      className="misago-markup"
+      dangerouslySetInnerHTML={{__html: props.markup}}
+      ref={node => onebox.render(node)}
+    />
   );
 }
---
+++ frontend/src/services/one-box.js
@@ -0,0 +1,90 @@
+const ytRegExp = new RegExp('^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*');
+
+export class OneBox {
+  constructor() {
+    this._youtube = {};
+  }
+
+  // jshint ignore:start
+  render = (domnode) => {
+    if (!domnode) return;
+
+    const anchors = domnode.querySelectorAll('p>a');
+    for(let i = 0; i < anchors.length; i++ ) {
+      const a = anchors[i];
+      const p = a.parentNode;
+      const onlyChild = p.children.length === 1;
+
+      if (!this._youtube[a.href]) {
+        this._youtube[a.href] = parseYoutubeUrl(a.href);
+      }
+
+      const youtubeMovie = this._youtube[a.href];
+      if (onlyChild && !!youtubeMovie && youtubeMovie.data !== false) {
+        this.swapYoutubePlayer(a, youtubeMovie);
+      }
+    }
+  };
+  // jshint ignore:end
+
+  swapYoutubePlayer(element, youtube) {
+    let url = 'https://www.youtube.com/embed/';
+    url += youtube.video;
+    url += '?rel=0';
+    if (youtube.start) {
+      url += '&start=' + youtube.start;
+    }
+
+    $(element).replaceWith($('<iframe width="560" height="315" src="' + url + '" frameborder="0" allowfullscreen></iframe>'));
+  }
+}
+
+export default new OneBox();
+
+export function parseYoutubeUrl(url) {
+  const cleanedUrl = cleanUrl(url);
+  const video = cleanedUrl.match(ytRegExp)[1];
+
+  if (!video) return null;
+
+  let start = 0;
+  if (cleanedUrl.indexOf('?') > 0){
+    const query = cleanedUrl.substr(cleanedUrl.indexOf('?') + 1);
+    const timebit = query.split('&').filter((i) => {
+      return i.substr(0, 2) === 't=';
+    })[0];
+
+    if (timebit) {
+      const bits = timebit.substr(2).split('m');
+      if (bits[0].substr(-1) === 's') {
+        start += parseInt(bits[0].substr(0, bits[0].length - 1));
+      } else {
+        start += parseInt(bits[0]) * 60;
+        if (!!bits[1] && bits[1].substr(-1) === 's') {
+          start += parseInt(bits[1].substr(0, bits[1].length - 1));
+        }
+      }
+    }
+  }
+
+  return {
+    start,
+    video
+  };
+}
+
+export function cleanUrl(url) {
+  let clean = url;
+
+  if (url.substr(0, 8) === 'https://') {
+    clean = clean.substr(8);
+  } else if (url.substr(0, 7) === 'http://') {
+    clean = clean.substr(7);
+  }
+
+  if (clean.substr(0, 4) === 'www.') {
+    clean = clean.substr(4);
+  }
+
+  return clean;
+}
