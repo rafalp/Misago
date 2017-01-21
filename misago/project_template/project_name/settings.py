05c929f8ce8389a40277e5f39f910342770f6ff0
Rafał Pitoń <kontakt@rpiton.com>
2017-01-21 23:28:46 +0100

    fix #590: added empty password to DB setting, tested project creation

--- misago/project_template/project_name/settings.py
+++ misago/project_template/project_name/settings.py
@@ -37,6 +37,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': '',
         'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 5432,
     }
