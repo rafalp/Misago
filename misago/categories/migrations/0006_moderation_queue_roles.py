59e2e2e3703fdbf1f7d99120ed86125086c80282
Rafał Pitoń <kontakt@rpiton.com>
2017-03-03 22:53:19 +0100

    #745: added moderation queue role

---
+++ misago/categories/migrations/0006_moderation_queue_roles.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.utils.translation import ugettext as _
+
+
+def create_default_categories_roles(apps, schema_editor):
+    CategoryRole = apps.get_model('misago_categories', 'CategoryRole')
+
+    CategoryRole.objects.create(
+        name=_('In moderation queue'),
+        permissions={
+            # threads perms
+            'misago.threads.permissions.threads': {
+                'require_threads_approval': 1,
+                'require_replies_approval': 1,
+            },
+        }
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('misago_categories', '0005_auto_20170303_2027'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_default_categories_roles),
+    ]
