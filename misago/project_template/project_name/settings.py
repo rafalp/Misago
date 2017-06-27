37a2299b08e69f09b45ad1a6d2ff629e8c2a5426
Rafał Pitoń <kontakt@rpiton.com>
2017-06-27 23:39:53 +0200

    small cleanups of template settings.py

--- misago/project_template/project_name/settings.py
+++ misago/project_template/project_name/settings.py
@@ -12,14 +12,15 @@ https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
 
 import os
 
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 # Define placeholder gettext function
 # This function will mark strings in settings visible to makemessages
 # without need for Django's i18n features be initialized first.
 _ = lambda x: x
-
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
