4ae2e5cf7c94f0d4ccee212d71386771827dcc06
Rafał Pitoń <kontakt@rpiton.com>
2017-08-13 01:06:04 +0200

    bump version to 0.10

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     ]},
     test_suite="runtests.runtests",
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
