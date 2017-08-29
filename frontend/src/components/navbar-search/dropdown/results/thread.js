ea4c133886bd3ee45b98279900cc3741ed4b21f1
Rafał Pitoń <kontakt@rpiton.com>
2017-08-29 22:25:43 +0200

    wip #886: tweak quick search results

--- frontend/src/components/navbar-search/dropdown/results/thread.js
+++ frontend/src/components/navbar-search/dropdown/results/thread.js
@@ -9,21 +9,17 @@ export default function({ result }) {
   return (
     <li>
       <a href={result.url.index} className="dropdown-search-thread">
-        <div className="media">
-          <div className="media-body">
-            <h5 className="media-heading">{thread.title}</h5>
-            <small className="dropdown-search-post-content">
-              {$(result.content).text()}
-            </small>
-            <small>
-              {interpolate(footer, {
-                category: result.category.name,
-                posted_on: moment(result.posted_on).format('LL'),
-                poster: result.poster_name
-              }, true)}
-            </small>
-          </div>
-        </div>
+        <h5>{thread.title}</h5>
+        <small className="dropdown-search-post-content">
+          {$(result.content).text()}
+        </small>
+        <small className="dropdown-search-post-footer">
+          {interpolate(footer, {
+            category: result.category.name,
+            posted_on: moment(result.posted_on).format('LL'),
+            poster: result.poster_name
+          }, true)}
+        </small>
       </a>
     </li>
   );
