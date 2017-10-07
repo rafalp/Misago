1b2d6a9e33e88760ea22ded04ccf06503380cad7
Rafał Pitoń <kontakt@rpiton.com>
2017-10-07 17:32:13 +0200

    #675: PostRead model and migrations for it

--- misago/readtracker/models.py
+++ misago/readtracker/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
 class CategoryRead(models.Model):
@@ -12,6 +13,11 @@ class CategoryRead(models.Model):
         on_delete=models.CASCADE,
     )
     last_read_on = models.DateTimeField()
+
+    def __init__(self, *args, **kwargs):
+        from misago.core import deprecations
+        deprecations.warn("CategoryRead has been deprecated")
+        super(CategoryRead, self).__init__(*args, **kwargs)
 
 
 class ThreadRead(models.Model):
@@ -28,3 +34,28 @@ class ThreadRead(models.Model):
         on_delete=models.CASCADE,
     )
     last_read_on = models.DateTimeField()
+
+    def __init__(self, *args, **kwargs):
+        from misago.core import deprecations
+        deprecations.warn("ThreadRead has been deprecated")
+        super(ThreadRead, self).__init__(*args, **kwargs)
+
+
+class PostRead(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    category = models.ForeignKey(
+        'misago_categories.Category',
+        on_delete=models.CASCADE,
+    )
+    thread = models.ForeignKey(
+        'misago_threads.Thread',
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        'misago_threads.Post',
+        on_delete=models.CASCADE,
+    )
+    last_read_on = models.DateTimeField(default=timezone.now)
