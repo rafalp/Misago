dd8332d1db03d984503e7ae5489163b07d4cbac4
dvv.avinash <dvv.avinash@gmail.com>
2018-05-30 01:19:42 +0530

    importing MISAGO_IP_STORE_TIME is updated to misago.setting

--- misago/users/management/commands/removeips.py
+++ misago/users/management/commands/removeips.py
@@ -1,17 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand, CommandError
 
+from misago.conf import settings
 from misago.core.utils import ANONYMOUS_IP
-from misago.conf.defaults import MISAGO_IP_STORE_TIME
 from misago.users.signals import anonymize_old_ips
-
 
 class Command(BaseCommand):
     help =  "Anonymizes users IPs stored for longer than set in MISAGO_IP_STORE_TIME."
     
     def handle(self, *args, **options):
-      if MISAGO_IP_STORE_TIME == None:
-        raise CommandError('Anonymization of the old user IPs is currently disabled')
+      if not settings.MISAGO_IP_STORE_TIME:
+        raise self.stdout.write('Anonymization of the old user IPs is currently disabled')
+        return
       else:
         anonymize_old_ips.send(sender=self)
         self.stdout.write("IP addresses older than {:d} days have been anonymized!".format(MISAGO_IP_STORE_TIME))
--- misago/users/signals.py
+++ misago/users/signals.py
@@ -1,10 +1,12 @@
-from django.dispatch import Signal, receiver
-from django.contrib.auth import get_user_model
 from django.utils import timezone
 from datetime import timedelta
+
+from django.dispatch import Signal, receiver
+from django.contrib.auth import get_user_model
+
 from django.db.models import Q
 from misago.core.utils import ANONYMOUS_IP
-from misago.conf.defaults import MISAGO_IP_STORE_TIME
+from misago.conf import settings
 
 
 UserModel = get_user_model()
@@ -19,10 +21,13 @@ username_changed = Signal()
 def handle_name_change(sender, **kwargs):
     sender.user_renames.update(changed_by_username=sender.username)
 
+
 @receiver(anonymize_old_ips)
 def anonymize_old_registrations_ips(sender, **kwargs):
-    anonymization_cutoff = timezone.now() - timedelta(days=MISAGO_IP_STORE_TIME)
+    anonymization_cutoff = timezone.now() - timedelta(days=settings.MISAGO_IP_STORE_TIME)
     ip_is_too_new = Q(joined_on__lt=anonymization_cutoff)
     ip_is_already_anonymized = Q(joined_from_ip=ANONYMOUS_IP)
     queryset = UserModel.objects.exclude(ip_is_too_new | ip_is_already_anonymized)
+
     queryset.update(joined_from_ip=ANONYMOUS_IP)
+
