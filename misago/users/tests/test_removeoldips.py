8518044fc3bb639941a6feb03f4246a43a9ab752
dvv.avinash <dvv.avinash@gmail.com>
2018-06-07 01:11:02 +0530

    anonymization is updated to removal

--- misago/users/tests/test_removeoldips.py
+++ misago/users/tests/test_removeoldips.py
@@ -15,7 +15,7 @@ UserModel = get_user_model()
 
 class RemoveOldIpsTests(TestCase):
     def test_removeoldips_recent_user(self):
-        """command is not anonymizing user's IP if its recent"""
+        """command is not removing user's IP if its recent"""
         user = UserModel.objects.create_user('Bob', 'bob@bob.com')
         call_command(removeoldips.Command())
         user_joined_from_ip = UserModel.objects.get(pk=user.pk).joined_from_ip
@@ -23,7 +23,7 @@ class RemoveOldIpsTests(TestCase):
         self.assertNotEqual(user_joined_from_ip, ANONYMOUS_IP)
     
     def test_removeoldips_old_user(self):
-        """command is anonymizing user's IP if its old"""
+        """command is removing user's IP if its old"""
         joined_on_past = timezone.now() - timedelta(days=50)
         user = UserModel.objects.create_user('Bob1', 'bob1@bob.com')
         user.joined_on = joined_on_past
@@ -34,15 +34,15 @@ class RemoveOldIpsTests(TestCase):
         self.assertEqual(user_joined_from_ip, ANONYMOUS_IP)
 
     @override_settings(MISAGO_IP_STORE_TIME=None)
-    def test_not_anonymizing_user_ip(self):
-        """command is not anonymizing user's IP if anonymization is disabled"""
+    def test_not_removing_user_ip(self):
+        """command is not removing user's IP if removing is disabled"""
         user = UserModel.objects.create_user('Bob1', 'bob1@bob.com')
         
         out = StringIO()
         call_command(removeoldips.Command(), stdout=out)
         command_output = out.getvalue().splitlines()[0].strip()
         
-        self.assertEqual(command_output, "IP anonymization is disabled.")
+        self.assertEqual(command_output, "Old IP removal is disabled.")
         
         user_joined_from_ip = UserModel.objects.get(pk=user.pk).joined_from_ip
         self.assertNotEqual(user_joined_from_ip, ANONYMOUS_IP)
