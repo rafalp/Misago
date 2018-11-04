b8aeb44238f8684717613c34fc4c34aab49dca8f
rafalp <kontakt@rpiton.com>
2018-11-04 03:34:04 +0100

    Fix testrunner

--- runtests.py
+++ runtests.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-if __name__ == "__main__":
+def runtests():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devproject.test_settings")
     try:
         from django.core.management import execute_from_command_line
