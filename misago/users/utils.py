d15b433f122204fea572f72d5a5e483038697aa7
Luis Esteban <rodriguezjluis0@gmail.com>
2018-11-05 01:20:40 -0600

    create create_user_deletion function

--- misago/users/utils.py
+++ misago/users/utils.py
@@ -1,3 +1,7 @@
+from django.utils import timezone
+
+from misago.users.models.deleteduser import DeletedUser
+
 import hashlib
 
 from django.utils import timezone
@@ -8,17 +12,14 @@ from misago.users.models.deleteduser import DeletedUser
 def hash_email(email):
     return hashlib.md5(email.lower().encode('utf-8')).hexdigest()
 
+
 def record_user_deleted_by_self():
-    return DeletedUser.objects.create(
-        deleted_by=1
-    )
+    return DeletedUser.objects.create(deleted_by=DeletedUser.DELETED_BY_USER)
+
 
 def record_user_deleted_by_staff():
-    return DeletedUser.objects.create(
-        deleted_by=2
-    )
+    return DeletedUser.objects.create(deleted_by=DeletedUser.DELETED_BY_STAFF)
+
 
 def record_user_deleted_by_system():
-    return DeletedUser.objects.create(
-        deleted_by=3
-    )
+    return DeletedUser.objects.create(deleted_by=DeletedUser.DELETED_BY_SYSTEM)
