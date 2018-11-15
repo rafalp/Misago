3f3e830f833cafe0d52fc0c1692b254e94cb0a7d
rafalp <kontakt@rpiton.com>
2018-11-15 23:23:50 +0100

    Move new and edit user forms to misago admin forms

--- misago/admin/templatetags/misago_admin_form.py
+++ misago/admin/templatetags/misago_admin_form.py
@@ -1,4 +1,5 @@
 from django import forms, template
+from django.utils.html import format_html_join
 
 register = template.Library()
 
@@ -14,12 +15,35 @@ def admin_form_row(field, label_class, field_class):
 
 @register.inclusion_tag("misago/admin/form/input.html")
 def admin_form_input(field):
-    widget_context = field.field.widget.get_context(field.html_name, field.value(), {})
+    attrs = field.field.widget.attrs
+    context = field.field.widget.get_context(field.html_name, field.value(), attrs)
+    context["attrs"] = attrs
+    context["field"] = field
+    return context
 
-    return {
-        "field": field,
-        "attrs": widget_context["widget"],
-    }
+
+@register.simple_tag
+def render_attrs(widget, class_name=None):
+    rendered_attrs = []
+    for attr, value in widget['attrs'].items():
+        rendered_attrs.append((attr, value))
+    if not widget["attrs"].get("class") and class_name:
+        rendered_attrs.append(("class", class_name))
+    return format_html_join(" ", '{}="{}"', rendered_attrs)
+
+
+@register.simple_tag
+def render_bool_attrs(widget):
+    attrs_html = []
+    for attr, value in widget.items():
+        if value is True:
+            attrs_html.append(attr)
+    return " ".join(attrs_html)
+
+
+@register.filter
+def is_radio_select_field(field):
+    return isinstance(field.field.widget, forms.RadioSelect)
 
 
 @register.filter
@@ -28,8 +52,21 @@ def is_select_field(field):
 
 
 @register.filter
-def extract_choices_from_attrs(attrs):
-    """Filter that extracts field choices into easily iterable list"""
-    if attrs.get("optgroups"):
-        return [i[1][0] for i in attrs["optgroups"]]
-    return []
+def is_multiple_choice_field(field):
+    multichoice_widgets = (forms.CheckboxSelectMultiple, forms.SelectMultiple)
+    return isinstance(field.field.widget, multichoice_widgets)
+
+
+@register.filter
+def is_textarea(field):
+    return isinstance(field.field.widget, forms.Textarea)
+
+
+@register.filter
+def get_options(field):
+    """Filter that extracts field choices into an easily iterable list"""
+    widget = field.field.widget
+    attrs = dict(id=field.auto_id, **widget.attrs)
+    context = widget.get_context(field.html_name, field.value(), attrs)
+    widget_context = context["widget"]
+    return widget.options(widget_context["name"], widget_context["value"], attrs)
