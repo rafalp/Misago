5f1ebd3881c62ae538d1b51dc9387621a17e8134
rafalp <kontakt@rpiton.com>
2018-11-17 02:51:48 +0100

    Simplify tests assertions

--- misago/admin/tests/test_admin_form_templatetags.py
+++ misago/admin/tests/test_admin_form_templatetags.py
@@ -4,68 +4,37 @@ from django.test import TestCase
 
 
 class TestForm(forms.Form):
-    somefield = forms.CharField(label="Hello!", max_length=255)
+    text_field = forms.CharField(label="Hello!", max_length=255)
+
+
+def render(template_str):
+    base_template = "{%% load misago_admin_form %%} %s"
+    context = Context({'form': TestForm()})
+    template = Template(base_template % template_str)
+    return template.render(context).strip()
 
 
 class FormRowTests(TestCase):
-    def setUp(self):
-        self.context = Context({'form': TestForm()})
-
     def test_tag_renders_row_with_field(self):
-        tpl_content = """
-{% load misago_admin_form %}
-
-{% form_row form.somefield %}
-"""
-
-        tpl = Template(tpl_content)
-        render = tpl.render(self.context).strip()
-        self.assertIn('id_somefield', render)
+        html = render("{% form_row form.text_field %}")
+        self.assertIn('id_text_field', html)
 
     def test_tag_with_label_class_option_renders_row_including_css_class(self):
-        tpl_content = """
-{% load misago_admin_form %}
-
-{% form_row form.somefield label_class="col-md-3" %}
-"""
-
-        tpl = Template(tpl_content)
-        render = tpl.render(self.context).strip()
-        self.assertIn('id_somefield', render)
-        self.assertIn('col-md-3', render)
+        html = render('{% form_row form.text_field label_class="col-md-3" %}')
+        self.assertIn('id_text_field', html)
+        self.assertIn('col-md-3', html)
 
     def test_tag_with_field_class_option_renders_row_including_css_class(self):
-        tpl_content = """
-{% load misago_admin_form %}
-
-{% form_row form.somefield field_class="col-md-9" %}
-"""
-
-        tpl = Template(tpl_content)
-        render = tpl.render(self.context).strip()
-        self.assertIn('id_somefield', render)
-        self.assertIn('col-md-9', render)
+        html = render('{% form_row form.text_field field_class="col-md-9" %}')
+        self.assertIn('id_text_field', html)
+        self.assertIn('col-md-9', html)
 
     def test_tag_with_label_and_control_options_renders_row_including_both_css_classes(self):
-        tpl_content = """
-{% load misago_admin_form %}
-
-{% form_row form.somefield "col-md-3" "col-md-9" %}
-"""
-
-        tpl = Template(tpl_content)
-        render = tpl.render(self.context).strip()
-
-        self.assertIn('id_somefield', render)
-        self.assertIn('col-md-3', render)
-        self.assertIn('col-md-9', render)
+        html = render('{% form_row form.text_field "col-md-3" "col-md-9" %}')
+        self.assertIn('id_text_field', html)
+        self.assertIn('col-md-3', html)
+        self.assertIn('col-md-9', html)
 
     def test_tag_without_field_raises_exception(self):
-        tpl_content = """
-{% load misago_admin_form %}
-
-{% form_row %}
-"""
-
         with self.assertRaises(TemplateSyntaxError):
-            Template(tpl_content)
+            render('{% form_row %}')
