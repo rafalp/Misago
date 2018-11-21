ca31054ab644b6da3d58acbe3043a07194367d79
Luis Esteban Rodriguez <rodriguezjluis0@gmail.com>
2018-11-21 15:47:04 -0600

    fix code

--- misago/users/models/deleteduser.py
+++ misago/users/models/deleteduser.py
@@ -1,20 +1,26 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 
-DELETED_BY = (
-    (1, 'By User'),
-    (2, 'By Staff'),
-    (3, 'By System'),
+DELETED_BY_USER = 1
+DELETED_BY_STAFF = 2
+DELETED_BY_SYSTEM = 3
+
+DELETED_BY_CHOICES = (
+    (DELETED_BY_USER, _('By User')),
+    (DELETED_BY_STAFF, _('By Staff')),
+    (DELETED_BY_SYSTEM, _('By System')),
 )
 
 
 class DeletedUser(models.Model):
     deleted_on = models.DateTimeField(default=timezone.now)
     deleted_by = models.PositiveIntegerField(
-        choices=DELETED_BY,
+        choices=DELETED_BY_CHOICES,
         default=1
     )
-    
+
     class Meta:
-        get_latest_by = "deleted_on"
+        ordering = ['deleted_on']
+        db_index = True
--- misago/users/utils.py
+++ misago/users/utils.py
@@ -1,15 +1,30 @@
+import hashlib
+
 from django.utils import timezone
 
 from misago.users.models.deleteduser import DeletedUser
-
-import hashlib
 
 
 def hash_email(email):
     return hashlib.md5(email.lower().encode('utf-8')).hexdigest()
 
-def create_user_deletion(deleted_by, deleted_on=timezone.now()):
+
+def record_user_deleted_by_self():
     return DeletedUser.objects.create(
-        deleted_by=deleted_by,
-        deleted_on=deleted_on
+        deleted_by=1,
+        deleted_on=timezone.now()
+    )
+
+
+def record_user_deleted_by_staff():
+    return DeletedUser.objects.create(
+        deleted_by=2,
+        deleted_on=timezone.now()
+    )
+
+
+def record_user_deleted_by_system():
+    return DeletedUser.objects.create(
+        deleted_by=3,
+        deleted_on=timezone.now()
     )
