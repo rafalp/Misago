c919604d4ec907bac64c2f1a29831d031c89ecf7
rafalp <kontakt@rpiton.com>
2018-11-27 20:46:32 +0100

    Add tests for misago.cache.cache

--- misago/cache/cache.py
+++ misago/cache/cache.py
@@ -23,14 +23,14 @@ def get_cache_versions_from_db():
     return {i.cache: i.version for i in queryset}
 
 
-def invalidate(cache):
+def invalidate_cache(cache):
     CacheVersion.objects.filter(cache=cache).update(
         version=generate_version_string(),
     )
     cache.delete(CACHE_NAME)
 
 
-def invalidate_all():
+def invalidate_all_caches():
     for cache in get_cache_versions_from_db().keys():
         CacheVersion.objects.filter(cache=cache).update(
             version=generate_version_string(),
