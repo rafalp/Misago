593a0fd1e0cede696febe90caa329fc3d1c58545
rafalp <kontakt@rpiton.com>
2018-12-17 02:50:49 +0100

    Fix allow_lift_ban permission

--- misago/users/permissions/moderation.py
+++ misago/users/permissions/moderation.py
@@ -170,8 +170,7 @@ can_ban_user = return_boolean(allow_ban_user)
 def allow_lift_ban(user_acl, target):
     if not user_acl['can_lift_bans']:
         raise PermissionDenied(_("You can't lift bans."))
-    # FIXME: this will require cache version delegation
-    ban = get_user_ban(target, user_ac["cache_versions"])
+    ban = get_user_ban(target, user_acl["cache_versions"])
     if not ban:
         raise PermissionDenied(_("This user is not banned."))
     if user_acl['max_lifted_ban_length']:
