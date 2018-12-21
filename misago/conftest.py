f6c4dcd01c396267290306bdef363d5be3f530fb
rafalp <kontakt@rpiton.com>
2018-12-21 18:28:39 +0100

    Move bunch of misago.acl tests to pytest

--- misago/conftest.py
+++ misago/conftest.py
@@ -1,6 +1,11 @@
+import pytest
+
 from misago.acl import ACL_CACHE
 from misago.conf import SETTINGS_CACHE
 from misago.users.constants import BANS_CACHE
+from misago.users.models import AnonymousUser
+from misago.users.testutils import create_test_superuser, create_test_user
+
 
 def get_cache_versions():
     return {
@@ -8,3 +13,36 @@ def get_cache_versions():
         BANS_CACHE: "abcdefgh",
         SETTINGS_CACHE: "abcdefgh",
     }
+
+
+@pytest.fixture
+def cache_versions():
+    return get_cache_versions()
+
+
+@pytest.fixture
+def user_password():
+    return "password"
+
+
+@pytest.fixture
+def anonymous_user():
+    return AnonymousUser()
+
+
+@pytest.fixture
+def user(db, user_password):
+    return create_test_user("User", "user@example.com", user_password)
+
+
+@pytest.fixture
+def staffuser(db, user_password):
+    user = create_test_superuser("Staffuser", "staffuser@example.com", user_password)
+    user.is_superuser = False
+    user.save()
+    return user
+
+
+@pytest.fixture
+def superuser(db, user_password):
+    return create_test_superuser("Superuser", "superuser@example.com", user_password)
