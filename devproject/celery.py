64d327bd955eb0c8b6818ec7d8ce7a1503aba291
Ali <ali.alsabbah08@gmail.com>
2018-12-31 11:27:38 -0800

    Changed 'Misago' to 'devproject' in celery.py

--- devproject/celery.py
+++ devproject/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
-app = Celery('Misago')
+app = Celery('devproject')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
