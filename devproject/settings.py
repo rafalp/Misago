a430d94310f8f291ca8d34056353fb4b3697194b
rafalp <kontakt@rpiton.com>
2019-01-03 00:02:15 +0100

    Add Celery section to settings.py

--- devproject/settings.py
+++ devproject/settings.py
@@ -319,6 +319,20 @@ REST_FRAMEWORK = {
 }
 
 
+# Celery - Distributed Task Queue
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html
+
+# Configure Celery to use Redis as message broker.
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+
+# Celery workers may leak the memory, eventually depriving the instance of resources.
+# This setting forces celery to stop worker, clean after it and create new one
+# after worker has processed 10 tasks.
+
+CELERY_WORKER_MAX_TASKS_PER_CHILD = 10
+
+
 # Misago specific settings
 # https://misago.readthedocs.io/en/latest/developers/settings.html
 
@@ -416,14 +430,6 @@ MISAGO_PROFILE_FIELDS = [
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-CELERY_BROKER_URL = "redis://redis:6379/0"
-
-# Celery workers may leak the memory, eventually depriving the instance of free
-# resources
-# This setting forces celery to stop worker, clean after it and create new one
-# after worker has processed 10 tasks.
-
-CELERY_WORKER_MAX_TASKS_PER_CHILD = 10
 
 # Display debug toolbar if IN_MISAGO_DOCKER enviroment var is set to "1"
 
