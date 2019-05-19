27f4ef73bf79ac2ea6fbd6c028d71eee8a227a1e
rafalp <kontakt@rpiton.com>
2019-05-19 04:59:25 +0200

    Remove unused field_extra

--- misago/conf/tests/conftest.py
+++ misago/conf/tests/conftest.py
@@ -6,26 +6,24 @@ from ..models import Setting
 @pytest.fixture
 def lazy_setting(db):
     return Setting.objects.create(
-        setting="lazy_setting", dry_value="Hello", is_lazy=True, field_extra={}
+        setting="lazy_setting", dry_value="Hello", is_lazy=True
     )
 
 
 @pytest.fixture
 def lazy_setting_without_value(db):
-    return Setting.objects.create(
-        setting="lazy_setting", dry_value="", is_lazy=True, field_extra={}
-    )
+    return Setting.objects.create(setting="lazy_setting", dry_value="", is_lazy=True)
 
 
 @pytest.fixture
 def private_setting(db):
     return Setting.objects.create(
-        setting="private_setting", dry_value="Hello", is_public=False, field_extra={}
+        setting="private_setting", dry_value="Hello", is_public=False
     )
 
 
 @pytest.fixture
 def public_setting(db):
     return Setting.objects.create(
-        setting="public_setting", dry_value="Hello", is_public=True, field_extra={}
+        setting="public_setting", dry_value="Hello", is_public=True
     )
