1218f2b8726e3d9fca23c37d0973b4d224a1b22a
rafalp <kontakt@rpiton.com>
2019-05-24 23:53:36 +0200

    Remove MISAGO_ADDRESS from codebase

--- devproject/settings.py
+++ devproject/settings.py
@@ -342,14 +342,6 @@ CELERY_WORKER_MAX_TASKS_PER_CHILD = 10
 # Misago specific settings
 # https://misago.readthedocs.io/en/latest/developers/settings.html
 
-# Complete HTTP address to your Misago site homepage. Misago relies on this address to create
-# links in e-mails that are sent to site users.
-# On Misago admin panel home page you will find a message telling you if you have entered the
-# correct value, or what value is correct in case you've didn't.
-
-MISAGO_ADDRESS = "http://my-misago-site.com/"
-
-
 # On dev instance, generate only three sizes of avatars instead of default 6 sizes.
 
 MISAGO_AVATARS_SIZES = [400, 200, 100]
