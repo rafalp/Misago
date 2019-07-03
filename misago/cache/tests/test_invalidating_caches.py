b0bf5eced6b09afd660c35980e510db8ef334b3a
Rafał Pitoń <kontakt@rpiton.com>
2019-07-03 21:58:20 +0200

    Dont cache cache versions (#1253)

--- misago/cache/tests/test_invalidating_caches.py
+++ misago/cache/tests/test_invalidating_caches.py
@@ -1,35 +1,14 @@
-import pytest
-
 from ..models import CacheVersion
-from ..versions import CACHE_NAME, invalidate_all_caches, invalidate_cache
+from ..versions import invalidate_all_caches, invalidate_cache
 
 
-@pytest.fixture
-def cache_delete(mocker):
-    return mocker.patch("django.core.cache.cache.delete")
-
-
-def test_invalidating_cache_updates_cache_version_in_database(
-    cache_delete, cache_version
-):
+def test_invalidating_cache_updates_cache_version_in_database(cache_version):
     invalidate_cache(cache_version.cache)
     updated_cache_version = CacheVersion.objects.get(cache=cache_version.cache)
     assert cache_version.version != updated_cache_version.version
 
 
-def test_invalidating_cache_deletes_versions_cache(cache_delete, cache_version):
-    invalidate_cache(cache_version.cache)
-    cache_delete.assert_called_once_with(CACHE_NAME)
-
-
-def test_invalidating_all_caches_updates_cache_version_in_database(
-    cache_delete, cache_version
-):
+def test_invalidating_all_caches_updates_cache_version_in_database(cache_version):
     invalidate_all_caches()
     updated_cache_version = CacheVersion.objects.get(cache=cache_version.cache)
     assert cache_version.version != updated_cache_version.version
-
-
-def test_invalidating_all_caches_deletes_versions_cache(cache_delete, cache_version):
-    invalidate_all_caches()
-    cache_delete.assert_called_once_with(CACHE_NAME)
