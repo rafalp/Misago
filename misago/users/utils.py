3b6a08fd32761766b6bf04a6c9281c56477f8a85
Luis Esteban <rodriguezjluis0@gmail.com>
2019-07-30 19:31:16 -0500

    fix spaces

--- misago/users/utils.py
+++ misago/users/utils.py
@@ -8,14 +8,11 @@ from misago.users.models.deleteduser import DeletedUser
 def hash_email(email):
     return hashlib.md5(email.lower().encode('utf-8')).hexdigest()
 
-
 def record_user_deleted_by_self():
     return DeletedUser.objects.create(deleted_by=DeletedUser.DELETED_BY_USER)
 
-
 def record_user_deleted_by_staff():
     return DeletedUser.objects.create(deleted_by=DeletedUser.DELETED_BY_STAFF)
-
 
 def record_user_deleted_by_system():
     return DeletedUser.objects.create(deleted_by=DeletedUser.DELETED_BY_SYSTEM)
