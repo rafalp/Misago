6600096ce457bb1834be8e581181506ea78885bb
rafalp <kontakt@rpiton.com>
2019-08-26 00:21:00 +0200

    Add SSO api for user sync

---
+++ misago/sso/tests/utils.py
@@ -0,0 +1,6 @@
+TEST_SSO_SETTINGS = {
+    "enable_sso": True,
+    "sso_private_key": "priv1",
+    "sso_public_key": "fakeSsoPublicKey",
+    "sso_url": "http://example.com/server/",
+}
--- misago/sso/urls.py
+++ misago/sso/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
+from .api import sync_user
 from .client import MisagoAuthenticateView, MisagoLoginView
 
 urlpatterns = [
+    url(r"^sync/$", sync_user, name="simple-sso-user-sync"),
     url(r"^client/$", MisagoLoginView.as_view(), name="simple-sso-login"),
     url(
         r"^client/authenticate/$",
