44af571ead2bad21e1b38aaf4b61155e84fcdf2f
rafalp <kontakt@rpiton.com>
2019-11-03 17:50:21 +0100

    Fix mypy types and add py.typed file to misago

--- misago/cli/tests/conftest.py
+++ misago/cli/tests/conftest.py
@@ -1,5 +1,3 @@
-from typing import List
-
 import pytest
 
 from click.testing import CliRunner, Result
@@ -9,7 +7,7 @@ from .. import cli
 
 @pytest.fixture
 def invoke_cli():
-    def invoke(*args: List[str]) -> Result:
+    def invoke(*args: str) -> Result:
         runner = CliRunner()
         return runner.invoke(cli, args)
 
