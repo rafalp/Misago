77cabdc40aa4a978aee9b9cec3a19368aaa6b5f2
rafalp <kontakt@rpiton.com>
2019-11-23 17:55:14 +0100

    Add tests for graphql context

--- misago/graphql/context.py
+++ misago/graphql/context.py
@@ -9,13 +9,12 @@ async def get_graphql_context(
     request: Request, context: GraphQLContext
 ) -> GraphQLContext:
     cache_versions = await get_cache_versions()
-    settings = await get_dynamic_settings(cache_versions)
 
     context.update(
         {
             "cache_versions": cache_versions,
             "request": request,
-            "settings": settings.items(),
+            "settings": await get_dynamic_settings(cache_versions),
         }
     )
 
