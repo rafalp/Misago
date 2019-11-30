67d31bae747478791eacacc5f0bf2ab1108c9e23
rafalp <kontakt@rpiton.com>
2019-11-30 23:37:58 +0100

    Rename mutation error to validation error

--- misago/graphql/types/mutationerror.py
+++
@@ -1,7 +0,0 @@
-from ariadne import ObjectType
-
-
-mutation_error_type = ObjectType("MutationError")
-
-mutation_error_type.set_alias("location", "loc")
-mutation_error_type.set_alias("message", "msg")
