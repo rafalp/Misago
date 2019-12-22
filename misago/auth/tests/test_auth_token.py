f20972b72bfd98a85af14a3f2d573f9bed2ddcbb
rafalp <kontakt@rpiton.com>
2019-12-22 19:38:51 +0100

    Fix tests

--- misago/auth/tests/test_auth_token.py
+++ misago/auth/tests/test_auth_token.py
@@ -64,5 +64,5 @@ async def test_no_user_is_returned_for_invalid_token(graphql_context):
 @pytest.mark.asyncio
 async def test_deleted_user_is_not_obtained_from_token(graphql_context, user):
     token = await create_user_token(graphql_context, user)
-    await delete_user(user.id)
+    await delete_user(user)
     assert await get_user_from_token(graphql_context, token) is None
--- misago/graphql/mutations/tests/test_deleting_thread.py
+++ misago/graphql/mutations/tests/test_deleting_thread.py
@@ -12,18 +12,17 @@ async def test_delete_thread_mutation_deletes_thread(moderator_graphql_info, thr
     )
 
     assert not data.get("errors")
-    assert await get_thread_by_id(data["thread"].id) is None
+    assert await get_thread_by_id(thread.id) is None
 
 
 @pytest.mark.asyncio
 async def test_delete_thread_mutation_fails_if_user_is_not_authorized(
-    graphql_info, thread, sibling_category
+    graphql_info, thread
 ):
     data = await resolve_delete_thread(
         None, graphql_info, input={"thread": str(thread.id)},
     )
 
-    assert data.get("thread")
     assert data.get("errors")
     assert data["errors"].get_errors_locations() == ["thread", ErrorsList.ROOT_LOCATION]
     assert data["errors"].get_errors_types() == [
@@ -34,13 +33,12 @@ async def test_delete_thread_mutation_fails_if_user_is_not_authorized(
 
 @pytest.mark.asyncio
 async def test_delete_thread_mutation_fails_if_user_is_not_moderator(
-    user_graphql_info, thread, sibling_category
+    user_graphql_info, thread
 ):
     data = await resolve_delete_thread(
         None, user_graphql_info, input={"thread": str(thread.id)},
     )
 
-    assert data.get("thread")
     assert data.get("errors")
     assert data["errors"].get_errors_locations() == ["thread"]
     assert data["errors"].get_errors_types() == [
@@ -50,13 +48,12 @@ async def test_delete_thread_mutation_fails_if_user_is_not_moderator(
 
 @pytest.mark.asyncio
 async def test_delete_thread_mutation_fails_if_thread_id_is_invalid(
-    moderator_graphql_info, sibling_category
+    moderator_graphql_info,
 ):
     data = await resolve_delete_thread(
         None, moderator_graphql_info, input={"thread": "invalid"},
     )
 
-    assert not data.get("thread")
     assert data.get("errors")
     assert data["errors"].get_errors_locations() == ["thread"]
     assert data["errors"].get_errors_types() == ["type_error.integer"]
@@ -64,13 +61,12 @@ async def test_delete_thread_mutation_fails_if_thread_id_is_invalid(
 
 @pytest.mark.asyncio
 async def test_delete_thread_mutation_fails_if_thread_doesnt_exist(
-    moderator_graphql_info, sibling_category
+    moderator_graphql_info,
 ):
     data = await resolve_delete_thread(
         None, moderator_graphql_info, input={"thread": "4000"},
     )
 
-    assert not data.get("thread")
     assert data.get("errors")
     assert data["errors"].get_errors_locations() == ["thread"]
     assert data["errors"].get_errors_types() == ["value_error.thread_does_not_exist"]
