c2b8505bca22c476b3e962362de5487f18703f5a
rafalp <kontakt@rpiton.com>
2020-03-13 00:40:32 +0100

    Add test for multiple fake users

--- plugins/datafaker/tests/test_user_creation.py
+++ plugins/datafaker/tests/test_user_creation.py
@@ -6,3 +6,9 @@ from ..users import create_fake_user
 @pytest.mark.asyncio
 async def test_fake_user_is_created(db, faker):
     assert await create_fake_user(faker)
+
+
+@pytest.mark.asyncio
+async def test_multiple_fake_users_are_created(db, faker):
+    for _ in range(5):
+        assert await create_fake_user(faker)
