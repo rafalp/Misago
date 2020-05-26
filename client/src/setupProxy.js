bc3afac1f2ccbe1811ee01b08997cb51f3df3f4f
rafalp <kontakt@rpiton.com>
2020-05-26 21:16:28 +0200

    Fix websockets in proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,16 +3,23 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     '/sockjs-node',
-    createProxyMiddleware({
-      target: 'ws://localhost:3000',
-    })
+    createProxyMiddleware(
+      '/sockjs-node',
+      {
+        target: 'ws://localhost:3000',
+        ws: true,
+      }
+    )
   );
   app.use(
     '/graphql',
-    createProxyMiddleware({
-      target: 'http://localhost:8000',
-      changeOrigin: true,
-      ws: true,
-    })
+    createProxyMiddleware(
+      '/graphql',
+      {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        ws: true,
+      }
+    )
   );
 };
