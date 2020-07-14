9c3d58d2f320f820d8537a6fe45ff71b27f74e60
rinter17 <rinter17@fastmail.com>
2020-07-14 12:49:58 -0500

    Replace chunk_queryset() util with server-side cursor #1230 only(for now) in file :
    Currently Misago uses chunk_queryset utility that iterates on queryset in chunks of specified length, but this task can be easily replaced with queryset.iterator() which on PostgreSQL should use chunking in database.

--- misago/core/tests/test_chunk_queryset.py
+++ misago/core/tests/test_chunk_queryset.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from ...cache.models import CacheVersion
 
+
 class ChunkQuerysetTest(TestCase):
     def setUp(self):
         # clear table
         CacheVersion.objects.all().delete()
-        
+
         items_pks = []
         for i in range(50):
             obj = CacheVersion.objects.create(cache="test%s" % i)
@@ -20,10 +21,10 @@ class ChunkQuerysetTest(TestCase):
             queryset = CacheVersion.objects.order_by("cache")
             for obj in queryset.iterator(chunk_size=5):
                 chunked_pks.append(obj.pk)
-                sorted_chunked_pks = list(sorted(chunked_pks, reverse=True))
+            sorted_chunked_pks = list(sorted(chunked_pks, reverse=True))
 
         self.assertEqual(sorted_chunked_pks, self.items_pks)
-    
+
     def test_chunk_shrinking_queryset(self):
         """chunk_queryset utility chunks queryset in delete action"""
         with self.assertNumQueries(51):
