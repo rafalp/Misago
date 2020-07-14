67527f4e7da94e658f8753cc39f75d84cb293967
rinter17 <rinter17@fastmail.com>
2020-07-14 09:30:17 -0500

    Replace chunk_queryset() util with server-side cursor #1230 only(for now) in misago/core/tests/test_chunk_queryset.py

--- misago/core/tests/test_chunk_queryset.py
+++ misago/core/tests/test_chunk_queryset.py
@@ -1,14 +1,13 @@
 from django.test import TestCase
 
 from ...cache.models import CacheVersion
-from ..pgutils import chunk_queryset
 
 
 class ChunkQuerysetTest(TestCase):
     def setUp(self):
         # clear table
         CacheVersion.objects.all().delete()
-
+        
         items_pks = []
         for i in range(50):
             obj = CacheVersion.objects.create(cache="test%s" % i)
@@ -19,18 +18,19 @@ class ChunkQuerysetTest(TestCase):
         """chunk_queryset utility chunks queryset but returns all items"""
         chunked_pks = []
 
-        with self.assertNumQueries(11):
+        with self.assertNumQueries(1):
             queryset = CacheVersion.objects.order_by("cache")
-            for obj in chunk_queryset(queryset, chunk_size=5):
+            for obj in queryset.iterator(chunk_size=5):
                 chunked_pks.append(obj.pk)
+                sorted_chunked_pks = list(sorted(chunked_pks, reverse=True))
 
-        self.assertEqual(chunked_pks, self.items_pks)
-
+        self.assertEqual(sorted_chunked_pks, self.items_pks)
+    
     def test_chunk_shrinking_queryset(self):
         """chunk_queryset utility chunks queryset in delete action"""
-        with self.assertNumQueries(61):
+        with self.assertNumQueries(51):
             queryset = CacheVersion.objects.all()
-            for obj in chunk_queryset(queryset, chunk_size=5):
+            for obj in queryset.iterator(chunk_size=5):
                 obj.delete()
 
         self.assertEqual(CacheVersion.objects.count(), 0)
