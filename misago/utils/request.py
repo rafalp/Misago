48f4ebfbe8162d5fe5f16216dabd6157866e1862
rafalp <kontakt@rpiton.com>
2020-08-23 17:55:16 +0200

    Fix double slash in base url

--- misago/utils/request.py
+++ misago/utils/request.py
@@ -4,7 +4,7 @@ from starlette.requests import Request
 
 
 def get_absolute_url(request: Request, url: Optional[str] = None) -> str:
-    base_url = request.base_url
+    base_url = str(request.base_url).rstrip("/")
     if url:
         return f"{base_url}{url}"
-    return str(base_url)
+    return base_url
