e5755c137e9ea269f5d980afa7cd570e58cf9b71
rafalp <kontakt@rpiton.com>
2020-08-29 22:13:00 +0200

    Remove iconSolid from moderation action

--- client/src/Routes/Thread/Thread.types.ts
+++ client/src/Routes/Thread/Thread.types.ts
@@ -43,7 +43,6 @@ export interface IThreadModeration {
 export interface IModerationAction {
   name: React.ReactNode
   icon: string
-  iconSolid?: boolean
   disabled?: boolean
   action: () => Promise<void> | void
 }
