177418f1ef5c0716173dbcb286f9092ba1128a40
rafalp <kontakt@rpiton.com>
2020-11-29 23:49:26 +0100

    First pass on addition controls to editor

--- client/src/Editor/EditorButton.tsx
+++ client/src/Editor/EditorButton.tsx
@@ -1,18 +1,32 @@
+import classnames from "classnames"
 import React from "react"
 
 interface IEditorButtonProps {
   children: React.ReactNode
+  className?: string
   disabled?: boolean
+  icon?: boolean
+  title?: string
   onClick?: () => void
 }
 
 const EditorButton: React.FC<IEditorButtonProps> = ({
   children,
+  className,
   disabled,
+  icon,
+  title,
   onClick,
 }) => (
   <button
-    className="btn btn-outline-secondary btn-editor btn-sm"
+    className={classnames(
+      "btn btn-outline-secondary btn-editor btn-sm",
+      className,
+      {
+        "btn-icon": icon,
+      }
+    )}
+    title={title}
     type="button"
     disabled={disabled}
     onClick={onClick}
