82b551aaecaecfe47efd10501dd216e08011c2b7
rafalp <kontakt@rpiton.com>
2021-01-16 23:29:24 +0100

    Format urls.ts

--- client/src/urls.ts
+++ client/src/urls.ts
@@ -10,30 +10,38 @@ interface ThreadParams extends Sluggable {
 }
 
 export const categories = () => `/categories/`
+
 export const category = (category: Sluggable) => {
   return `/c/${category.slug}/${category.id}/`
 }
+
 export const post = (post: { id: string | number }) => {
   return `/post/${post.id}/`
 }
+
 export const postThread = (category?: Sluggable | null) => {
   if (category) return `/c/${category.slug}/${category.id}/post-thread/`
   return `/post-thread/`
 }
+
 export const threads = () => `/threads/`
+
 export const thread = (thread: ThreadParams) => {
   if (thread.page && thread.page > 1) {
     return `/t/${thread.slug}/${thread.id}/${thread.page}/`
   }
   return `/t/${thread.slug}/${thread.id}/`
 }
+
 export const threadLastPost = (thread: Sluggable) => {
   return `/t/${thread.slug}/${thread.id}/last/`
 }
+
 export const threadPost = (
   thread: Sluggable,
   post: { id: string | number }
 ) => {
   return `/t/${thread.slug}/${thread.id}/post/${post.id}/`
 }
+
 export const user = (user: Sluggable) => `/u/${user.slug}/${user.id}/`
