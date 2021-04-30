5021617543b4e2875edce5074839d8dd915eb0fd
rafalp <kontakt@rpiton.com>
2021-04-30 15:00:54 +0200

    Move categories to models

--- misago/users/models.py
+++ misago/users/models.py
@@ -2,9 +2,18 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
+from ..database import Model, register_model
+from ..passwords import hash_password
+from ..tables import users
+from ..graphql import GraphQLContext
+from ..utils import timezone
+from ..utils.strings import slugify
+from .email import get_email_hash, normalize_email
 
+
+@register_model("User", users)
 @dataclass
-class User:
+class User(Model):
     id: int
     name: str
     slug: str
@@ -17,3 +26,44 @@ class User:
     is_administrator: bool
     joined_at: datetime
     extra: dict
+
+    @classmethod
+    async def create(
+        cls,
+        name: str,
+        email: str,
+        *,
+        full_name: Optional[str] = None,
+        password: Optional[str] = None,
+        is_active: bool = True,
+        is_moderator: bool = False,
+        is_administrator: bool = False,
+        joined_at: Optional[datetime] = None,
+        extra: Optional[Dict[str, Any]] = None,
+        context: Optional[GraphQLContext] = None,
+    ) -> "User":
+        password_hash = None
+        if password:
+            password_hash = await hash_password(password)
+
+        data: Dict[str, Any] = {
+            "name": name,
+            "slug": slugify(name),
+            "email": normalize_email(email),
+            "email_hash": get_email_hash(email),
+            "full_name": full_name,
+            "password": password_hash,
+            "is_active": is_active,
+            "is_moderator": is_moderator,
+            "is_administrator": is_administrator,
+            "joined_at": joined_at or timezone.now(),
+            "extra": extra or {},
+        }
+
+        return await cls.query.insert(**data)
+
+    async def update() -> "User":
+        pass
+
+    def delete(self):
+        return User.query.filter(id=self.id).delete()
--- misago/users/tests/test_user_creation.py
+++ misago/users/tests/test_user_creation.py
@@ -2,69 +2,69 @@ import pytest
 
 from ...passwords import verify_password
 from ...utils import timezone
-from ..create import create_user
+from ..models import User
 from ..get import get_user_by_id
 
 
 @pytest.mark.asyncio
 async def test_user_is_created_in_db(db):
-    user = await create_user("test", "test@example.com")
+    user = await User.create("test", "test@example.com")
     assert user.id
     assert user == await get_user_by_id(user.id)
 
 
 @pytest.mark.asyncio
 async def test_user_is_created_with_slug(db):
-    user = await create_user("TeST", "test@example.com")
+    user = await User.create("TeST", "test@example.com")
     assert user.slug == "test"
 
 
 @pytest.mark.asyncio
 async def test_user_is_created_with_default_join_datetime(db, user_password):
-    user = await create_user("test", "test@example.com")
+    user = await User.create("test", "test@example.com")
     assert user.joined_at
 
 
 @pytest.mark.asyncio
 async def test_user_is_created_with_specified_join_datetime(db, user_password):
     joined_at = timezone.now()
-    user = await create_user("test", "test@example.com", joined_at=joined_at)
+    user = await User.create("test", "test@example.com", joined_at=joined_at)
     assert user.joined_at == joined_at
 
 
 @pytest.mark.asyncio
 async def test_user_is_created_with_useable_password(db, user_password):
-    user = await create_user("test", "test@example.com", password=user_password)
+    user = await User.create("test", "test@example.com", password=user_password)
     assert user.id
     assert await verify_password(user_password, user.password)
 
 
 @pytest.mark.asyncio
 async def test_user_is_active_by_default(db, user_password):
-    user = await create_user("test", "test@example.com")
+    user = await User.create("test", "test@example.com")
     assert user.is_active
 
 
 @pytest.mark.asyncio
 async def test_user_is_created_with_inactive_status(db, user_password):
-    user = await create_user("test", "test@example.com", is_active=False)
+    user = await User.create("test", "test@example.com", is_active=False)
     assert not user.is_active
 
 
 @pytest.mark.asyncio
 async def test_user_team_statuses_default_to_false(db, user_password):
-    user = await create_user("test", "test@example.com")
+    user = await User.create("test", "test@example.com")
     assert not user.is_moderator
     assert not user.is_administrator
 
 
 @pytest.mark.asyncio
 async def test_user_is_created_with_moderator_status(db, user_password):
-    user = await create_user("test", "test@example.com", is_moderator=True)
+    user = await User.create("test", "test@example.com", is_moderator=True)
     assert user.is_moderator
 
 
 @pytest.mark.asyncio
 async def test_user_is_created_with_admin_status(db, user_password):
-    user = await create_user("test", "test@example.com", is_administrator=True)
+    user = await User.create("test", "test@example.com", is_administrator=True)
     assert user.is_administrator
