2821df46b92d59828d82cf59a265572d9627a418
rafalp <kontakt@rpiton.com>
2021-07-18 23:16:05 +0200

    Small cleanup in object mapper

--- misago/users/search.py
+++ misago/users/search.py
@@ -1,10 +1,8 @@
 from typing import List
 
-from sqlalchemy import asc, or_
+from sqlalchemy import or_
 
-from ..database import database
 from ..database.queries import istartswith
-from ..tables import users
 from .models import User
 
 RESULTS_MAX_LIMIT = 50
@@ -17,20 +15,17 @@ async def search_users(search: str, *, limit: int = 10) -> List[User]:
     if safe_limit < 1:
         return []
 
-    query = (
-        users.select(None)
-        .where(
+    results = (
+        await User.query.filter(
             or_(
-                users.c.slug.startswith(search_lowercased),
-                istartswith(users.c.full_name, search),
+                User.table.c.slug.startswith(search_lowercased),
+                istartswith(User.table.c.full_name, search),
             )
         )
-        .order_by(asc(users.c.slug))
+        .order_by("slug")
         .limit(safe_limit)
+        .all()
     )
-
-    rows = await database.fetch_all(query)
-    results = [User(**row) for row in rows]
 
     # Bring exact matches for full name or slug to front of suggestions
     ordered_results = []
