ad5bcf6a72396ca4aa958a36ceef520431767661
rafalp <kontakt@rpiton.com>
2021-08-02 00:26:43 +0200

    Remove admin_mutation decorator

--- misago/graphql/admin/decorators.py
+++ misago/graphql/admin/decorators.py
@@ -2,7 +2,6 @@ from functools import wraps
 from inspect import isawaitable
 
 from ...auth import get_authenticated_admin, get_authenticated_user
-from ...errors import NotAdminError
 from .errors import AuthenticationGraphQLError, ForbiddenGraphQLError
 
 ERRORS = "errors"
@@ -13,20 +12,6 @@ def admin_query(f):
     async def wrapper(obj, info, *args, **kwargs):
         if not await get_authenticated_admin(info.context):
             return None
-
-        result = f(obj, info, *args, **kwargs)
-        if isawaitable(result):
-            result = await result
-        return result
-
-    return wrapper
-
-
-def admin_mutation(f):
-    @wraps(f)
-    async def wrapper(obj, info, *args, **kwargs):
-        if not await get_authenticated_admin(info.context):
-            raise NotAdminError()
 
         result = f(obj, info, *args, **kwargs)
         if isawaitable(result):
--- misago/graphql/admin/tests/test_admin_resolver_decorator.py
+++ misago/graphql/admin/tests/test_admin_resolver_decorator.py
@@ -1,36 +1,7 @@
 import pytest
 
-from ....errors import NotAdminError
 from ..errors import AuthenticationGraphQLError, ForbiddenGraphQLError
-from ..decorators import admin_mutation, admin_query, admin_resolver
-
-
-@admin_mutation
-def admin_mutation_resolver(*args, **kwargs):
-    pass
-
-
-@pytest.mark.asyncio
-async def test_no_error_is_raised_when_mutation_resolver_is_called_with_admin_auth(
-    admin_graphql_info,
-):
-    await admin_mutation_resolver(None, admin_graphql_info)
-
-
-@pytest.mark.asyncio
-async def test_not_admin_error_is_raised_when_mutation_resolver_is_called_without_admin_auth(
-    user_graphql_info,
-):
-    with pytest.raises(NotAdminError):
-        await admin_mutation_resolver(None, user_graphql_info)
-
-
-@pytest.mark.asyncio
-async def test_not_admin_error_is_raised_when_mutation_resolver_is_called_without_any_auth(
-    graphql_info,
-):
-    with pytest.raises(NotAdminError):
-        await admin_mutation_resolver(None, graphql_info)
+from ..decorators import admin_query, admin_resolver
 
 
 @admin_query
