5ae7a56463cfa2ccea2f244c3fd810299df0e023
rafalp <kontakt@rpiton.com>
2021-12-22 14:48:11 +0100

    Fix test

--- misago/plugins/tests/test_plugin_discovery.py
+++ misago/plugins/tests/test_plugin_discovery.py
@@ -3,5 +3,6 @@ from ..discovery import discover_plugins
 
 def test_datafaker_plugin_is_discovered(plugins_root):
     plugins = discover_plugins(plugins_root)
-    plugins_packages = [plugin.package for plugin in plugins]
+    plugins_packages = [plugin.package_name for plugin in plugins]
+    print(plugins_packages)
     assert "someplugin" in plugins_packages
