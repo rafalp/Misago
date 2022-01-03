88a1e0875dd358225b5d3c5c915468e5b8c4b9b3
rafalp <kontakt@rpiton.com>
2022-01-03 22:42:24 +0100

    Update tests for login mutation

--- misago/graphql/public/mutations/tests/test_login.py
+++ misago/graphql/public/mutations/tests/test_login.py
@@ -1,89 +1,105 @@
+from unittest.mock import ANY
+
 import pytest
 
 from .....auth import get_user_from_token
 from .....errors import ErrorsList
-from ..login import resolve_login
+
+LOGIN_MUTATION = """
+    mutation Login($username: String!, $password: String!) {
+        login(username: $username, password: $password) {
+            user {
+                id
+                name
+            }
+            token
+            errors {
+                location
+                type
+            }
+        }
+    }
+"""
 
 
 @pytest.mark.asyncio
-async def test_login_mutation_returns_user_token_on_success(
-    graphql_info, user, user_password
+async def test_login_mutation_returns_user_and_token_on_success(
+    graphql_info, query_public_api, user, user_password
 ):
-    data = await resolve_login(
-        None,
-        graphql_info,
-        username=user.name,
-        password=user_password,
+    result = await query_public_api(
+        LOGIN_MUTATION,
+        {"username": user.name, "password": user_password},
     )
 
-    assert not data.get("errors")
-    assert data.get("token")
+    assert result["data"]["login"] == {
+        "user": {
+            "id": str(user.id),
+            "name": user.name,
+        },
+        "token": ANY,
+        "errors": None,
+    }
 
-    token_user = await get_user_from_token(graphql_info.context, data["token"])
+    token_user = await get_user_from_token(
+        graphql_info.context, result["data"]["login"]["token"]
+    )
     assert token_user == user
 
 
 @pytest.mark.asyncio
-async def test_login_mutation_returns_user_on_success(
-    graphql_info, user, user_password
-):
-    data = await resolve_login(
-        None,
-        graphql_info,
-        username=user.name,
-        password=user_password,
+async def test_login_mutation_requires_username(query_public_api, user, user_password):
+    result = await query_public_api(
+        LOGIN_MUTATION,
+        {"username": "", "password": user_password},
     )
 
-    assert not data.get("errors")
-    assert data.get("user")
-    assert data["user"] == user
+    assert result["data"]["login"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": [ErrorsList.ROOT_LOCATION],
+                "type": "value_error.all_fields_are_required",
+            },
+        ],
+    }
 
 
 @pytest.mark.asyncio
-async def test_login_mutation_requires_username(graphql_info, user, user_password):
-    data = await resolve_login(
-        None,
-        graphql_info,
-        username="",
-        password=user_password,
+async def test_login_mutation_requires_password(query_public_api, user):
+    result = await query_public_api(
+        LOGIN_MUTATION,
+        {"username": user.name, "password": ""},
     )
 
-    assert not data.get("user")
-    assert not data.get("token")
-    assert data.get("errors")
-    assert data["errors"].get_errors_locations() == [ErrorsList.ROOT_LOCATION]
-    assert data["errors"].get_errors_types() == ["value_error.all_fields_are_required"]
-
-
-@pytest.mark.asyncio
-async def test_login_mutation_requires_password(graphql_info, user, user_password):
-    data = await resolve_login(
-        None,
-        graphql_info,
-        username=user.name,
-        password="",
-    )
-
-    assert not data.get("user")
-    assert not data.get("token")
-    assert data.get("errors")
-    assert data["errors"].get_errors_locations() == [ErrorsList.ROOT_LOCATION]
-    assert data["errors"].get_errors_types() == ["value_error.all_fields_are_required"]
+    assert result["data"]["login"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": [ErrorsList.ROOT_LOCATION],
+                "type": "value_error.all_fields_are_required",
+            },
+        ],
+    }
 
 
 @pytest.mark.asyncio
 async def test_login_mutation_returns_error_on_nonexistent_user_credentials(
-    graphql_info, user
+    query_public_api, db
 ):
-    data = await resolve_login(
-        None,
-        graphql_info,
-        username=user.name,
-        password="invalid",
+    result = await query_public_api(
+        LOGIN_MUTATION,
+        {"username": "John", "password": "secret123"},
     )
 
-    assert not data.get("user")
-    assert not data.get("token")
-    assert data.get("errors")
-    assert data["errors"].get_errors_locations() == [ErrorsList.ROOT_LOCATION]
-    assert data["errors"].get_errors_types() == ["value_error.invalid_credentials"]
+    assert result["data"]["login"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": [ErrorsList.ROOT_LOCATION],
+                "type": "value_error.invalid_credentials",
+            },
+        ],
+    }
