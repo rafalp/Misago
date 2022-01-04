31af332c96275bf8d229bdce5b0dc9fc5266f4e8
rafalp <kontakt@rpiton.com>
2022-01-04 18:06:07 +0100

    Update registration and setup site mutations

--- misago/graphql/public/mutations/tests/test_registration.py
+++ misago/graphql/public/mutations/tests/test_registration.py
@@ -1,206 +1,274 @@
+from unittest.mock import ANY
+
 import pytest
 
 from .....auth import get_user_from_token
 from .....passwords import check_password
 from .....testing import override_dynamic_settings
 from .....users.models import User
-from ..register import resolve_register
+
+REGISTER_MUTATION = """
+    mutation Register($input: RegisterInput!) {
+        register(input: $input) {
+            user {
+                id
+                name
+            }
+            token
+            errors {
+                location
+                type
+            }
+        }
+    }
+"""
 
 
 @pytest.mark.asyncio
-async def test_registration_mutation_creates_new_user_account(graphql_info):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "John",
-            "email": "john@example.com",
-            "password": " password123 ",
+async def test_registration_mutation_creates_new_user_account(
+    query_public_api, graphql_info, db
+):
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": "John",
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert "errors" not in data
-    assert "user" in data
-    assert data["user"] == await User.query.one(id=data["user"].id)
+    data = result["data"]["register"]
 
+    assert data == {
+        "user": {"id": ANY, "name": "John"},
+        "token": ANY,
+        "errors": None,
+    }
 
-@pytest.mark.asyncio
-async def __test_registration_mutation_preserves_spaces_in_user_password(graphql_info):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "John",
-            "email": "john@example.com",
-            "password": " password123 ",
-        },
-    )
+    # Mutation creates user account
+    user_from_db = await User.query.one(id=int(data["user"]["id"]))
+    assert user_from_db.is_active
+    assert not user_from_db.is_moderator
+    assert not user_from_db.is_admin
+    assert await check_password(" password123 ", user_from_db.password)
 
-    assert "user" in data
-
-    user = await User.query.one(id=data["user"].id)
-    assert await check_password(" password123 ", user.password)
+    # Mutation creates user token
+    user_from_token = await get_user_from_token(graphql_info.context, data["token"])
+    assert user_from_token.id == int(data["user"]["id"])
 
 
 @pytest.mark.asyncio
 @override_dynamic_settings(password_min_length=10)
-async def __test_registration_mutation_validates_min_password_length(graphql_info):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "abcd",
-            "email": "john@example.com",
-            "password": "pass",
+async def test_registration_mutation_validates_min_password_length(
+    query_public_api, db
+):
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": "abcd",
+                "email": "john@example.com",
+                "password": "pass",
+            },
         },
     )
 
-    assert "errors" in data
-    assert data["errors"].get_errors_locations() == ["password"]
-    assert data["errors"].get_errors_types() == ["value_error.any_str.min_length"]
+    assert result["data"]["register"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": ["password"],
+                "type": "value_error.any_str.min_length",
+            }
+        ],
+    }
 
 
 @pytest.mark.asyncio
-async def __test_registration_mutation_validates_max_password_length(graphql_info):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "abcd",
-            "email": "john@example.com",
-            "password": "p" * 60,
+async def test_registration_mutation_validates_max_password_length(
+    query_public_api, db
+):
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": "abcd",
+                "email": "john@example.com",
+                "password": "p" * 60,
+            },
         },
     )
 
-    assert "errors" in data
-    assert data["errors"].get_errors_locations() == ["password"]
-    assert data["errors"].get_errors_types() == ["value_error.any_str.max_length"]
+    assert result["data"]["register"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": ["password"],
+                "type": "value_error.any_str.max_length",
+            }
+        ],
+    }
 
 
 @pytest.mark.asyncio
 @override_dynamic_settings(username_min_length=10)
-async def __test_registration_mutation_validates_min_user_name_length(graphql_info):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "abcd",
-            "email": "john@example.com",
-            "password": " password123 ",
+async def test_registration_mutation_validates_min_user_name_length(
+    query_public_api, db
+):
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": "abcd",
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert "errors" in data
-    assert data["errors"].get_errors_locations() == ["name"]
-    assert data["errors"].get_errors_types() == ["value_error.any_str.min_length"]
+    assert result["data"]["register"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": ["name"],
+                "type": "value_error.any_str.min_length",
+            }
+        ],
+    }
 
 
 @pytest.mark.asyncio
 @override_dynamic_settings(username_min_length=1, username_max_length=3)
-async def __test_registration_mutation_validates_max_user_name_length(graphql_info):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "abcd",
-            "email": "john@example.com",
-            "password": " password123 ",
-        },
-    )
-
-    assert "errors" in data
-    assert data["errors"].get_errors_locations() == ["name"]
-    assert data["errors"].get_errors_types() == ["value_error.any_str.max_length"]
-
-
-@pytest.mark.asyncio
-async def __test_registration_mutation_validates_user_name_content(graphql_info):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "invalid!",
-            "email": "john@example.com",
-            "password": " password123 ",
-        },
-    )
-
-    assert "errors" in data
-    assert data["errors"].get_errors_locations() == ["name"]
-    assert data["errors"].get_errors_types() == ["value_error.username"]
-
-
-@pytest.mark.asyncio
-async def __test_registration_mutation_validates_if_username_is_available(
-    graphql_info, user
+async def test_registration_mutation_validates_max_user_name_length(
+    query_public_api, db
 ):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": user.name,
-            "email": "john@example.com",
-            "password": " password123 ",
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": "abcd",
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert "errors" in data
-    assert data["errors"].get_errors_locations() == ["name"]
-    assert data["errors"].get_errors_types() == ["value_error.username.not_available"]
+    assert result["data"]["register"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": ["name"],
+                "type": "value_error.any_str.max_length",
+            }
+        ],
+    }
 
 
 @pytest.mark.asyncio
-async def __test_registration_mutation_validates_user_email(graphql_info):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "John",
-            "email": "invalidemail",
-            "password": " password123 ",
+async def test_registration_mutation_validates_user_name_content(query_public_api, db):
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": "invalid!",
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert "errors" in data
-    assert data["errors"].get_errors_locations() == ["email"]
-    assert data["errors"].get_errors_types() == ["value_error.email"]
+    assert result["data"]["register"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": ["name"],
+                "type": "value_error.username",
+            }
+        ],
+    }
 
 
 @pytest.mark.asyncio
-async def __test_registration_mutation_validates_if_user_email_is_available(
-    graphql_info, user
+async def test_registration_mutation_validates_if_username_is_available(
+    query_public_api, user
 ):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "John",
-            "email": user.email,
-            "password": " password123 ",
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": user.name,
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert "errors" in data
-    assert data["errors"].get_errors_locations() == ["email"]
-    assert data["errors"].get_errors_types() == ["value_error.email.not_available"]
+    assert result["data"]["register"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": ["name"],
+                "type": "value_error.username.not_available",
+            }
+        ],
+    }
 
 
 @pytest.mark.asyncio
-async def __test_registration_mutation_returns_authorization_token_for_user(
-    graphql_info,
-):
-    data = await resolve_register(
-        None,
-        graphql_info,
-        input={
-            "name": "John",
-            "email": "john@example.com",
-            "password": " password123 ",
+async def test_registration_mutation_validates_user_email(query_public_api, db):
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": "John",
+                "email": "invalidemail",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert "errors" not in data
-    assert "token" in data
+    assert result["data"]["register"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": ["email"],
+                "type": "value_error.email",
+            }
+        ],
+    }
 
-    user = await get_user_from_token(graphql_info.context, data["token"])
-    assert user.id == data["user"].id
+
+@pytest.mark.asyncio
+async def test_registration_mutation_validates_if_user_email_is_available(
+    query_public_api, user
+):
+    result = await query_public_api(
+        REGISTER_MUTATION,
+        {
+            "input": {
+                "name": "John",
+                "email": user.email,
+                "password": " password123 ",
+            },
+        },
+    )
+
+    assert result["data"]["register"] == {
+        "user": None,
+        "token": None,
+        "errors": [
+            {
+                "location": ["email"],
+                "type": "value_error.email.not_available",
+            }
+        ],
+    }
--- misago/graphql/public/mutations/tests/test_setup_site.py
+++ misago/graphql/public/mutations/tests/test_setup_site.py
@@ -4,120 +4,96 @@ import pytest
 
 from .....auth import get_user_from_token
 from .....conf.dynamicsettings import get_settings_from_db
+from .....errors import ErrorsList
 from .....passwords import check_password
 from .....testing import override_dynamic_settings
 from .....users.get import get_user_by_email
 from .....users.models import User
-from ..setupsite import resolve_setup_site
+
+SETUP_SITE_MUTATION = """
+    mutation SetupSite($input: SetupSiteInput!) {
+        setupSite(input: $input) {
+            user {
+                id
+                name
+            }
+            token
+            errors {
+                location
+                type
+            }
+        }
+    }
+"""
 
 
 @pytest.mark.asyncio
-async def test_setup_site_mutation_disables_site_wizard(db, graphql_info):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": "John",
-            "email": "john@example.com",
-            "password": " password123 ",
+async def test_setup_site_mutation_setups_site(query_public_api, graphql_info, db):
+    result = await query_public_api(
+        SETUP_SITE_MUTATION,
+        {
+            "input": {
+                "forumName": "Hello world!",
+                "forumIndexThreads": False,
+                "name": "John",
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert not "errors" in data
+    data = result["data"]["setupSite"]
+
+    assert data == {
+        "user": {"id": ANY, "name": "John"},
+        "token": ANY,
+        "errors": None,
+    }
 
     settings = await get_settings_from_db()
-    assert not settings["enable_site_wizard"]
-
-
-@pytest.mark.asyncio
-async def test_setup_site_mutation_updates_initial_settings(db, graphql_info):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": "John",
-            "email": "john@example.com",
-            "password": " password123 ",
-        },
-    )
-
-    assert not "errors" in data
-
-    settings = await get_settings_from_db()
+    assert not settings["enable_site_wizard"], "Site wizard should disable after use"
     assert settings["forum_name"] == "Hello world!"
     assert settings["forum_index_threads"] is False
 
+    # Wizard creates admin account
+    user_from_db = await User.query.one(id=int(data["user"]["id"]))
+    assert user_from_db.is_active
+    assert user_from_db.is_moderator
+    assert user_from_db.is_admin
+    assert await check_password(" password123 ", user_from_db.password)
 
-@pytest.mark.asyncio
-async def test_setup_site_mutation_creates_admin_account(db, graphql_info):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": "John",
-            "email": "john@example.com",
-            "password": " password123 ",
-        },
-    )
-
-    assert not "errors" in data
-
-    assert data["user"] == await User.query.one(id=data["user"].id)
-    assert await check_password(" password123 ", data["user"].password)
-
-
-@pytest.mark.asyncio
-async def test_setup_site_mutation_returns_admin_token(db, graphql_info):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": "John",
-            "email": "john@example.com",
-            "password": " password123 ",
-        },
-    )
-
-    assert not "errors" in data
-    assert "token" in data
-
+    # Wizard creates admin token
     user_from_token = await get_user_from_token(graphql_info.context, data["token"])
-    assert user_from_token.id == data["user"].id
+    assert user_from_token.id == int(data["user"]["id"])
 
 
 @pytest.mark.asyncio
 @override_dynamic_settings(enable_site_wizard=False)
 async def test_setup_site_mutation_returns_error_if_site_wizard_is_disabled(
-    db, graphql_info
+    query_public_api, db
 ):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": "John",
-            "email": "john@example.com",
-            "password": " password123 ",
+    result = await query_public_api(
+        SETUP_SITE_MUTATION,
+        {
+            "input": {
+                "forumName": "Hello world!",
+                "forumIndexThreads": False,
+                "name": "John",
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert data == {
+    assert result["data"]["setupSite"] == {
+        "user": None,
+        "token": None,
         "errors": [
             {
-                "loc": ("__root__",),
+                "location": [ErrorsList.ROOT_LOCATION],
                 "type": "value_error.site_wizard.disabled",
-                "msg": "site wizard is disabled",
-            }
-        ]
+            },
+        ],
     }
 
     user = await get_user_by_email("john@example.com")
@@ -125,105 +101,112 @@ async def test_setup_site_mutation_returns_error_if_site_wizard_is_disabled(
 
 
 @pytest.mark.asyncio
-async def test_setup_site_mutation_validates_user_name(db, graphql_info):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": "!!!",
-            "email": "john@example.com",
-            "password": " password123 ",
+async def test_setup_site_mutation_validates_user_name(query_public_api, db):
+    result = await query_public_api(
+        SETUP_SITE_MUTATION,
+        {
+            "input": {
+                "forumName": "Hello world!",
+                "forumIndexThreads": False,
+                "name": "!!!",
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert data == {
+    assert result["data"]["setupSite"] == {
+        "user": None,
+        "token": None,
         "errors": [
             {
-                "loc": ("name",),
+                "location": ["name"],
                 "type": "value_error.username",
-                "msg": ANY,
-                "ctx": ANY,
-            }
-        ]
+            },
+        ],
     }
 
 
 @pytest.mark.asyncio
-async def test_setup_site_mutation_validates_user_namel_is_available(
-    db, graphql_info, user
+async def test_setup_site_mutation_validates_user_name_is_available(
+    query_public_api, user
 ):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": user.name,
-            "email": "john@example.com",
-            "password": " password123 ",
+    result = await query_public_api(
+        SETUP_SITE_MUTATION,
+        {
+            "input": {
+                "forumName": "Hello world!",
+                "forumIndexThreads": False,
+                "name": user.name,
+                "email": "john@example.com",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert data == {
+    assert result["data"]["setupSite"] == {
+        "user": None,
+        "token": None,
         "errors": [
             {
-                "loc": ("name",),
+                "location": ["name"],
                 "type": "value_error.username.not_available",
-                "msg": "username is not available",
-            }
-        ]
+            },
+        ],
     }
 
 
 @pytest.mark.asyncio
-async def test_setup_site_mutation_validates_user_email(db, graphql_info):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": "John",
-            "email": "invalidemail",
-            "password": " password123 ",
+async def test_setup_site_mutation_validates_user_email(query_public_api, db):
+    result = await query_public_api(
+        SETUP_SITE_MUTATION,
+        {
+            "input": {
+                "forumName": "Hello world!",
+                "forumIndexThreads": False,
+                "name": "John",
+                "email": "invalidemail",
+                "password": " password123 ",
+            },
         },
     )
 
-    assert data == {
+    assert result["data"]["setupSite"] == {
+        "user": None,
+        "token": None,
         "errors": [
             {
-                "loc": ("email",),
+                "location": ["email"],
                 "type": "value_error.email",
-                "msg": "value is not a valid email address",
-            }
-        ]
+            },
+        ],
     }
 
 
 @pytest.mark.asyncio
 async def test_setup_site_mutation_validates_user_email_is_available(
-    db, graphql_info, user
+    query_public_api, user
 ):
-    data = await resolve_setup_site(
-        None,
-        graphql_info,
-        input={
-            "forumName": "Hello world!",
-            "forumIndexThreads": False,
-            "name": "John",
-            "email": user.email,
-            "password": " password123 ",
+    result = await query_public_api(
+        SETUP_SITE_MUTATION,
+        {
+            "input": {
+                "forumName": "Hello world!",
+                "forumIndexThreads": False,
+                "name": "John",
+                "email": user.email,
+                "password": " password123 ",
+            },
         },
     )
 
-    assert data == {
+    assert result["data"]["setupSite"] == {
+        "user": None,
+        "token": None,
         "errors": [
             {
-                "loc": ("email",),
+                "location": ["email"],
                 "type": "value_error.email.not_available",
-                "msg": "e-mail is not available",
-            }
-        ]
+            },
+        ],
     }
