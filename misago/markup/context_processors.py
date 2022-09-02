15f6b16d264ef6bac7f2c4c3f966e450bcd961a7
raydeal <alnw@interia.eu>
2022-09-02 21:02:57 +0100

    tweak

--- misago/markup/context_processors.py
+++ misago/markup/context_processors.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 
 
-def preload_api_path(equest):
+def preload_api_url(request):
     request.frontend_context.update(
         {"PARSE_MARKUP_API": reverse("misago:api:parse-markup")}
     )
