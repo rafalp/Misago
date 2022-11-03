c5a4de581536fdfb2e0ed7e7b2fa1071f2442dfe
rafalp <kontakt@rpiton.com>
2022-11-03 22:55:26 +0100

    Move frontend to webpack

---
+++ frontend/babel.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: false
+      }
+    ],
+    "@babel/preset-react"
+  ],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-proposal-class-properties"
+  ],
+  env: {
+    production: {
+      only: ["src"],
+      plugins: [
+        [
+          "transform-react-remove-prop-types",
+          {
+            removeImport: true
+          }
+        ],
+        "@babel/plugin-transform-react-inline-elements",
+        "@babel/plugin-transform-react-constant-elements"
+      ]
+    }
+  }
+};
