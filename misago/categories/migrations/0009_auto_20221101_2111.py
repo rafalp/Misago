f681a2673eb7216414c484cea0e679370ce4a594
rafalp <kontakt@rpiton.com>
2022-12-21 18:38:28 +0100

    Format code with black

--- misago/categories/migrations/0009_auto_20221101_2111.py
+++ misago/categories/migrations/0009_auto_20221101_2111.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('misago_categories', '0008_auto_20190518_1659'),
+        ("misago_categories", "0008_auto_20190518_1659"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='category',
-            name='color',
+            model_name="category",
+            name="color",
             field=models.CharField(blank=True, max_length=7, null=True),
         ),
         migrations.AddField(
-            model_name='category',
-            name='short_name',
+            model_name="category",
+            name="short_name",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
     ]
