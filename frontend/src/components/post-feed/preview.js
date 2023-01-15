2749145ea3b8486274742cfb0f05df7e8e82b822
rafalp <kontakt@rpiton.com>
2023-01-15 15:53:27 +0100

    Fix broken posts ui preview on user profiles

--- frontend/src/components/post-feed/preview.js
+++ frontend/src/components/post-feed/preview.js
@@ -8,16 +8,26 @@ export default function () {
       <li className="post">
         <div className="panel panel-default panel-post">
           <div className="panel-body">
-            <div className="post-side post-side-anonymous">
-              <div className="media">
-                <div className="media-left">
-                  <span>
-                    <Avatar className="poster-avatar" size={50} />
-                  </span>
-                </div>
-                <div className="media-body">
-                  <div className="media-heading">
-                    <span className="item-title">
+            <div className="panel-content">
+              <div className="post-side post-side-anonymous">
+                <div className="media">
+                  <div className="media-left">
+                    <span>
+                      <Avatar className="poster-avatar" size={50} />
+                    </span>
+                  </div>
+                  <div className="media-body">
+                    <div className="media-heading">
+                      <span className="item-title">
+                        <span
+                          className="ui-preview-text"
+                          style={{ width: random.int(30, 200) + "px" }}
+                        >
+                          &nbsp;
+                        </span>
+                      </span>
+                    </div>
+                    <span className="user-title user-title-anonymous">
                       <span
                         className="ui-preview-text"
                         style={{ width: random.int(30, 200) + "px" }}
@@ -26,50 +36,42 @@ export default function () {
                       </span>
                     </span>
                   </div>
-                  <span className="user-title user-title-anonymous">
+                </div>
+              </div>
+              <div className="post-heading">
+                <span
+                  className="ui-preview-text"
+                  style={{ width: random.int(30, 200) + "px" }}
+                >
+                  &nbsp;
+                </span>
+              </div>
+              <div className="post-body">
+                <article className="misago-markup">
+                  <p>
                     <span
                       className="ui-preview-text"
                       style={{ width: random.int(30, 200) + "px" }}
                     >
                       &nbsp;
                     </span>
-                  </span>
-                </div>
+                    &nbsp;
+                    <span
+                      className="ui-preview-text"
+                      style={{ width: random.int(30, 200) + "px" }}
+                    >
+                      &nbsp;
+                    </span>
+                    &nbsp;
+                    <span
+                      className="ui-preview-text"
+                      style={{ width: random.int(30, 200) + "px" }}
+                    >
+                      &nbsp;
+                    </span>
+                  </p>
+                </article>
               </div>
-            </div>
-            <div className="post-heading">
-              <span
-                className="ui-preview-text"
-                style={{ width: random.int(30, 200) + "px" }}
-              >
-                &nbsp;
-              </span>
-            </div>
-            <div className="post-body">
-              <article className="misago-markup">
-                <p>
-                  <span
-                    className="ui-preview-text"
-                    style={{ width: random.int(30, 200) + "px" }}
-                  >
-                    &nbsp;
-                  </span>
-                  &nbsp;
-                  <span
-                    className="ui-preview-text"
-                    style={{ width: random.int(30, 200) + "px" }}
-                  >
-                    &nbsp;
-                  </span>
-                  &nbsp;
-                  <span
-                    className="ui-preview-text"
-                    style={{ width: random.int(30, 200) + "px" }}
-                  >
-                    &nbsp;
-                  </span>
-                </p>
-              </article>
             </div>
           </div>
         </div>
