b55636cc8618367302ef7f3f98eedc04e4aa554e
Rafał Pitoń <rafio.xudb@gmail.com>
2023-03-26 04:24:38 +0200

    Change UI color to blue (#1506)
    * Experiment with colors
    
    * Change primary color to blue
    
    * Update admin build setup and color scheme
    
    * Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,65 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require("path")
-const StyleLintPlugin = require('stylelint-webpack-plugin')
-const { ProvidePlugin } = require("webpack")
 
-module.exports = {
-  cache: false,
-  entry: "./misago/admin/src/index.js",
-  output: {
-    path: path.resolve(__dirname, "misago", "static", "misago", "admin"),
-    filename: "index.js"
-  },
-  resolve: {
-    alias: {
-      moment$: path.resolve(__dirname, "node_modules", "moment", "min", "moment.min.js")
-    }
-  },
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    devtool: "source-map",
+    cache: false,
+    entry: "./misago/admin/src/index.js",
+    output: {
+      path: path.resolve(__dirname, "misago", "static", "misago", "admin"),
+      filename: "index.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
             options: {
-              plugins: function() {
-                return [
-                  require("autoprefixer")
-                ]
-              }
-            }
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isProduction ? "production" : "development",
+            },
           },
-          "sass-loader"
-        ]
-      }
+        },
+        {
+          test: /\.mjs/,
+          resolve: {
+              fullySpecified: false
+          }
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: function() {
+                    return [
+                      require("autoprefixer")
+                    ]
+                  }
+                }
+              }
+            },
+            "sass-loader"
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "index.css"
+      }),
     ]
-  },
-  plugins: [
-    new StyleLintPlugin({
-      configFile: "./node_modules/stylelint-config-twbs-bootstrap/scss/index.js"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "index.css"
-    })
-  ]
+  }
 }
