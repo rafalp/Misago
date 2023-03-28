32f0dfb36390e3d0d47accada8a9b9d5e7009e81
Rafał Pitoń <rafio.xudb@gmail.com>
2023-03-28 21:49:14 +0200

    Open login modal from link (#1518)

--- frontend/src/components/StartSocialAuth.js
+++ frontend/src/components/StartSocialAuth.js
@@ -12,14 +12,14 @@ const StartSocialAuth = (props) => {
     <div className="form-group form-social-auth">
       <FormHeader className={labelClassName} text={header} />
       <div className="row">
-        {socialAuth.map(({ id, name, button_text, button_color, url }) => {
-          const className = "btn btn-block btn-default btn-social-" + id
+        {socialAuth.map(({ pk, name, button_text, button_color, url }) => {
+          const className = "btn btn-block btn-default btn-social-" + pk
           const style = button_color ? { color: button_color } : null
           const finalButtonLabel =
             button_text || interpolate(buttonLabel, { site: name }, true)
 
           return (
-            <div className={buttonClassName || "col-xs-12"} key={id}>
+            <div className={buttonClassName || "col-xs-12"} key={pk}>
               <a className={className} style={style} href={url}>
                 {finalButtonLabel}
               </a>
