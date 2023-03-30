89d1f2d67bb785d5d3b5910b97ec19f3684fd467
rafalp <kontakt@rpiton.com>
2023-03-31 01:13:50 +0200

    Move targets enum to separate file

---
+++ misago/notifications/targets.py
@@ -0,0 +1,7 @@
+from enum import StrEnum
+
+
+class NotificationTarget(StrEnum):
+    USER = "USER"
+    THREAD = "THREAD"
+    POST = "POST"
--- misago/notifications/verbs.py
+++ misago/notifications/verbs.py
@@ -1,11 +1,5 @@
 from enum import StrEnum
 
 
-class NotificationTarget(StrEnum):
-    USER = "USER"
-    THREAD = "THREAD"
-    POST = "POST"
-
-
 class NotificationVerb(StrEnum):
     REPLIED = "REPLIED"
