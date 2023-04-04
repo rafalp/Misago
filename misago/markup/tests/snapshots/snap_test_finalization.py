06ccd62487874d5532a22dfaa789e37d92b27828
Rafał Pitoń <rafio.xudb@gmail.com>
2023-04-04 18:35:12 +0200

    Fix quotes (#1544)

--- misago/markup/tests/snapshots/snap_test_finalization.py
+++ misago/markup/tests/snapshots/snap_test_finalization.py
@@ -8,8 +8,8 @@ from snapshottest import Snapshot
 snapshots = Snapshot()
 
 snapshots[
-    "test_finalization_sets_translation_strings_in_quotes 1"
-] = '<div class="quote-heading">Quoted message:</div>'
+    "test_finalization_sets_translation_strings_in_empty_quotes_headings 1"
+] = '<div class="quote-heading" data-noquote="1">Quoted message:</div>'
 
 snapshots[
     "test_finalization_sets_translation_strings_in_spoilers_buttons 1"
