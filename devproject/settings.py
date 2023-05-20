59f29148ae0946fedd6c917b88409601dd087fb1
rafalp <kontakt@rpiton.com>
2023-05-20 14:38:50 +0200

    Move context processors list to misago.settings

--- devproject/settings.py
+++ devproject/settings.py
@@ -241,40 +241,7 @@ TEMPLATES = [
         "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.i18n",
-                "django.template.context_processors.media",
-                "django.template.context_processors.request",
-                "django.template.context_processors.static",
-                "django.template.context_processors.tz",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                "misago.acl.context_processors.user_acl",
-                "misago.conf.context_processors.conf",
-                "misago.conf.context_processors.og_image",
-                "misago.core.context_processors.misago_version",
-                "misago.core.context_processors.request_path",
-                "misago.core.context_processors.momentjs_locale",
-                "misago.icons.context_processors.icons",
-                "misago.search.context_processors.search_providers",
-                "misago.themes.context_processors.theme",
-                "misago.legal.context_processors.legal_links",
-                "misago.menus.context_processors.menus",
-                "misago.users.context_processors.user_links",
-                "misago.core.context_processors.hooks",
-                # Data preloaders
-                "misago.conf.context_processors.preload_settings_json",
-                "misago.core.context_processors.current_link",
-                "misago.markup.context_processors.preload_api_url",
-                "misago.threads.context_processors.preload_threads_urls",
-                "misago.users.context_processors.preload_user_json",
-                "misago.categories.context_processors.preload_categories_json",
-                "misago.socialauth.context_processors.preload_socialauth_json",
-                # Note: keep frontend_context processor last for previous processors
-                # to be able to expose data UI app via request.frontend_context
-                "misago.core.context_processors.frontend_context",
-            ]
+            "context_processors": CONTEXT_PROCESSORS,
         },
     }
 ]
--- misago/settings.py
+++ misago/settings.py
@@ -1,6 +1,7 @@
 """Misago's default Django Project settings"""
 
 __all__ = [
+    "CONTEXT_PROCESSORS",
     "INSTALLED_APPS",
 ]
 
@@ -47,4 +48,39 @@ INSTALLED_APPS = [
     "misago.menus",
     "misago.plugins",
     "misago.apiv2",
+]
+
+CONTEXT_PROCESSORS = [
+    "django.template.context_processors.debug",
+    "django.template.context_processors.i18n",
+    "django.template.context_processors.media",
+    "django.template.context_processors.request",
+    "django.template.context_processors.static",
+    "django.template.context_processors.tz",
+    "django.contrib.auth.context_processors.auth",
+    "django.contrib.messages.context_processors.messages",
+    "misago.acl.context_processors.user_acl",
+    "misago.conf.context_processors.conf",
+    "misago.conf.context_processors.og_image",
+    "misago.core.context_processors.misago_version",
+    "misago.core.context_processors.request_path",
+    "misago.core.context_processors.momentjs_locale",
+    "misago.icons.context_processors.icons",
+    "misago.search.context_processors.search_providers",
+    "misago.themes.context_processors.theme",
+    "misago.legal.context_processors.legal_links",
+    "misago.menus.context_processors.menus",
+    "misago.users.context_processors.user_links",
+    "misago.core.context_processors.hooks",
+    # Data preloaders
+    "misago.conf.context_processors.preload_settings_json",
+    "misago.core.context_processors.current_link",
+    "misago.markup.context_processors.preload_api_url",
+    "misago.threads.context_processors.preload_threads_urls",
+    "misago.users.context_processors.preload_user_json",
+    "misago.categories.context_processors.preload_categories_json",
+    "misago.socialauth.context_processors.preload_socialauth_json",
+    # Note: keep frontend_context processor last for previous processors
+    # to be able to expose data UI app via request.frontend_context
+    "misago.core.context_processors.frontend_context",
 ]
