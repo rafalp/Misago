f1cb537e266c1bb3a08485dd166192d59d3e5de3
rafalp <kontakt@rpiton.com>
2023-05-24 20:21:46 +0200

    Fix google search console verification

--- misago/analytics/tests/test_google_verification_page.py
+++ misago/analytics/tests/test_google_verification_page.py
@@ -6,35 +6,18 @@ from ...test import assert_contains
 
 @override_dynamic_settings(google_site_verification=None)
 def test_verification_page_returns_404_if_verification_token_is_not_set(db, client):
-    verification_link = reverse(
-        "misago:google-site-verification", kwargs={"token": "t0k3n"}
-    )
-    response = client.get(verification_link)
+    response = client.get("/googlet0k3n.html")
     assert response.status_code == 404
 
 
 @override_dynamic_settings(google_site_verification="t0k3n")
 def test_verification_page_returns_404_if_verification_token_is_invalid(db, client):
-    verification_link = reverse(
-        "misago:google-site-verification", kwargs={"token": "inv4l1d"}
-    )
-    response = client.get(verification_link)
+    response = client.get("/googleinv4l1d.html")
     assert response.status_code == 404
 
 
 @override_dynamic_settings(google_site_verification="t0k3n")
-def test_verification_page_returns_200_if_verification_token_is_correct(db, client):
-    verification_link = reverse(
-        "misago:google-site-verification", kwargs={"token": "t0k3n"}
-    )
-    response = client.get(verification_link)
+def test_verification_page_returns_google_verification_token(db, client):
+    response = client.get("/googlet0k3n.html")
     assert response.status_code == 200
-
-
-@override_dynamic_settings(google_site_verification="t0k3n")
-def test_verification_page_contains_token(db, client):
-    verification_link = reverse(
-        "misago:google-site-verification", kwargs={"token": "t0k3n"}
-    )
-    response = client.get(verification_link)
-    assert_contains(response, "googlet0k3n.html")
+    assert_contains(response, f"google-site-verification: googlet0k3n.html")
