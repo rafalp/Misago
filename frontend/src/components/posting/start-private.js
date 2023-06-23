c5084a1045ff82952dfe4be550edb35b40e469f3
Manuel <manuelpinedacabeza@gmail.com>
2023-06-23 18:07:22 +0200

    Added editorEmpty conditional and minimized, closed and returned if it's true

--- frontend/src/components/posting/start-private.js
+++ frontend/src/components/posting/start-private.js
@@ -40,6 +40,15 @@ export default class extends Form {
   }
 
   onCancel = () => {
+    const editorEmpty =
+      this.state.post.length === 0 && this.state.title.length === 0
+
+    if (editorEmpty) {
+      this.minimize()
+      posting.close()
+      return
+    }
+
     const cancel = window.confirm(
       pgettext(
         "post thread",
