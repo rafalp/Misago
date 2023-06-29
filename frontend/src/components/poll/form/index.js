c4e22f190bf508ad7f30bb572837529b954e0046
Manuel <manuelpinedacabeza@gmail.com>
2023-06-29 21:58:14 +0200

    Added parenthesis around conditionals

--- frontend/src/components/poll/form/index.js
+++ frontend/src/components/poll/form/index.js
@@ -68,7 +68,6 @@ export default class extends Form {
     let cancel = false
 
     // Nothing added to the poll so no changes to discard
-
     const formEmpty = !!(
       this.state.question === "" &&
       this.state.choices &&
