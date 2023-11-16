680666ea77e6244835197f525eeb49b36b879157
rafalp <kontakt@rpiton.com>
2023-11-16 21:16:22 +0100

    Remove admin assets from misago/admin/src

--- misago/admin/src/tooltips.js
+++
@@ -1,8 +0,0 @@
-import $ from "jquery"
-
-const initTooltips = () => {
-  $('[data-tooltip="top"]').tooltip({ placement: "top" })
-  $('[data-tooltip="bottom"]').tooltip({ placement: "bottom" })
-}
-
-export default initTooltips
--- misago/admin/src/utils.js
+++
@@ -1,7 +0,0 @@
-const addEventListenerToAll = (selector, event, handler) => {
-  document.querySelectorAll(selector).forEach(el => {
-    el.addEventListener(event, handler)
-  })
-}
-
-export { addEventListenerToAll }
