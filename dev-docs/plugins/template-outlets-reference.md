ca90357d9c9d81ba6e2b9cb16177329f2975ef89
rafalp <kontakt@rpiton.com>
2023-12-20 18:09:00 +0100

    Regenerate docs

--- dev-docs/plugins/template-outlets-reference.md
+++ dev-docs/plugins/template-outlets-reference.md
@@ -5,22 +5,32 @@ This document contains a list of all built-in template outlets in Misago.
 
 ## `ADMIN_DASHBOARD_AFTER_ANALYTICS`
 
-On the Admin Dashboard page, below the Analytics card.
+On the admin dashboard page, below the Analytics card.
 
 
 ## `ADMIN_DASHBOARD_AFTER_CHECKS`
 
-On the Admin Dashboard page, below the Checks card.
+On the admin dashboard page, below the Checks card.
 
 
 ## `ADMIN_DASHBOARD_END`
 
-On the Admin Dashboard page, below all other content.
+On the admin dashboard page, below all other content.
 
 
 ## `ADMIN_DASHBOARD_START`
 
-On the Admin Dashboard page, above all other content.
+On the admin dashboard page, above all other content.
+
+
+## `CATEGORIES_LIST_END`
+
+On the categories page, under the list.
+
+
+## `CATEGORIES_LIST_START`
+
+On the categories page, above the list.
 
 
 ## `TEST`
--- misago/plugins/enums.py
+++ misago/plugins/enums.py
@@ -8,11 +8,11 @@ class PluginOutlet(Enum):
     """
 
     TEST = "Used in some tests."
-    ADMIN_DASHBOARD_START = "On the Admin Dashboard page, above all other content."
-    ADMIN_DASHBOARD_AFTER_CHECKS = "On the Admin Dashboard page, below the Checks card."
+    ADMIN_DASHBOARD_START = "On the admin dashboard page, above all other content."
+    ADMIN_DASHBOARD_AFTER_CHECKS = "On the admin dashboard page, below the Checks card."
     ADMIN_DASHBOARD_AFTER_ANALYTICS = (
-        "On the Admin Dashboard page, below the Analytics card."
+        "On the admin dashboard page, below the Analytics card."
     )
-    ADMIN_DASHBOARD_END = "On the Admin Dashboard page, below all other content."
+    ADMIN_DASHBOARD_END = "On the admin dashboard page, below all other content."
     CATEGORIES_LIST_START = "On the categories page, above the list."
     CATEGORIES_LIST_END = "On the categories page, under the list."
