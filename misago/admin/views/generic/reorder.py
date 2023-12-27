52eda47d0e49b71cdb6499fd35f275254b08529a
rafalp <kontakt@rpiton.com>
2023-12-27 22:31:38 +0100

    Add tests for reorder

--- misago/admin/views/generic/reorder.py
+++ misago/admin/views/generic/reorder.py
@@ -12,9 +12,9 @@ class ReorderView(AdminView):
         try:
             ordered_items = self.get_ordered_items(request, items)
             self.reorder_items(request, ordered_items)
-            return HttpResponse()
+            return HttpResponse(status=204)
         except ValidationError as error:
-            return HttpResponse(str(error))
+            return HttpResponse(error.message, status=400)
 
     def get_queryset(self):
         return self.get_model().objects.all()
@@ -22,27 +22,32 @@ class ReorderView(AdminView):
     def get_ordered_items(
         self, request: HttpRequest, items: dict[int, Model]
     ) -> list[Model]:
+        items_ordering = request.POST.getlist("item")
+        if not items_ordering:
+            raise ValidationError(pgettext("admin reorder", "No items sent."))
+
         ordered_items: dict[int, Model] = {}
-        for item_id_str in request.POST.getlist("item"):
+        for item_id_str in items_ordering:
             try:
                 item_id = int(item_id_str)
             except (TypeError, ValueError):
                 raise ValidationError(
-                    pgettext(
-                        "admin reorder", "One or more items were of an invalid type."
-                    )
+                    pgettext("admin reorder", "Invalid item type: %(item)s")
+                    % {"item": item_id_str}
                 )
 
             if item_id in ordered_items:
                 raise ValidationError(
-                    pgettext("admin reorder", "One or more items were non-unique.")
+                    pgettext("admin reorder", "The item is not unique: %(item)s")
+                    % {"item": item_id}
                 )
 
             try:
                 ordered_items[item_id] = items[item_id]
             except KeyError:
                 raise ValidationError(
-                    pgettext("admin reorder", "One or more items did not exist.")
+                    pgettext("admin reorder", "The item does not exist: %(item)s")
+                    % {"item": item_id}
                 )
 
         return list(ordered_items.values())
