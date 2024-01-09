2d5150a7182119a74d46a7e20bea5d364d6e8afe
rafalp <kontakt@rpiton.com>
2024-01-09 22:24:24 +0100

    WIP nested lists

--- misago/parser/patterns/lists.py
+++ misago/parser/patterns/lists.py
@@ -4,43 +4,79 @@ from textwrap import dedent
 from ..parser import Parser, Pattern
 
 LIST_PATTERN = r"(\n|^) *([-*+]|(1[.)])).+(\n\n* *([-*+]|([0-9]+[.)])).+)*"
-LIST_CONTENTS = re.compile(r"(?P<prefix> )*(?P<marker>[-*+]|([0-9]+[.)]))(?P<text>.+)")
+LIST_CONTENTS = re.compile(r"(?P<prefix> *)(?P<marker>[-*+]|([0-9]+[.)]))(?P<text>.+)")
 
 
 class ListMarkdown(Pattern):
-    pattern: str = r"(\n|^)[-*+].*(\n\n*\s*?[-*+].*)*"
+    pattern: str = LIST_PATTERN
 
     def parse(self, parser: Parser, match: str) -> list[dict]:
         items = parse_list_items(parser, match)
-
-        items_ast: list[dict] = []
-        for item in items:
-            items_ast.append(
-                {
-                    "type": "list-item",
-                    "children": item[1],
-                    "lists": [],
-                }
-            )
-
-        return {
-            "type": "list",
-            "ordered": False,
-            "items": items_ast,
-        }
+        return get_lists_from_items(items)
 
 
 def parse_list_items(parser: Parser, match: str) -> list[tuple[int, str, list[dict]]]:
     items: list[tuple[int, str, list[dict]]] = []
     for item in LIST_CONTENTS.finditer(dedent(match)):
-        previous_depth = items[-1][0] if items else 0
-        depth = len(item.group("prefix") or "")
-        if depth % 2 != 0:
-            depth -= 1
-        if depth > previous_depth:
-            depth = previous_depth + 1
+        previous_level = items[-1][0] if items else 0
+        level = len(item.group("prefix") or "")
+        if level % 2 != 0:
+            level -= 1
+        if level > previous_level:
+            level = previous_level + 1
+        print(level)
+
+        marker = item.group("marker")
+        if marker not in "-*+":
+            marker = "n"
 
         text = item.group("text").strip()
-        items.append((depth, item.group("marker"), parser.parse_inline(text)))
+        items.append((level, marker, parser.parse_inline(text)))
 
     return items
+
+
+def get_lists_from_items(
+    items: list[tuple[int, str, list[dict]]],
+) -> list[dict]:
+    lists_asts: list[dict] = []
+    lists_stack: list[dict] = []
+
+    level: int = -1
+    marker: str | None = None
+
+    for item_level, item_marker, children in items:
+        if item_level != level or item_marker != marker:
+            list_ast = {
+                "type": "list",
+                "ordered": item_marker == "n",
+                "items": [],
+            }
+
+            # Remove N levels from stack
+            if item_level < level:
+                for _ in range(level - item_level):
+                    lists_stack.pop(-1)
+
+            elif item_level == level:
+                lists_stack.pop(-1)
+
+            if not lists_stack:
+                lists_asts.append(list_ast)
+
+            if lists_stack:
+                lists_stack[-1]["items"][-1]["lists"].append(list_ast)
+
+            lists_stack.append(list_ast)
+            level = item_level
+            marker = item_marker
+
+        lists_stack[-1]["items"].append(
+            {
+                "type": "list-item",
+                "children": children,
+                "lists": [],
+            }
+        )
+
+    return lists_asts
