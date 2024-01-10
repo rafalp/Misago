caa603c0b46651264a63352e7a669dafd3b97523
rafalp <kontakt@rpiton.com>
2024-01-10 21:41:03 +0100

    Fix list level/marker handling

--- misago/parser/patterns/lists.py
+++ misago/parser/patterns/lists.py
@@ -6,6 +6,8 @@ from ..parser import Parser, Pattern
 LIST_PATTERN = r"(\n|^) *([-*+]|(1[.)])).+(\n\n* *([-*+]|([0-9]+[.)])).+)*"
 LIST_CONTENTS = re.compile(r"(?P<prefix> *)(?P<marker>[-*+]|([0-9]+[.)]))(?P<text>.+)")
 
+ListItem = tuple[int, str, list[dict]]
+
 
 class ListMarkdown(Pattern):
     pattern: str = LIST_PATTERN
@@ -15,9 +17,9 @@ class ListMarkdown(Pattern):
         return get_lists_from_items(items)
 
 
-def parse_list_items(parser: Parser, match: str) -> list[tuple[int, str, list[dict]]]:
-    items: list[tuple[int, str, list[dict]]] = []
-    for item in LIST_CONTENTS.finditer(dedent(match)):
+def parse_list_items(parser: Parser, match: str) -> list[ListItem]:
+    items: list[ListItem] = []
+    for i, item in enumerate(LIST_CONTENTS.finditer(dedent(match))):
         level = len(item.group("prefix") or "")
         if level % 2 != 0:
             level -= 1
@@ -35,7 +37,7 @@ def parse_list_items(parser: Parser, match: str) -> list[tuple[int, str, list[di
 
 
 def get_lists_from_items(
-    items: list[tuple[int, str, list[dict]]],
+    items: list[ListItem],
 ) -> list[dict]:
     lists_asts: list[dict] = []
     lists_stack: list[dict] = []
@@ -44,21 +46,14 @@ def get_lists_from_items(
     marker: str | None = None
 
     for item_level, item_marker, children in items:
-        if item_level != level or item_marker != marker:
-            # Fixme: this always makes new list when item_level < level
+        if item_level > level or item_marker != marker:
             list_ast = {
                 "type": "list",
                 "ordered": item_marker == "n",
                 "items": [],
             }
 
-            # Remove N levels from stack
-            if item_level < level:
-                for _ in range(level - item_level):
-                    print("POP")
-                    lists_stack.pop(-1)
-
-            elif item_level == level:
+            if item_level == level:
                 lists_stack.pop(-1)
 
             if not lists_stack:
@@ -68,6 +63,12 @@ def get_lists_from_items(
                 lists_stack[-1]["items"][-1]["lists"].append(list_ast)
 
             lists_stack.append(list_ast)
+
+        if item_level < level:
+            for _ in range(level - item_level):
+                lists_stack.pop(-1)
+
+        if item_level != level or item_marker != marker:
             level = item_level
             marker = item_marker
 
---
+++ misago/parser/tests/test_lists.py
@@ -0,0 +1,406 @@
+def test_unordered_list(parse_markup):
+    result = parse_markup("- Lorem")
+    assert result == [
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        }
+    ]
+
+
+def test_unordered_list_with_multiple_items(parse_markup):
+    result = parse_markup("- Lorem\n- Ipsum\n- Dolor")
+    assert result == [
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Ipsum",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Dolor",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        }
+    ]
+
+
+def test_unordered_list_with_asterisk(parse_markup):
+    result = parse_markup("* Lorem\n* Ipsum")
+    assert result == [
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Ipsum",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        }
+    ]
+
+
+def test_unordered_list_with_indent(parse_markup):
+    result = parse_markup("  - Lorem\n  - Ipsum")
+    assert result == [
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Ipsum",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        }
+    ]
+
+
+def test_ordered_list(parse_markup):
+    result = parse_markup("1. Lorem\n2) Ipsum\n4. Dolor")
+    assert result == [
+        {
+            "type": "list",
+            "ordered": True,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Ipsum",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Dolor",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        }
+    ]
+
+
+def test_list_with_separating_blank_lines(parse_markup):
+    result = parse_markup("  - Lorem\n\n\n  - Ipsum")
+    assert result == [
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Ipsum",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        }
+    ]
+
+
+def test_multiple_lists_separated_by_marker(parse_markup):
+    result = parse_markup("- Lorem\n- Ipsum\n* Dolor\n* Met\n+ Sit")
+    assert result == [
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Ipsum",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        },
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Dolor",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Met",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        },
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Sit",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        },
+    ]
+
+
+def test_list_with_first_item_subitems(parse_markup):
+    result = parse_markup(
+        """
+        - Lorem
+          - Ipsum
+          - Dolor
+        - Met
+        """
+    )
+    assert result == [
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [
+                        {
+                            "type": "list",
+                            "ordered": False,
+                            "items": [
+                                {
+                                    "type": "list-item",
+                                    "children": [
+                                        {
+                                            "type": "text",
+                                            "text": "Ipsum",
+                                        },
+                                    ],
+                                    "lists": [],
+                                },
+                                {
+                                    "type": "list-item",
+                                    "children": [
+                                        {
+                                            "type": "text",
+                                            "text": "Dolor",
+                                        },
+                                    ],
+                                    "lists": [],
+                                },
+                            ],
+                        }
+                    ],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Met",
+                        },
+                    ],
+                    "lists": [],
+                },
+            ],
+        }
+    ]
+
+
+def test_list_with_second_item_subitems(parse_markup):
+    result = parse_markup(
+        """
+        - Lorem
+        - Met
+          - Ipsum
+          - Dolor
+        """
+    )
+    assert result == [
+        {
+            "type": "list",
+            "ordered": False,
+            "items": [
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Lorem",
+                        },
+                    ],
+                    "lists": [],
+                },
+                {
+                    "type": "list-item",
+                    "children": [
+                        {
+                            "type": "text",
+                            "text": "Met",
+                        },
+                    ],
+                    "lists": [
+                        {
+                            "type": "list",
+                            "ordered": False,
+                            "items": [
+                                {
+                                    "type": "list-item",
+                                    "children": [
+                                        {
+                                            "type": "text",
+                                            "text": "Ipsum",
+                                        },
+                                    ],
+                                    "lists": [],
+                                },
+                                {
+                                    "type": "list-item",
+                                    "children": [
+                                        {
+                                            "type": "text",
+                                            "text": "Dolor",
+                                        },
+                                    ],
+                                    "lists": [],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        }
+    ]
