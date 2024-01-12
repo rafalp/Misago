84b04e346f5585ee1bfc9178ead9755a1a35fea1
rafalp <kontakt@rpiton.com>
2024-01-12 20:45:42 +0100

    Add quote bbcode author

--- misago/parser/tests/test_quote_bbcode.py
+++ misago/parser/tests/test_quote_bbcode.py
@@ -66,6 +66,23 @@ def test_quote_bbcode_with_author_and_post(parse_markup):
     ]
 
 
+def test_quote_bbcode_with_author_and_empty_post(parse_markup):
+    result = parse_markup("[quote=John; post:]Hello world![/quote]")
+    assert result == [
+        {
+            "type": "quote-bbcode",
+            "author": "John",
+            "post": None,
+            "children": [
+                {
+                    "type": "paragraph",
+                    "children": [{"type": "text", "text": "Hello world!"}],
+                },
+            ],
+        },
+    ]
+
+
 def test_quote_bbcode_strips_quotes_and_spaces_from_author(parse_markup):
     result = parse_markup('[quote="  Dune, part 2 "]Hello world![/quote]')
     assert result == [
