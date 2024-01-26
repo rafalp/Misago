3ea308ba003170baf5276a5b8dba592b2a05dc0a
rafalp <kontakt@rpiton.com>
2024-01-26 22:20:20 +0100

    WIP html renderer

--- misago/parser/html.py
+++ misago/parser/html.py
@@ -1,2 +1,55 @@
-def render_html(ast: list, metadata: dict) -> str:
-    raise NotImplementedError()
+from html import escape
+
+from .context import ParserContext
+
+
+def render_ast_to_html(context: ParserContext, ast: list[dict], metadata: dict) -> str:
+    html = ""
+    for ast_node in ast:
+        node_html = render_ast_node_to_html(context, ast_node, metadata)
+        if node_html:
+            if html:
+                html += "\n"
+            html += node_html
+    return html
+
+
+def render_inline_ast_to_html(
+    context: ParserContext, ast: list[dict], metadata: dict
+) -> str:
+    html = ""
+    for ast_node in ast:
+        html += render_ast_node_to_html(context, ast_node, metadata)
+    return html
+
+
+def render_ast_node_to_html(
+    context: ParserContext, ast_node: dict, metadata: dict
+) -> str:
+    return _render_ast_node_to_html_action(context, ast_node, metadata)
+
+
+def _render_ast_node_to_html_action(
+    context: ParserContext, ast_node: dict, metadata: dict
+) -> str:
+    ast_type = ast_node["type"]
+
+    if ast_type in ("heading", "heading-setex"):
+        html_tag = f"h{ast_node['level']}"
+        children = render_inline_ast_to_html(context, ast_node["children"], metadata)
+        return f"<{html_tag}>{children}</{html_tag}>"
+
+    if ast_type == "paragraph":
+        children = render_inline_ast_to_html(context, ast_node["children"], metadata)
+        return f"<p>{children}</p>"
+
+    if ast_type == "line-break":
+        return "<br />"
+
+    if ast_type in ("thematic-break", "thematic-break-bbcode"):
+        return "<hr />"
+
+    if ast_type == "text":
+        return escape(ast_node["text"])
+
+    raise ValueError(f"Unknown AST type: {ast_type}")
---
+++ misago/parser/tests/test_html.py
@@ -0,0 +1,74 @@
+import pytest
+
+from ..html import render_ast_to_html
+from ..metadata import create_ast_metadata
+
+
+@pytest.mark.parametrize(
+    "markup",
+    (
+        "# Hello world!",
+        "## Hello world!",
+        "### Hello world!",
+        "#### Hello world!",
+        "##### Hello world!",
+        "###### Hello world!",
+    ),
+)
+def test_render_ast_to_html_heading(markup, parser_context, parse_markup, snapshot):
+    ast = parse_markup(markup)
+    metadata = create_ast_metadata(parser_context, ast)
+    assert snapshot == render_ast_to_html(parser_context, ast, metadata)
+
+
+@pytest.mark.parametrize(
+    "markup",
+    (
+        "Hello world!\n=====",
+        "Hello world!\n-----",
+    ),
+)
+def test_render_ast_to_html_setex_heading(
+    markup, parser_context, parse_markup, snapshot
+):
+    ast = parse_markup(markup)
+    metadata = create_ast_metadata(parser_context, ast)
+    assert snapshot == render_ast_to_html(parser_context, ast, metadata)
+
+
+def test_render_ast_to_html_paragraph(parser_context, parse_markup, snapshot):
+    ast = parse_markup("Hello world!")
+    metadata = create_ast_metadata(parser_context, ast)
+    assert snapshot == render_ast_to_html(parser_context, ast, metadata)
+
+
+def test_render_ast_to_html_two_paragraphs(parser_context, parse_markup, snapshot):
+    ast = parse_markup("Hello world!\n\nHow's going?")
+    metadata = create_ast_metadata(parser_context, ast)
+    assert snapshot == render_ast_to_html(parser_context, ast, metadata)
+
+
+def test_render_ast_to_html_thematic_break(parser_context, parse_markup, snapshot):
+    ast = parse_markup("Hello world!\n- - -\nHow's going?")
+    metadata = create_ast_metadata(parser_context, ast)
+    assert snapshot == render_ast_to_html(parser_context, ast, metadata)
+
+
+def test_render_ast_to_html_thematic_break_bbcode(
+    parser_context, parse_markup, snapshot
+):
+    ast = parse_markup("Hello world!\n[hr]\nHow's going?")
+    metadata = create_ast_metadata(parser_context, ast)
+    assert snapshot == render_ast_to_html(parser_context, ast, metadata)
+
+
+def test_render_ast_to_html_soft_linebreak(parser_context, parse_markup, snapshot):
+    ast = parse_markup("Hello world!\nHow's going?")
+    metadata = create_ast_metadata(parser_context, ast)
+    assert snapshot == render_ast_to_html(parser_context, ast, metadata)
+
+
+def test_render_ast_to_html_for_unsupported_ast_raises_error(parser_context):
+    with pytest.raises(ValueError):
+        metadata = create_ast_metadata(parser_context, [])
+        render_ast_to_html(parser_context, [{"type": "unsupported"}], metadata)
