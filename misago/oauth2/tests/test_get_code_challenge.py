7d5b061f6650e064b6e7c5f41618c6cefc9aaab6
raydeal <alnw@interia.eu>
2024-02-07 09:02:48 +0000

    Update misago/oauth2/tests/test_get_code_challenge.py
    Co-authored-by: Rafał Pitoń <rafio.xudb@gmail.com>

--- misago/oauth2/tests/test_get_code_challenge.py
+++ misago/oauth2/tests/test_get_code_challenge.py
@@ -4,7 +4,7 @@ from .. import exceptions
 from ..client import get_code_challenge
 
 
-def test_exception_is_raised_if_code_verifier_not_provided():
+def test_exception_is_raised_if_code_verifier_is_not_provided():
     with pytest.raises(exceptions.OAuth2CodeVerifierNotProvidedError):
         get_code_challenge(None, "S256")
 
