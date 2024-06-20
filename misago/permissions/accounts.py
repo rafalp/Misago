74eb459bd7f847662bc4212d034ebe2479557695
rafalp <kontakt@rpiton.com>
2024-06-20 19:59:41 +0200

    Add see/browse category permission check utils

--- misago/permissions/accounts.py
+++ misago/permissions/accounts.py
@@ -1,20 +1,20 @@
 from django.core.exceptions import PermissionDenied
-from django.utils.translation import pgettext_lazy
+from django.utils.translation import pgettext
 
 
 def check_delete_own_account_permission(user):
     if user.is_misago_admin:
         raise PermissionDenied(
-            pgettext_lazy(
-                "users delete permission",
+            pgettext(
+                "delete own account error",
                 "You can't delete your account because you are an administrator.",
             )
         )
 
     if user.is_staff:
         raise PermissionDenied(
-            pgettext_lazy(
-                "users delete permission",
+            pgettext(
+                "delete own account error",
                 "You can't delete your account because you are a staff user.",
             )
         )
--- misago/permissions/categories.py
+++ misago/permissions/categories.py
@@ -1,24 +1,33 @@
-from django.core.exceptions import PermissionDenied
 from django.db.models import Q
-from django.http import Http404
 
 from ..categories.models import Category
 from .enums import CategoryPermission
+from .exceptions import (
+    CategoryBrowseError,
+    CategoryNotFoundError,
+)
 from .proxy import UserPermissionsProxy
 
 
-def check_category_browse_permission(
+def check_see_category_permission(
+    permissions: UserPermissionsProxy,
+    category: Category,
+):
+    if category.id not in permissions.categories[CategoryPermission.SEE]:
+        raise CategoryNotFoundError()
+
+
+def check_browse_category_permission(
     permissions: UserPermissionsProxy,
     category: Category,
     delay_browse_check: bool = False,
 ):
-    if category.id not in permissions.categories[CategoryPermission.SEE]:
-        raise Http404()
+    check_see_category_permission(permissions, category)
 
     if category.id not in permissions.categories[CategoryPermission.BROWSE] and not (
         delay_browse_check and category.delay_browse_check
     ):
-        raise PermissionDenied()
+        raise CategoryBrowseError()
 
 
 def filter_categories_threads_queryset(
