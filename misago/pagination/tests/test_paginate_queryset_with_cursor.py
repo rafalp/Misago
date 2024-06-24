981dc7a048f4d68c7b598b6dbbe4b062094f54a2
rafalp <kontakt@rpiton.com>
2024-06-24 21:59:54 +0200

    Fix pagination tests

--- misago/pagination/tests/test_paginate_queryset_with_cursor.py
+++ misago/pagination/tests/test_paginate_queryset_with_cursor.py
@@ -7,66 +7,76 @@ from ...notifications.models import Notification
 from ..cursor import paginate_queryset
 
 
-def test_pagination_with_after_returns_items_up_to_limit(notifications):
-    request = Mock(GET={"after": notifications[4]})
+def test_pagination_with_cursor_returns_items_up_to_limit(notifications):
+    request = Mock(GET={"cursor": notifications[4]})
     page = paginate_queryset(request, Notification.objects, 4, "id")
 
     assert len(page.items) == 4
     assert page.has_next
     assert page.has_previous
+    assert page.next_cursor == notifications[8]
+    assert page.previous_cursor == notifications[4]
 
     items_verbs = [item.verb for item in page.items]
     assert items_verbs == ["test_5", "test_6", "test_7", "test_8"]
 
 
-def test_pagination_with_after_returns_last_items(notifications):
-    request = Mock(GET={"after": notifications[9]})
+def test_pagination_with_cursor_returns_last_items(notifications):
+    request = Mock(GET={"cursor": notifications[9]})
     page = paginate_queryset(request, Notification.objects, 10, "id")
 
     assert len(page.items) == 5
     assert not page.has_next
     assert page.has_previous
+    assert not page.next_cursor
+    assert not page.previous_cursor
 
     items_verbs = [item.verb for item in page.items]
     assert items_verbs == ["test_10", "test_11", "test_12", "test_13", "test_14"]
 
 
-def test_pagination_with_after_returns_empty_items(notifications):
-    request = Mock(GET={"after": notifications[14]})
+def test_pagination_with_cursor_returns_empty_items(notifications):
+    request = Mock(GET={"cursor": notifications[14]})
     page = paginate_queryset(request, Notification.objects, 10, "id", raise_404=False)
 
     assert len(page.items) == 0
     assert not page.has_next
     assert not page.has_previous
+    assert not page.next_cursor
+    assert not page.previous_cursor
 
 
-def test_pagination_with_after_raises_404_for_empty_item(notifications):
-    request = Mock(GET={"after": notifications[14]})
+def test_pagination_with_cursor_raises_404_for_empty_item(notifications):
+    request = Mock(GET={"cursor": notifications[14]})
     with pytest.raises(Http404):
         paginate_queryset(request, Notification.objects, 10, "id", raise_404=True)
 
 
-def test_pagination_with_after_returns_items_up_to_limit_in_reverse_order(
+def test_pagination_with_cursor_returns_items_up_to_limit_in_reverse_order(
     notifications,
 ):
-    request = Mock(GET={"after": notifications[9]})
+    request = Mock(GET={"cursor": notifications[9]})
     page = paginate_queryset(request, Notification.objects, 4, "-id")
 
     assert len(page.items) == 4
     assert page.has_next
     assert page.has_previous
+    assert page.next_cursor == notifications[5]
+    assert page.previous_cursor == notifications[13]
 
     items_verbs = [item.verb for item in page.items]
     assert items_verbs == ["test_8", "test_7", "test_6", "test_5"]
 
 
-def test_pagination_with_after_returns_last_items_in_reverse_order(notifications):
-    request = Mock(GET={"after": notifications[4]})
+def test_pagination_with_cursor_returns_last_items_in_reverse_order(notifications):
+    request = Mock(GET={"cursor": notifications[4]})
     page = paginate_queryset(request, Notification.objects, 10, "-id")
 
     assert len(page.items) == 4
     assert not page.has_next
     assert page.has_previous
+    assert not page.next_cursor
+    assert page.previous_cursor == notifications[14]
 
     items_verbs = [item.verb for item in page.items]
     assert items_verbs == ["test_3", "test_2", "test_1", "test_0"]
