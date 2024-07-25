b0b59ce6f314774a242468a642d0c7966bbd4faa
rafalp <kontakt@rpiton.com>
2024-07-25 23:33:39 +0200

    Add more tests for threads lists moderation flows

--- misago/threads/tests/test_threads_lists_moderation.py
+++ misago/threads/tests/test_threads_lists_moderation.py
@@ -41,7 +41,9 @@ def test_category_threads_list_shows_fixed_moderation_form_to_moderators(
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_doesnt_show_moderation_to_users(default_category, user_client):
+def test_site_threads_list_doesnt_show_moderation_to_users(
+    default_category, user_client
+):
     response = user_client.get(reverse("misago:threads"))
     assert_not_contains(response, MODERATION_FORM_HTML)
     assert_not_contains(response, MODERATION_FIXED_HTML)
@@ -56,7 +58,9 @@ def test_category_threads_list_doesnt_show_moderation_to_users(
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_doesnt_show_moderation_to_guests(default_category, user_client):
+def test_site_threads_list_doesnt_show_moderation_to_guests(
+    default_category, user_client
+):
     response = user_client.get(reverse("misago:threads"))
     assert_not_contains(response, MODERATION_FORM_HTML)
     assert_not_contains(response, MODERATION_FIXED_HTML)
@@ -85,7 +89,7 @@ def test_category_threads_list_doesnt_show_moderation_to_other_category_moderato
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_shows_threads_checkboxes_to_global_moderators(
+def test_site_threads_list_shows_threads_checkboxes_to_global_moderators(
     default_category, moderator_client
 ):
     thread = post_thread(default_category, "Moderate Thread")
@@ -108,7 +112,7 @@ def test_category_threads_shows_threads_checkboxes_to_global_moderators(
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_shows_threads_checkboxes_to_category_moderators(
+def test_site_threads_list_shows_threads_checkboxes_to_category_moderators(
     default_category, user, user_client
 ):
     Moderator.objects.create(
@@ -141,7 +145,7 @@ def test_category_threads_shows_threads_checkboxes_to_category_moderators(
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_shows_disabled_threads_checkboxes_to_other_category_moderators(
+def test_site_threads_list_shows_disabled_threads_checkboxes_to_other_category_moderators(
     default_category, child_category, user, user_client
 ):
     Moderator.objects.create(
@@ -174,7 +178,7 @@ def test_category_threads_shows_disabled_threads_checkboxes_to_other_category_mo
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_executes_single_stage_moderation_action(
+def test_site_threads_list_executes_single_stage_moderation_action(
     default_category, moderator_client
 ):
     thread = post_thread(default_category, "Moderate Thread")
@@ -190,7 +194,6 @@ def test_site_threads_executes_single_stage_moderation_action(
     assert thread.is_closed
 
 
-@override_dynamic_settings(index_view="categories")
 def test_category_threads_executes_single_stage_moderation_action(
     default_category, moderator_client
 ):
@@ -208,7 +211,7 @@ def test_category_threads_executes_single_stage_moderation_action(
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_executes_single_stage_moderation_action_in_htmx(
+def test_site_threads_list_executes_single_stage_moderation_action_in_htmx(
     default_category, moderator_client
 ):
     thread = post_thread(default_category, "Moderate Thread")
@@ -226,7 +229,6 @@ def test_site_threads_executes_single_stage_moderation_action_in_htmx(
     assert thread.is_closed
 
 
-@override_dynamic_settings(index_view="categories")
 def test_category_threads_executes_single_stage_moderation_action_in_htmx(
     default_category, moderator_client
 ):
@@ -246,7 +248,7 @@ def test_category_threads_executes_single_stage_moderation_action_in_htmx(
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_executes_multi_stage_moderation_action(
+def test_site_threads_list_executes_multi_stage_moderation_action(
     default_category, child_category, moderator_client
 ):
     thread = post_thread(default_category, "Moderate Thread")
@@ -273,7 +275,6 @@ def test_site_threads_executes_multi_stage_moderation_action(
     assert thread.category == child_category
 
 
-@override_dynamic_settings(index_view="categories")
 def test_category_threads_executes_multi_stage_moderation_action(
     default_category, child_category, moderator_client
 ):
@@ -302,7 +303,7 @@ def test_category_threads_executes_multi_stage_moderation_action(
 
 
 @override_dynamic_settings(index_view="categories")
-def test_site_threads_executes_multi_stage_moderation_action_in_htmx(
+def test_site_threads_list_executes_multi_stage_moderation_action_in_htmx(
     default_category, child_category, moderator_client
 ):
     thread = post_thread(default_category, "Moderate Thread")
@@ -330,7 +331,6 @@ def test_site_threads_executes_multi_stage_moderation_action_in_htmx(
     assert thread.category == child_category
 
 
-@override_dynamic_settings(index_view="categories")
 def test_category_threads_executes_multi_stage_moderation_action_in_htmx(
     default_category, child_category, moderator_client
 ):
@@ -357,3 +357,181 @@ def test_category_threads_executes_multi_stage_moderation_action_in_htmx(
 
     thread.refresh_from_db()
     assert thread.category == child_category
+
+
+@override_dynamic_settings(index_view="categories")
+def test_site_threads_list_returns_error_for_invalid_moderation_action(
+    default_category, moderator_client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = moderator_client.post(
+        reverse("misago:threads"),
+        {"moderation": "invalid", "threads": [thread.id]},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+
+def test_category_threads_returns_error_for_invalid_moderation_action(
+    default_category, moderator_client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = moderator_client.post(
+        default_category.get_absolute_url(),
+        {"moderation": "invalid", "threads": [thread.id]},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+
+@override_dynamic_settings(index_view="categories")
+def test_site_threads_list_returns_error_for_invalid_moderation_action_in_htmx(
+    default_category, moderator_client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = moderator_client.post(
+        reverse("misago:threads"),
+        {"moderation": "invalid", "threads": [thread.id]},
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+
+def test_category_threads_returns_error_for_invalid_moderation_action_in_htmx(
+    default_category, moderator_client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = moderator_client.post(
+        default_category.get_absolute_url(),
+        {"moderation": "invalid", "threads": [thread.id]},
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+
+@override_dynamic_settings(index_view="categories")
+def test_site_threads_list_moderation_returns_error_for_user(
+    default_category, user_client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = user_client.post(
+        reverse("misago:threads"),
+        {"moderation": "close", "threads": [thread.id]},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+    thread.refresh_from_db()
+    assert not thread.is_closed
+
+
+def test_category_threads_list_moderation_returns_error_for_user(
+    default_category, user_client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = user_client.post(
+        default_category.get_absolute_url(),
+        {"moderation": "close", "threads": [thread.id]},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+    thread.refresh_from_db()
+    assert not thread.is_closed
+
+
+@override_dynamic_settings(index_view="categories")
+def test_site_threads_list_moderation_returns_error_for_user_in_htmx(
+    default_category, user_client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = user_client.post(
+        reverse("misago:threads"),
+        {"moderation": "close", "threads": [thread.id]},
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+    thread.refresh_from_db()
+    assert not thread.is_closed
+
+
+def test_category_threads_list_moderation_returns_error_for_user_in_htmx(
+    default_category, user_client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = user_client.post(
+        default_category.get_absolute_url(),
+        {"moderation": "close", "threads": [thread.id]},
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+    thread.refresh_from_db()
+    assert not thread.is_closed
+
+
+@override_dynamic_settings(index_view="categories")
+def test_site_threads_list_moderation_returns_error_for_guest(default_category, client):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = client.post(
+        reverse("misago:threads"),
+        {"moderation": "close", "threads": [thread.id]},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+    thread.refresh_from_db()
+    assert not thread.is_closed
+
+
+def test_category_threads_list_moderation_returns_error_for_guest(
+    default_category, client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = client.post(
+        default_category.get_absolute_url(),
+        {"moderation": "close", "threads": [thread.id]},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+    thread.refresh_from_db()
+    assert not thread.is_closed
+
+
+@override_dynamic_settings(index_view="categories")
+def test_site_threads_list_moderation_returns_error_for_guest_in_htmx(
+    default_category, client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = client.post(
+        reverse("misago:threads"),
+        {"moderation": "close", "threads": [thread.id]},
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+    thread.refresh_from_db()
+    assert not thread.is_closed
+
+
+def test_category_threads_list_moderation_returns_error_for_guest_in_htmx(
+    default_category, client
+):
+    thread = post_thread(default_category, "Moderate Thread")
+
+    response = client.post(
+        default_category.get_absolute_url(),
+        {"moderation": "close", "threads": [thread.id]},
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, "Invalid moderation action")
+
+    thread.refresh_from_db()
+    assert not thread.is_closed
