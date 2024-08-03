02f6970aa188fd23a9b1786b36ae86baab9edb5f
rafalp <kontakt@rpiton.com>
2024-08-03 13:15:38 +0200

    Add get_login_url utility

--- misago/auth/urls.py
+++ misago/auth/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import NoReverseMatch, path, reverse
 
 from .views import login
 
 
 urlpatterns = [path("login/", login, name="login")]
+
+
+def get_login_url():
+    try:
+        return reverse(settings.LOGIN_URL)
+    except NoReverseMatch:
+        return settings.LOGIN_URL
