a6c7307de6e9c6dd08263749f356828eef9dfc89
rafalp <kontakt@rpiton.com>
2024-08-06 22:18:38 +0200

    Add hooks for posting in closed category and start thread permission checks

--- misago/permissions/hooks/__init__.py
+++ misago/permissions/hooks/__init__.py
@@ -1,8 +1,14 @@
 from .build_user_category_permissions import build_user_category_permissions_hook
 from .build_user_permissions import build_user_permissions_hook
 from .check_browse_category_permission import check_browse_category_permission_hook
+from .check_post_in_closed_category_permission import (
+    check_post_in_closed_category_permission_hook,
+)
 from .check_private_threads_permission import check_private_threads_permission_hook
 from .check_see_category_permission import check_see_category_permission_hook
+from .check_start_thread_in_category_permission import (
+    check_start_thread_in_category_permission_hook,
+)
 from .copy_category_permissions import copy_category_permissions_hook
 from .copy_group_permissions import copy_group_permissions_hook
 from .filter_private_threads_queryset import filter_private_threads_queryset_hook
@@ -22,8 +28,10 @@ __all__ = [
     "build_user_category_permissions_hook",
     "build_user_permissions_hook",
     "check_browse_category_permission_hook",
+    "check_post_in_closed_category_permission_hook",
     "check_private_threads_permission_hook",
     "check_see_category_permission_hook",
+    "check_start_thread_in_category_permission_hook",
     "copy_category_permissions_hook",
     "copy_group_permissions_hook",
     "filter_private_threads_queryset_hook",
--- misago/permissions/threads/checks.py
+++ misago/permissions/threads/checks.py
@@ -3,10 +3,24 @@ from django.utils.translation import pgettext
 
 from ...categories.models import Category
 from ..enums import CategoryPermission
+from ..hooks import (
+    check_post_in_closed_category_permission_hook,
+    check_start_thread_in_category_permission_hook,
+)
 from ..proxy import UserPermissionsProxy
 
 
 def check_post_in_closed_category_permission(
+    permissions: UserPermissionsProxy, category: Category
+):
+    check_post_in_closed_category_permission_hook(
+        _check_post_in_closed_category_permission_action,
+        permissions,
+        category,
+    )
+
+
+def _check_post_in_closed_category_permission_action(
     permissions: UserPermissionsProxy, category: Category
 ):
     if category.is_closed and not (
@@ -22,6 +36,16 @@ def check_post_in_closed_category_permission(
 
 
 def check_start_thread_in_category_permission(
+    permissions: UserPermissionsProxy, category: Category
+):
+    check_start_thread_in_category_permission_hook(
+        _check_start_thread_in_category_permission_action,
+        permissions,
+        category,
+    )
+
+
+def _check_start_thread_in_category_permission_action(
     permissions: UserPermissionsProxy, category: Category
 ):
     if category.id not in permissions.categories[CategoryPermission.START]:
