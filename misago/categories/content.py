546a3af91ebb4b56909c2f344fa6194e14bf6f05
rafalp <kontakt@rpiton.com>
2024-09-06 00:16:00 +0200

    Add skeletons for new functions

--- misago/categories/content.py
+++ misago/categories/content.py
@@ -6,4 +6,14 @@ def move_category_content(src: Category, dst: Category):
 
 
 def _move_category_content_action(src: Category, dst: Category):
+    src.readthreadset.update(category=dst)
+    src.threadset.update(category=dst)
+    src.postset.update(category=dst)
+
+
+def delete_category_content(category: Category):
+    _delete_category_content_action(category)
+
+
+def _delete_category_content_action(category: Category):
     pass
---
+++ misago/categories/synchronization.py
@@ -0,0 +1,9 @@
+from .models import Category
+
+
+def synchronize_category(category: Category):
+    _synchronize_category_action(category)
+
+
+def _synchronize_category_action(category: Category):
+    pass
