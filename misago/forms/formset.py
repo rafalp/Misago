07d28e629c4c92bb232816a7f198f73121d1d5d6
rafalp <kontakt@rpiton.com>
2024-10-29 21:44:14 +0100

    Extend misago's formset utility API for plugin use

--- misago/forms/formset.py
+++ misago/forms/formset.py
@@ -16,27 +16,57 @@ class Formset:
     def __getitem__(self, form_id: str):
         return self.forms[form_id]
 
+    def __contains__(self, form: Form | str):
+        if isinstance(form, str):
+            return form in self.forms
+
+        if not form.prefix:
+            raise ValueError(f"{type(form)} instance must have a prefix.")
+
+        return form.prefix in self.forms
+
     def get_forms(self) -> list[Form]:
         return list(self.forms.values())
 
-    def add_form(self, form: Form, *, append: bool = True):
+    def add_form(
+        self, form: Form, *, after: str | None = None, before: str | None = None
+    ):
         if not form.prefix:
-            raise ValueError("Forms added to 'Formset' must define a prefix.")
+            raise ValueError("Forms added to 'Formset' must have a prefix.")
         if form.prefix in self.forms:
             raise ValueError(
                 f"Form with prefix '{form.prefix}' is already a part of this formset."
             )
 
-        if append:
+        if after and before:
+            raise ValueError("'after' and 'before' arguments can't be combined.")
+
+        if after or before:
+            self._add_form_at_position(form, after=after, before=before)
+        else:
             self.forms[form.prefix] = form
 
-        else:
-            forms: dict[str, Form] = {}
-            forms[form.prefix] = form
-            forms.update(self.forms)
-            self.forms = forms
+    def _add_form_at_position(
+        self, form: Form, *, after: str | None = None, before: str | None = None
+    ):
+        if (after or before) not in self.forms:
+            raise ValueError(
+                f"Form with prefix '{after or before}' doesn't exist in this formset."
+            )
 
-    @cached_property
+        new_forms: dict[str, Form] = {}
+        for form_id, form_obj in self.forms.items():
+            if form_id == before:
+                new_forms[form.prefix] = form
+
+            new_forms[form_id] = form_obj
+
+            if form_id == after:
+                new_forms[form.prefix] = form
+
+        self.forms = new_forms
+
+    @property
     def is_bound(self) -> bool:
         return all([form.is_bound for form in self.forms.values()])
 
--- misago/forms/tests/test_formset.py
+++ misago/forms/tests/test_formset.py
@@ -24,7 +24,7 @@ class AgeForm(forms.Form):
         return data
 
 
-def test_basic_formset_add_form_adds_form_with_prefix():
+def test_formset_add_form_adds_form_with_prefix():
     form = UserForm(prefix="user")
 
     formset = Formset()
@@ -33,15 +33,15 @@ def test_basic_formset_add_form_adds_form_with_prefix():
     assert formset["user"] is form
 
 
-def test_basic_formset_add_form_raises_value_error_if_form_is_missing_prefix():
+def test_formset_add_form_raises_value_error_if_form_is_missing_prefix():
     with pytest.raises(ValueError) as exc_info:
         formset = Formset()
         formset.add_form(UserForm())
 
-    assert "must define a prefix" in str(exc_info.value)
+    assert "must have a prefix" in str(exc_info.value)
 
 
-def test_basic_formset_add_form_raises_value_error_if_form_with_prefix_already_exists():
+def test_formset_add_form_raises_value_error_if_form_with_prefix_already_exists():
     with pytest.raises(ValueError) as exc_info:
         formset = Formset()
         formset.add_form(UserForm(prefix="user"))
@@ -50,49 +50,117 @@ def test_basic_formset_add_form_raises_value_error_if_form_with_prefix_already_e
     assert "is already a part of this formset" in str(exc_info.value)
 
 
-def test_basic_formset_is_bound_is_true_if_all_forms_are_bound():
+def test_formset_add_form_inserts_form_after_other():
+    formset = Formset()
+    formset.add_form(UserForm(prefix="user"))
+    formset.add_form(AgeForm(prefix="age"), after="user")
+
+    prefixes = [form.prefix for form in formset.get_forms()]
+    assert prefixes == ["user", "age"]
+
+
+def test_formset_add_form_inserts_form_before_other():
+    formset = Formset()
+    formset.add_form(UserForm(prefix="user"))
+    formset.add_form(AgeForm(prefix="age"), before="user")
+
+    prefixes = [form.prefix for form in formset.get_forms()]
+    assert prefixes == ["age", "user"]
+
+
+def test_formset_add_form_raises_value_error_if_both_after_and_before_are_used():
+    with pytest.raises(ValueError) as exc_info:
+        formset = Formset()
+        formset.add_form(UserForm(prefix="user"))
+        formset.add_form(AgeForm(prefix="age"), after="user", before="user")
+
+    assert "'after' and 'before' arguments can't be combined." in str(exc_info.value)
+
+
+def test_formset_add_form_raises_value_error_if_after_value_is_invalid():
+    with pytest.raises(ValueError) as exc_info:
+        formset = Formset()
+        formset.add_form(UserForm(prefix="user"))
+        formset.add_form(AgeForm(prefix="age"), after="invalid")
+
+    assert "Form with prefix 'invalid' doesn't exist in this formset." in str(
+        exc_info.value
+    )
+
+
+def test_formset_add_form_raises_value_error_if_before_value_is_invalid():
+    with pytest.raises(ValueError) as exc_info:
+        formset = Formset()
+        formset.add_form(UserForm(prefix="user"))
+        formset.add_form(AgeForm(prefix="age"), after="invalid")
+
+    assert "Form with prefix 'invalid' doesn't exist in this formset." in str(
+        exc_info.value
+    )
+
+
+def test_formset_in_operation_returns_true_if_form_is_already_in_formset():
+    user_form = UserForm(prefix="user")
+    age_form = AgeForm(prefix="age")
+
+    formset = Formset()
+    formset.add_form(user_form)
+
+    assert user_form in formset
+    assert age_form not in formset
+
+
+def test_formset_in_operation_returns_true_if_form_prefix_is_already_in_formset():
+    formset = Formset()
+    formset.add_form(UserForm(prefix="user"))
+
+    assert "user" in formset
+    assert "age" not in formset
+
+
+def test_formset_is_bound_is_true_if_all_forms_are_bound():
     formset = Formset()
     formset.add_form(UserForm({"user-name": "Alice"}, prefix="user"))
     formset.add_form(AgeForm({"age-age": 12}, prefix="age"))
     assert formset.is_bound
 
 
-def test_basic_formset_is_bound_is_false_if_some_forms_are_bound():
+def test_formset_is_bound_is_false_if_some_forms_are_bound():
     formset = Formset()
     formset.add_form(UserForm({"user-name": "Alice"}, prefix="user"))
     formset.add_form(AgeForm(prefix="age"))
     assert not formset.is_bound
 
 
-def test_basic_formset_is_bound_is_false_if_no_forms_are_bound():
+def test_formset_is_bound_is_false_if_no_forms_are_bound():
     formset = Formset()
     formset.add_form(UserForm(prefix="user"))
     formset.add_form(AgeForm(prefix="age"))
     assert not formset.is_bound
 
 
-def test_basic_formset_is_valid_is_true_if_all_forms_are_valid():
+def test_formset_is_valid_is_true_if_all_forms_are_valid():
     formset = Formset()
     formset.add_form(UserForm({"user-name": "Alice"}, prefix="user"))
     formset.add_form(AgeForm({"age-age": 22}, prefix="age"))
     assert formset.is_valid()
 
 
-def test_basic_formset_is_valid_is_false_if_some_forms_are_valid():
+def test_formset_is_valid_is_false_if_some_forms_are_valid():
     formset = Formset()
     formset.add_form(UserForm({"user-name": "Alice"}, prefix="user"))
     formset.add_form(AgeForm({"age-age": 12}, prefix="age"))
     assert not formset.is_valid()
 
 
-def test_basic_formset_is_valid_is_false_if_no_forms_are_valid():
+def test_formset_is_valid_is_false_if_no_forms_are_valid():
     formset = Formset()
     formset.add_form(UserForm({"user-name": "Alice" * 10}, prefix="user"))
     formset.add_form(AgeForm({"age-age": 12}, prefix="age"))
     assert not formset.is_valid()
 
 
-def test_basic_formset_non_field_errors_returns_empty_if_forms_have_no_non_field_errors():
+def test_formset_non_field_errors_returns_empty_if_forms_have_no_non_field_errors():
     formset = Formset()
     formset.add_form(UserForm({"user-name": "Alice"}, prefix="user"))
     formset.add_form(AgeForm({"age-age": 22}, prefix="age"))
@@ -101,7 +169,7 @@ def test_basic_formset_non_field_errors_returns_empty_if_forms_have_no_non_field
     assert formset.non_field_errors() == []
 
 
-def test_basic_formset_non_field_errors_returns_list_if_any_form_has_no_non_field_errors():
+def test_formset_non_field_errors_returns_list_if_any_form_has_no_non_field_errors():
     formset = Formset()
     formset.add_form(UserForm({"user-name": "Bob"}, prefix="user"))
     formset.add_form(AgeForm({"age-age": 22}, prefix="age"))
@@ -110,7 +178,7 @@ def test_basic_formset_non_field_errors_returns_list_if_any_form_has_no_non_fiel
     assert len(formset.non_field_errors()) == 1
 
 
-def test_basic_formset_non_field_errors_returns_list_of_all_non_field_errors():
+def test_formset_non_field_errors_returns_list_of_all_non_field_errors():
     formset = Formset()
     formset.add_form(UserForm({"user-name": "Bob"}, prefix="user"))
     formset.add_form(AgeForm({"age-age": 42}, prefix="age"))
