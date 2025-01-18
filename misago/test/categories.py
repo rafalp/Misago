e5f0ebca7472390674ff13926bc1945929474829
rafalp <kontakt@rpiton.com>
2025-01-18 13:16:47 +0100

    Complete CategoryRelationsFactory util

--- misago/test/categories.py
+++ misago/test/categories.py
@@ -6,7 +6,17 @@ from ..categories.models import Category, CategoryRole, RoleCategoryACL
 from ..notifications.models import Notification, WatchedThread
 from ..permissions.models import CategoryGroupPermission
 from ..readtracker.models import ReadCategory, ReadThread
-from ..threads.models import Post, Thread
+from ..threads.models import (
+    Attachment as LegacyAttachment,
+    AttachmentType,
+    Poll,
+    PollVote,
+    Post,
+    PostEdit,
+    PostLike,
+    Subscription,
+    Thread,
+)
 from ..threads.test import post_poll, post_thread, reply_thread
 from ..users.models import User, Group
 
@@ -20,13 +30,19 @@ class CategoryRelationsFactory:
 
     attachment: Attachment
     category_group_permission: CategoryGroupPermission
+    legacy_attachment: LegacyAttachment
     notification: Notification
+    poll: Poll
+    poll_vote: PollVote
+    post_edit: PostEdit
+    post_like: PostLike
     read_category: ReadCategory
     read_thread: ReadThread
     role_category_acl: RoleCategoryACL
     thread: Thread
     thread_first_post: Post
     thread_reply: Post
+    thread_subscription: Subscription
     watched_thread: WatchedThread
 
     def __init__(
@@ -52,11 +68,17 @@ class CategoryRelationsFactory:
 
         self.attachment = self.create_attachment()
         self.category_group_permission = self.create_category_group_permission()
+        self.legacy_attachment = self.create_legacy_attachment()
         self.notification = self.create_notification()
+        self.poll = self.create_poll()
+        self.poll_vote = self.create_poll_vote()
+        self.post_edit = self.create_post_edit()
+        self.post_like = self.create_post_like()
         self.read_category = self.create_read_category()
         self.read_thread = self.create_read_thread()
         self.role_category_acl = self.create_role_category_acl()
-        self.watched_thread = self.crea()
+        self.subscription = self.create_subscription()
+        self.watched_thread = self.create_watched_thread()
 
     def create_attachment(self):
         return Attachment.objects.create(
@@ -77,6 +99,17 @@ class CategoryRelationsFactory:
             permission="TEST",
         )
 
+    def create_legacy_attachment(self) -> LegacyAttachment:
+        return LegacyAttachment.objects.create(
+            secret="secret",
+            filetype=AttachmentType.objects.order_by("id").first(),
+            post=self.thread_reply,
+            uploader=self.user,
+            uploader_name=self.user.username,
+            uploader_slug=self.user.slug,
+            filename="filename.txt",
+        )
+
     def create_notification(self) -> Notification:
         return Notification.objects.create(
             user=self.user,
@@ -87,6 +120,34 @@ class CategoryRelationsFactory:
             thread=self.thread,
             thread_title=self.thread.title,
             post=self.thread_reply,
+        )
+
+    def create_poll(self) -> Poll:
+        post_poll(self.thread, self.user)
+
+    def create_poll_vote(self) -> PollVote:
+        return self.poll.pollvote_set.order_by("id").first()
+
+    def create_post_edit(self) -> PostEdit:
+        return PostEdit.objects.create(
+            category=self.category,
+            thread=self.thread,
+            post=self.thread_reply,
+            editor=self.user,
+            editor_name=self.user.username,
+            editor_slug=self.user.slug,
+            edited_from="",
+            edited_to="",
+        )
+
+    def create_post_like(self) -> PostLike:
+        return PostLike.objects.create(
+            category=self.category,
+            thread=self.thread,
+            post=self.thread_first_post,
+            liker=self.other_user,
+            liker_name=self.other_user.username,
+            liker_slug=self.other_user.slug,
         )
 
     def create_read_category(self) -> ReadCategory:
@@ -109,6 +170,13 @@ class CategoryRelationsFactory:
             category_role=CategoryRole.objects.order_by("id").first(),
         )
 
+    def create_subscription(self) -> Subscription:
+        return Subscription.objects.create(
+            user=self.user,
+            thread=self.thread,
+            category=self.category,
+        )
+
     def create_thread(self) -> Thread:
         return post_thread(self.category, poster=self.user)
 
@@ -124,18 +192,43 @@ class CategoryRelationsFactory:
 
     def assert_relations_are_deleted(self):
         self.attachment.refresh_from_db()
-        assert self.attachment.is_deleted, "Attachment was marked for deletion"
-        assert not self.attachment.category, "Attachment category relation was removed"
-        assert not self.attachment.thread, "Attachment thread relation was removed"
-        assert not self.attachment.post, "Attachment post relation was removed"
+        assert self.attachment.is_deleted, "Attachment should be marked for deletion"
+        assert (
+            not self.attachment.category
+        ), "Attachment category relation should be removed"
+        assert (
+            not self.attachment.thread
+        ), "Attachment thread relation should be removed"
+        assert not self.attachment.post, "Attachment post relation should be removed"
 
         with pytest.raises(CategoryGroupPermission.DoesNotExist):
             """CategoryGroupPermission should be deleted when category is deleted"""
             self.category_group_permission.refresh_from_db()
 
+        self.legacy_attachment.refresh_from_db()
+        assert (
+            self.legacy_attachment.post is None
+        ), "LegacyAttachment should marked for deletion"
+
         with pytest.raises(Notification.DoesNotExist):
             """Notification should be deleted when category is deleted"""
             self.notification.refresh_from_db()
+
+        with pytest.raises(Poll.DoesNotExist):
+            """Poll should be deleted when category is deleted"""
+            self.poll.refresh_from_db()
+
+        with pytest.raises(PollVote.DoesNotExist):
+            """PollVote should be deleted when category is deleted"""
+            self.poll_vote.refresh_from_db()
+
+        with pytest.raises(PostEdit.DoesNotExist):
+            """PostEdit should be deleted when category is deleted"""
+            self.post_edit.refresh_from_db()
+
+        with pytest.raises(PostLike.DoesNotExist):
+            """Notification PostLike be deleted when category is deleted"""
+            self.post_like.refresh_from_db()
 
         with pytest.raises(ReadCategory.DoesNotExist):
             """ReadCategory should be deleted when category is deleted"""
@@ -148,6 +241,10 @@ class CategoryRelationsFactory:
         with pytest.raises(RoleCategoryACL.DoesNotExist):
             """RoleCategoryACL should be deleted when category is deleted"""
             self.role_category_acl.refresh_from_db()
+
+        with pytest.raises(Subscription.DoesNotExist):
+            """Subscription should be deleted when category is deleted"""
+            self.subscription.refresh_from_db()
 
         with pytest.raises(Thread.DoesNotExist):
             """Thread should be deleted when category is deleted"""
@@ -170,16 +267,41 @@ class CategoryRelationsFactory:
         assert not self.attachment.is_deleted, "Attachment was NOT marked for deletion"
         assert (
             self.attachment.category_id == new_category.id
-        ), "Attachment category relation was updated"
+        ), "Attachment category relation was not updated"
 
         with pytest.raises(CategoryGroupPermission.DoesNotExist):
             """CategoryGroupPermission should be deleted when category is deleted"""
             self.category_group_permission.refresh_from_db()
 
+        self.legacy_attachment.refresh_from_db()
+        assert (
+            self.legacy_attachment.post_id == self.thread_reply.id
+        ), "LegacyAttachment post was updated"
+
         self.notification.refresh_from_db()
         assert (
             self.notification.category_id == new_category.id
-        ), "Notification category relation was updated"
+        ), "Notification category relation was not updated"
+
+        self.poll.refresh_from_db()
+        assert (
+            self.poll.category_id == new_category.id
+        ), "Poll category relation was not updated"
+
+        self.poll_vote.refresh_from_db()
+        assert (
+            self.poll_vote.category_id == new_category.id
+        ), "PollVote category relation was not updated"
+
+        self.post_edit.refresh_from_db()
+        assert (
+            self.post_edit.category_id == new_category.id
+        ), "PostEdit category relation was not updated"
+
+        self.post_like.refresh_from_db()
+        assert (
+            self.post_like.category_id == new_category.id
+        ), "PostLike category relation was not updated"
 
         with pytest.raises(ReadCategory.DoesNotExist):
             """ReadCategory should be deleted when category is deleted"""
@@ -188,28 +310,33 @@ class CategoryRelationsFactory:
         self.read_thread.refresh_from_db()
         assert (
             self.read_thread.category_id == new_category.id
-        ), "ReadThread category relation was updated"
+        ), "ReadThread category relation was not updated"
 
         with pytest.raises(RoleCategoryACL.DoesNotExist):
             """RoleCategoryACL should be deleted when category is deleted"""
             self.role_category_acl.refresh_from_db()
 
+        self.subscription.refresh_from_db()
+        assert (
+            self.subscription.category_id == new_category.id
+        ), "Subscription category relation was not updated"
+
         self.thread.refresh_from_db()
         assert (
             self.thread.category_id == new_category.id
-        ), "Thread category relation was updated"
+        ), "Thread category relation was not updated"
 
         self.thread_first_post.refresh_from_db()
         assert (
             self.thread_first_post.category_id == new_category.id
-        ), "Thread's first post's category relation was updated"
+        ), "Thread's first post's category relation was not updated"
 
         self.thread_reply.refresh_from_db()
         assert (
             self.thread_reply.category_id == new_category.id
-        ), "Thread reply's category relation was updated"
+        ), "Thread reply's category relation was not updated"
 
         self.watched_thread.refresh_from_db()
         assert (
             self.watched_thread.category_id == new_category.id
-        ), "WatchedThread category relation was updated"
+        ), "WatchedThread category relation was not updated"
