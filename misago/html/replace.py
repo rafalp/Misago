32724e0fcb52c5d15905fac0f5d71d0585912b4c
rafalp <kontakt@rpiton.com>
2025-03-11 22:46:18 +0100

    Small cleanups

--- misago/html/replace.py
+++ misago/html/replace.py
@@ -19,7 +19,7 @@ def replace_html_element_func(f):
     @wraps(f)
     def _html_element_replacer_wrapper(match):
         if args := match.group("args").strip():
-            args = parse_args(args)
+            args = parse_args_str(args)
         else:
             args = None
 
@@ -31,8 +31,8 @@ def replace_html_element_func(f):
 ARG_RE = re.compile(r"(?P<name>[a-z-]+)(\=\"(?P<value>.*?)\")?", re.DOTALL)
 
 
-def parse_args(args: str) -> dict[str, str | bool] | None:
-    clean_args: dict[str, str | bool] = {}
-    for name, value_match, value in ARG_RE.findall(args):
-        clean_args[name] = value.strip() if value_match else True
-    return clean_args or None
+def parse_args_str(args_str: str) -> dict[str, str | bool] | None:
+    args: dict[str, str | bool] = {}
+    for name, value_match, value in ARG_RE.findall(args_str):
+        args[name] = value.strip() if value_match else True
+    return args or None
