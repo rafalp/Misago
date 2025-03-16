1a25dfa6e151820e5695af540e260b08d93e1507
rafalp <kontakt@rpiton.com>
2025-03-16 12:19:00 +0100

    Tweak tests

--- misago/parserv2/tests/test_formatting_bbcode.py
+++ misago/parserv2/tests/test_formatting_bbcode.py
@@ -1,23 +1,30 @@
-def test_bold_bbcode_is_rendered(parse_to_html):
+def test_bold_bbcode(parse_to_html):
     html = parse_to_html("Hello [b]world[/b]!")
     assert html == "<p>Hello <b>world</b>!</p>"
 
 
-def test_italics_bbcode_is_rendered(parse_to_html):
+def test_italics_bbcode(parse_to_html):
     html = parse_to_html("Hello [i]world[/i]!")
     assert html == "<p>Hello <i>world</i>!</p>"
 
 
-def test_underline_bbcode_is_rendered(parse_to_html):
+def test_underline_bbcode(parse_to_html):
     html = parse_to_html("Hello [u]world[/u]!")
     assert html == "<p>Hello <u>world</u>!</p>"
 
 
-def test_strikethrough_bbcode_is_rendered(parse_to_html):
+def test_strikethrough_bbcode(parse_to_html):
     html = parse_to_html("Hello [s]world[/s]!")
     assert html == "<p>Hello <del>world</del>!</p>"
 
 
-def test_nested_formatting_bbcode_is_rendered(parse_to_html):
+def test_nested_formatting_bbcode(parse_to_html):
     html = parse_to_html("Hello [b]wo[u]r[/u]ld[/b]!")
     assert html == "<p>Hello <b>wo<u>r</u>ld</b>!</p>"
+
+
+def test_formatting_bbcode_parses_contents(parse_to_html):
+    html = parse_to_html("Hello [b]<http://example.com>[/b]!")
+    assert html == (
+        '<p>Hello <b><a href="http://example.com">http://example.com</a></b>!</p>'
+    )
