617e7e388f4f8de9cdf1f9dc46014e91cfcb21f5
rafalp <kontakt@rpiton.com>
2025-03-24 23:52:51 +0100

    Add utility class for bbcode blocks parser

--- misago/parserv2/bbcode.py
+++ misago/parserv2/bbcode.py
@@ -1,22 +1,103 @@
-from contextlib import contextmanager
+from typing import Callable
 
 from markdown_it.rules_block.state_block import StateBlock
 
 
-@contextmanager
-def parse_nested_blocks(state: StateBlock, parent: str, start_line: int, end_line: int):
-    if start_line + 1 == end_line:
-        return
+class BBCodeBlockRule:
+    name: str
+    element: str
+    start: Callable[[str], tuple[str, dict | None]]
+    end: Callable[[str], str | None]
 
-    old_line_max = state.lineMax
-    old_parent = state.parentType
+    def __init__(
+        self,
+        name: str,
+        element: str,
+        start: Callable[[str], tuple[str, dict | None]],
+        end: Callable[[str], str | None],
+    ):
+        self.name = name
+        self.element = element
 
-    state.lineMax = start_line + 1
-    state.parentType = parent
+        self.start = start
+        self.end = end
 
-    state.level += 1
-    state.md.block.tokenize(state, start_line + 1, end_line - 1)
-    state.level -= 1
+    def __call__(
+        self, state: StateBlock, startLine: int, endLine: int, silent: bool
+    ) -> bool:
+        if state.is_code_block(startLine):
+            return False
 
-    state.lineMax = old_line_max
-    state.parentType = old_parent
+        line = startLine
+
+        start = state.bMarks[line] + state.tShift[line]
+        maximum = state.eMarks[line]
+
+        opening = self.start(state.src[start:maximum])
+        if not opening:
+            return False
+
+        markup, attrs = opening
+        closing = None
+
+        nesting = 1
+        line += 1
+
+        while line <= endLine:
+            start = state.bMarks[line] + state.tShift[line]
+            maximum = state.eMarks[line]
+            src = state.src[start:maximum]
+
+            if state.is_code_block(startLine):
+                line += 1
+                continue
+
+            if self.start(src):
+                nesting += 1
+
+            elif match := self.end(src):
+                nesting -= 1
+
+                if nesting == 0:
+                    closing = match
+                    break
+
+            line += 1
+
+        if silent:
+            return nesting == 0
+
+        max_line = line + 1
+
+        token = state.push(f"{self.name}_open", self.element, 1)
+        token.markup = markup
+        token.map = [startLine, max_line]
+
+        if attrs:
+            for attr_name, attr_value in attrs.items():
+                token.attrSet(attr_name, attr_value)
+
+        self.parse_children_blocks(state, startLine, max_line)
+
+        token = state.push(f"{self.name}_close", self.element, -1)
+        token.markup = closing
+
+        state.line = max_line + 1
+        return True
+
+    def parse_children_blocks(self, state: StateBlock, startLine: int, endLine: int):
+        if startLine + 1 == endLine:
+            return
+
+        old_line_max = state.lineMax
+        old_parent = state.parentType
+
+        state.lineMax = startLine + 1
+        state.parentType = self.name
+
+        state.level += 1
+        state.md.block.tokenize(state, startLine + 1, endLine - 1)
+        state.level -= 1
+
+        state.lineMax = old_line_max
+        state.parentType = old_parent
--- misago/parserv2/plugins/quotebbcode.py
+++ misago/parserv2/plugins/quotebbcode.py
@@ -1,76 +1,23 @@
 from markdown_it import MarkdownIt
 from markdown_it.rules_block.state_block import StateBlock
 
-from ..bbcode import parse_nested_blocks
+from ..bbcode import BBCodeBlockRule
 
 
 def quote_bbcode_plugin(md: MarkdownIt):
-    md.block.ruler.before("paragraph", "quote_bbcode", quote_bbcode_rule)
+    md.block.ruler.before(
+        "paragraph",
+        "quote_bbcode",
+        BBCodeBlockRule(
+            name="quote_bbcode",
+            element="misago-quote",
+            start=quote_bbcode_start,
+            end=quote_bbcode_end,
+        ),
+    )
 
 
-def quote_bbcode_rule(state: StateBlock, startLine: int, endLine: int, silent: bool):
-    if state.is_code_block(startLine):
-        return False
-
-    line = startLine
-
-    start = state.bMarks[line] + state.tShift[line]
-    maximum = state.eMarks[line]
-
-    opening = quote_bbcode_open_rule(state.src[start:maximum])
-    if not opening:
-        return False
-
-    markup, attrs = opening
-    closing = None
-
-    nesting = 1
-    line += 1
-
-    while line <= endLine:
-        start = state.bMarks[line] + state.tShift[line]
-        maximum = state.eMarks[line]
-        src = state.src[start:maximum]
-
-        if state.is_code_block(startLine):
-            line += 1
-            continue
-
-        if quote_bbcode_open_rule(src):
-            nesting += 1
-
-        elif match := quote_bbcode_close_rule(src):
-            nesting -= 1
-
-            if nesting == 0:
-                closing = match
-                break
-
-        line += 1
-
-    if silent:
-        return nesting == 0
-
-    max_line = line + 1
-
-    token = state.push("quote_bbcode_open", "misago-quote", 1)
-    token.markup = markup
-    token.map = [startLine, max_line]
-
-    if attrs:
-        for attr_name, attr_value in attrs.items():
-            token.attrSet(attr_name, attr_value)
-
-    parse_nested_blocks(state, "quote_bbcode", startLine, max_line)
-
-    token = state.push("quote_bbcode_close", "misago-quote", -1)
-    token.markup = closing
-
-    state.line = max_line + 1
-    return True
-
-
-def quote_bbcode_open_rule(src: str) -> tuple[str, dict | None] | None:
+def quote_bbcode_start(src: str) -> tuple[str, dict | None] | None:
     if not src.lower().startswith("[quote") or "]" not in src:
         return None
 
@@ -96,7 +43,7 @@ def quote_bbcode_parse_args(args_str: str) -> dict | None:
     return {"info": args_str}
 
 
-def quote_bbcode_close_rule(src: str) -> str | None:
+def quote_bbcode_end(src: str) -> str | None:
     if src.lower().startswith("[/quote]"):
         return src
 
