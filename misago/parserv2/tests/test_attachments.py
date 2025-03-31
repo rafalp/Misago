eaf2fec2a794ed4344967a2ffdd5d70021f338bc
rafalp <kontakt@rpiton.com>
2025-03-31 21:21:56 +0200

    First pass on extracting attachments from paragraphs

--- misago/parserv2/tests/test_attachments.py
+++ misago/parserv2/tests/test_attachments.py
@@ -1,13 +1,19 @@
 def test_attachment(parse_to_html):
     html = parse_to_html("<attachment=image.png:12>")
-    assert html == ('<misago-attachment name="image.png" slug="image-png" id="12">')
+    assert html == (
+        '<div class="rich-text-attachment-group">'
+        '\n<misago-attachment name="image.png" slug="image-png" id="12">'
+        "\n</div>"
+    )
 
 
 def test_multiple_attachments_in_one_line(parse_to_html):
     html = parse_to_html("<attachment=image.png:12> <attachment=text.png:13>")
     assert html == (
-        '<misago-attachment name="image.png" slug="image-png" id="12">'
-        '<misago-attachment name="text.png" slug="text-png" id="13">'
+        '<div class="rich-text-attachment-group">'
+        '\n<misago-attachment name="image.png" slug="image-png" id="12">'
+        '\n<misago-attachment name="text.png" slug="text-png" id="13">'
+        "\n</div>"
     )
 
 
@@ -15,8 +21,10 @@ def test_attachment_breaks_paragraph(parse_to_html):
     html = parse_to_html("Lorem ipsum <attachment=image.png:12> Dolor met")
     assert html == (
         "<p>Lorem ipsum</p>"
-        '<misago-attachment name="image.png" slug="image-png" id="12">'
-        "<p>Dolor met</p>"
+        '\n<div class="rich-text-attachment-group">'
+        '\n<misago-attachment name="image.png" slug="image-png" id="12">'
+        "\n</div>"
+        "\n<p>Dolor met</p>"
     )
 
 
@@ -25,7 +33,11 @@ def test_attachments_with_paragraph_between(parse_to_html):
         "<attachment=image.png:12> Lorem ipsum <attachment=text.png:13>"
     )
     assert html == (
-        '<misago-attachment name="image.png" slug="image-png" id="12">'
+        '<div class="rich-text-attachment-group">'
+        '\n<misago-attachment name="image.png" slug="image-png" id="12">'
+        "\n</div>"
         "\n<p>Lorem ipsum</p>"
+        '\n<div class="rich-text-attachment-group">'
         '\n<misago-attachment name="text.png" slug="text-png" id="13">'
+        "\n</div>"
     )
--- misago/parserv2/tokenizer.py
+++ misago/parserv2/tokenizer.py
@@ -1,3 +1,4 @@
+from dataclasses import replace
 from typing import Callable
 
 from markdown_it import MarkdownIt
@@ -10,7 +11,14 @@ TokensProcessor = Callable[[list[Token]], list[Token] | None]
 
 
 def tokenize(parser: MarkdownIt, markup: str) -> list[Token]:
-    return tokenize_hook(_tokenize_action, parser, markup, [])
+    return tokenize_hook(
+        _tokenize_action,
+        parser,
+        markup,
+        [
+            extract_attachments,
+        ],
+    )
 
 
 def _tokenize_action(
@@ -24,6 +32,107 @@ def _tokenize_action(
     return tokens
 
 
-def tokenize_uwu(tokens: list[Token]) -> list[Token] | None:
-    raise Exception(tokens)
-    return tokens
+def extract_attachments(tokens: list[Token]) -> list[Token] | None:
+    nesting: int = 0
+    paragraph: list[Token] = []
+    new_tokens: list[Token] = []
+
+    for token in tokens:
+        if token.tag == "p":
+            nesting += token.nesting
+            paragraph.append(token)
+
+            if not nesting:
+                new_tokens += _extract_attachments_from_paragraph(paragraph)
+                paragraph = []
+
+        elif nesting:
+            paragraph.append(token)
+
+        else:
+            new_tokens.append(token)
+
+    if paragraph:
+        new_tokens += _extract_attachments_from_paragraph(paragraph)
+
+    return new_tokens
+
+
+def _extract_attachments_from_paragraph(tokens: list[Token]) -> list[Token]:
+    p_open, inline, p_close = tokens
+
+    if not _inline_contains_attachments(inline):
+        return tokens
+
+    groups: list[tuple[bool, list[Token]]] = []
+    for child in inline.children:
+        if child.type == "attachment":
+            if not groups or not groups[-1][0]:
+                groups.append((True, []))
+        else:
+            if (
+                groups
+                and groups[-1][0]
+                and child.type == "text"
+                and not child.content.strip()
+            ):
+                continue
+
+            if not groups or groups[-1][0]:
+                groups.append((False, []))
+
+        groups[-1][1].append(child)
+
+    new_tokens: list[Token] = []
+    for attachments, children in groups:
+        if attachments:
+            new_tokens.append(
+                Token(
+                    type="attachments_open",
+                    tag="div",
+                    nesting=1,
+                    attrs={"class": "rich-text-attachment-group"},
+                    block=True,
+                ),
+            )
+
+            for attachment in children:
+                new_tokens.append(replace(attachment, block=True))
+
+            new_tokens.append(
+                Token(
+                    type="attachments_close",
+                    tag="div",
+                    nesting=-1,
+                    block=True,
+                ),
+            )
+
+        elif children:
+            if children[0].type == "text":
+                children[0].content = children[0].content.lstrip()
+            if children[-1].type == "text":
+                children[-1].content = children[-1].content.rstrip()
+
+            new_tokens.append(p_open)
+            new_tokens.append(
+                Token(
+                    type="inline",
+                    tag="",
+                    nesting=0,
+                    level=inline.level,
+                    children=children,
+                    block=True,
+                )
+            )
+            new_tokens.append(p_close)
+
+    return new_tokens
+
+
+def _inline_contains_attachments(token: Token) -> bool:
+    for child in token.children:
+        if child.type == "attachment":
+            return True
+
+    return False
