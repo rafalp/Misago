00951a8028ab22189c95bdcdb970ccf1014d1195
rafalp <kontakt@rpiton.com>
2025-03-31 19:26:22 +0200

    Add extra test for HR BBCode not rendering after HR markdown

--- misago/parserv2/tests/test_hr_bbcode.py
+++ misago/parserv2/tests/test_hr_bbcode.py
@@ -26,3 +26,8 @@ def test_hr_bbcode_repeated_in_line(parse_to_html):
 def test_hr_bbcode_repeated_in_separate_lines(parse_to_html):
     html = parse_to_html("paragraph1\n[hr]\n[hr]\n\n[hr]\n[hr]\nparagraph2")
     assert html == "<p>paragraph1</p>\n<hr>\n<p>paragraph2</p>"
+
+
+def test_hr_bbcode_after_markdown_hr(parse_to_html):
+    html = parse_to_html("paragraph1\n- - -\n[hr]\nparagraph2")
+    assert html == "<p>paragraph1</p>\n<hr>\n<p>paragraph2</p>"
