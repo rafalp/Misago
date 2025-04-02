9006c48c46a9671811ee4ba4834791f23d32952b
rafalp <kontakt@rpiton.com>
2025-04-02 23:15:58 +0200

    Update hook for render_tokens_to_plaintext

--- misago/parserv2/hooks/render_tokens_to_plaintext.py
+++ misago/parserv2/hooks/render_tokens_to_plaintext.py
@@ -1,114 +1,112 @@
-from typing import Callable, Protocol
+from typing import TYPE_CHECKING, Callable, Protocol
 
 from markdown_it import MarkdownIt
 from markdown_it.token import Token
 
 from ...plugins.hooks import FilterHook
 
+if TYPE_CHECKING:
+    from ..plaintext import RendererPlaintext
 
-class TokenizeHookAction(Protocol):
+
+class RenderTokensToPlaintextHookAction(Protocol):
     """
-    A standard Misago function used to create a token stream from markup.
+    A standard Misago function used to convert a token stream into plain text.
 
     # Arguments
 
-    ## `parser: MarkdownIt`
+    ## `tokens: list[Token]`
 
-    A `MarkdownIt` instance used to parse the `markup` string.
+    A list of `Token` instances to render as text.
 
-    ## `markup: str`
+    ## `rules: list[tuple[str, Callable[[RendererPlaintext, list[Token], int], str | None]]]`
 
-    A `str` to tokenize.
-
-    ## `processors: Iterable[Callable[[list[Token]], list[Token] | None]]`
-
-    A list of callables that each accept a single argument (a list of tokens)
-    and return either a updated list of tokens or None if no changes were made.
+    A list of `str`-`callable` pairs where `str` is the name of a `Token` type and `callable` implements the rendering logic.
 
     # Return value
 
-    A list of `Token` instances.
+    A `str` with rendered text.
     """
 
     def __call__(
         self,
-        parser: MarkdownIt,
-        markup: str,
-        processors: list[Callable[[list[Token]], list[Token] | None]],
-    ) -> list[Token]: ...
+        tokens: list[Token],
+        rules: list[
+            tuple[str, Callable[["RendererPlaintext", list[Token], int], str | None]]
+        ],
+    ) -> str: ...
 
 
-class TokenizeHookFilter(Protocol):
+class RenderTokensToPlaintextHookFilter(Protocol):
     """
     A function implemented by a plugin that can be registered in this hook.
 
     # Arguments
 
-    ## `action: TokenizeHookAction`
+    ## `action: RenderTokensToPlaintextHookAction`
 
-    A standard Misago function used to create a token stream from markup.
+    A standard Misago function used to convert a token stream into plain text.
 
     See the [action](#action) section for details.
 
-    ## `parser: MarkdownIt`
+    ## `tokens: list[Token]`
 
-    A `MarkdownIt` instance used to parse the `markup` string.
+    A list of `Token` instances to render as text.
 
-    ## `markup: str`
+    ## `rules: list[tuple[str, Callable[[RendererPlaintext, list[Token], int], str | None]]]`
 
-    A `str` to tokenize.
-
-    ## `processors: Iterable[Callable[[list[Token]], list[Token] | None]]`
-
-    A list of callables that each accept a single argument (a list of tokens)
-    and return either a updated list of tokens or None if no changes were made.
+    A list of `str`-`callable` pairs where `str` is the name of a `Token` type and `callable` implements the rendering logic.
 
     # Return value
 
-    A list of `Token` instances.
+    A `str` with rendered text.
     """
 
     def __call__(
         self,
-        action: TokenizeHookAction,
-        parser: MarkdownIt,
-        markup: str,
-        processors: list[Callable[[list[Token]], list[Token] | None]],
-    ) -> list[Token]: ...
+        action: RenderTokensToPlaintextHookAction,
+        tokens: list[Token],
+        rules: list[
+            tuple[str, Callable[["RendererPlaintext", list[Token], int], str | None]]
+        ],
+    ) -> str: ...
 
 
-class TokenizeHook(FilterHook[TokenizeHookAction, TokenizeHookFilter]):
+class RenderTokensToPlaintextHook(
+    FilterHook[RenderTokensToPlaintextHookAction, RenderTokensToPlaintextHookFilter]
+):
     """
-    This hook wraps the standard function Misago uses to create a token stream
-    from markup.
+    This hook wraps the standard function Misago uses to convert a token stream
+    into plain text.
 
     Token stream is a list of the `Token` instances from `markdown_it.tokens` module.
 
     # Example
 
     The code below implements a custom filter function that includes custom
-    processor for token stream:
+    rule for token stream rendering:
 
     ```python
     from typing import Callable
 
-    from markdown_it import MarkdownIt
     from markdown_it.tokens import Token
     from misago.parser.hooks import render_tokens_to_plaintext_hook
+    from misago.parser.plaintext import RendererPlaintext
 
 
-    def custom_tokens_processor(tokens: list[Token]) -> list[Token] | None:
-        return tokens  # Return changed `tokens` list or None
+    def custom_renderer_rule(
+        renderer: RendererPlaintext, tokens: list[Token], idx: int
+    ) -> str | None:
+        return "\n" + tokens[idx].content
 
 
     @render_tokens_to_plaintext_hook.append_filter
     def tokenize_with_custom_tokens_processor(
         action,
-        parser: MarkdownIt,
-        markup: str,
-        processors: list[Callable[[list[Token]], list[Token] | None]],
-    ) -> list[Token]:
-        processors.append(custom_tokens_processor)
+        tokens: list[Token],
+        rules: list[tuple[str, Callable[[RendererPlaintext, list[Token], int], str | None]]],
+    ) -> str:
+        rules.append(("custom_rule", custom_renderer_rule))
         return action(parser, markup, processors)
     ```
     """
@@ -117,11 +115,13 @@ class TokenizeHook(FilterHook[TokenizeHookAction, TokenizeHookFilter]):
 
     def __call__(
         self,
-        action: TokenizeHookAction,
+        action: RenderTokensToPlaintextHookAction,
         tokens: list[Token],
-        rules: list[Callable[[list[Token]], list[Token] | None]],
-    ) -> list[Token]:
+        rules: list[
+            tuple[str, Callable[["RendererPlaintext", list[Token], int], str | None]]
+        ],
+    ) -> str:
         return super().__call__(action, tokens, rules)
 
 
-render_tokens_to_plaintext_hook = TokenizeHook()
+render_tokens_to_plaintext_hook = RenderTokensToPlaintextHook()
