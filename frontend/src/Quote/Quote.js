507a1d931f957067d19104433604c94647110a5f
rafalp <kontakt@rpiton.com>
2025-04-24 22:05:22 +0200

    Fix quote ui showing reply button to guests

--- frontend/src/Quote/Quote.js
+++ frontend/src/Quote/Quote.js
@@ -102,7 +102,7 @@ class Quote {
     const toolbar = document.createElement("div")
     toolbar.className = "quote-toolbar"
 
-    if (this.options.reply) {
+    if (this.options.reply && false) {
       const reply = this.createToolbarButton(
         pgettext("quote toolbar", "Reply"),
         "reply",
