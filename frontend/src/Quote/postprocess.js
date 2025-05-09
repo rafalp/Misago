7abeb3f18ddcaa145730f9dc86dbb54bb192e114
rafalp <kontakt@rpiton.com>
2025-05-09 19:54:06 +0200

    Select quote tables

--- frontend/src/Quote/postprocess.js
+++ frontend/src/Quote/postprocess.js
@@ -71,8 +71,6 @@ function fixTableColumns(selection, table) {
     })
   })
 
-  const length = Object.keys(cols).length
-
   head.children[0].children = selection.extractNodes(
     Array.from(element.querySelector("thead tr").childNodes)
       .filter((node) => node.nodeName === "TH")
@@ -95,7 +93,49 @@ function table_head(selection, root, nodes) {
     return nodes
   }
 
-  return [{ type: "table", children: nodes }]
+  const table = root.ancestor.closest("table")
+
+  const cols = {}
+  nodes.forEach(function (rowset) {
+    rowset.children.forEach(function (row) {
+      row.children.forEach(function (cell) {
+        cols[cell.index] = true
+      })
+    })
+  })
+
+  const thead = {
+    type: "table_head",
+    children: [
+      {
+        type: "table_row",
+        children: selection.extractNodes(
+          Array.from(table.querySelector("thead tr").childNodes)
+            .filter((node) => node.nodeName === "TH")
+            .filter((_, index) => cols[index])
+        ),
+      },
+    ],
+  }
+
+  const tbody = {
+    type: "table_body",
+    children: nodes[1].children.map((row) => {
+      row.children = selection.extractNodes(
+        Array.from(document.getElementById(row.id).childNodes)
+          .filter((node) => node.nodeName === "TD")
+          .filter((_, index) => cols[index])
+      )
+      return row
+    }),
+  }
+
+  return [
+    {
+      type: "table",
+      children: [thead, tbody],
+    },
+  ]
 }
 
 function table_row(selection, root, nodes) {
@@ -104,11 +144,6 @@ function table_row(selection, root, nodes) {
   }
 
   const table = root.ancestor.closest("table")
-  const header = selection.extractNodes(
-    Array.from(table.querySelector("thead tr").childNodes).filter(
-      (node) => node.nodeName === "TH"
-    )
-  )
 
   const cols = {}
   nodes.forEach(function (row) {
@@ -117,53 +152,31 @@ function table_row(selection, root, nodes) {
     })
   })
 
-  const length = Object.keys(cols).length
-
   const thead = {
     type: "table_head",
-    children: [{ type: "table_row", children: [] }],
+    children: [
+      {
+        type: "table_row",
+        children: selection.extractNodes(
+          Array.from(table.querySelector("thead tr").childNodes)
+            .filter((node) => node.nodeName === "TH")
+            .filter((_, index) => cols[index])
+        ),
+      },
+    ],
   }
-
-  header.forEach(function (node, index) {
-    if (cols[index]) {
-      thead.children[0].children.push(header[index])
-    }
-  })
 
   const tbody = {
     type: "table_body",
-    children: [],
+    children: nodes.map((row) => {
+      row.children = selection.extractNodes(
+        Array.from(document.getElementById(row.id).childNodes)
+          .filter((node) => node.nodeName === "TD")
+          .filter((_, index) => cols[index])
+      )
+      return row
+    }),
   }
-
-  const blankCell = {
-    type: "table_td",
-    index: null,
-    alignment: null,
-    children: [],
-  }
-
-  nodes.forEach(function (rowNode) {
-    const children = []
-
-    for (let index = 0; index < rowNode.children[0].index; index++) {
-      if (cols[index]) {
-        children.push(blankCell)
-      }
-    }
-
-    rowNode.children.forEach(function (node) {
-      children.push(node)
-    })
-
-    while (children.length < length) {
-      children.push(blankCell)
-    }
-
-    tbody.children.push({
-      type: "table_row",
-      children,
-    })
-  })
 
   return [
     {
@@ -178,9 +191,23 @@ function table_th(selection, root, nodes) {
     return nodes
   }
 
+  const cols = {}
+  nodes.forEach(function (cell) {
+    cols[cell.index] = true
+  })
+
   const thead = {
     type: "table_head",
-    children: [{ type: "table_row", children: nodes }],
+    children: [
+      {
+        type: "table_row",
+        children: selection.extractNodes(
+          Array.from(root.ancestor.closest("tr").childNodes)
+            .filter((node) => node.nodeName === "TH")
+            .filter((_, index) => cols[index])
+        ),
+      },
+    ],
   }
 
   return [
@@ -212,9 +239,23 @@ function table_td(selection, root, nodes) {
     )
   })
 
+  const cols = {}
+  nodes.forEach(function (cell) {
+    cols[cell.index] = true
+  })
+
   const tbody = {
     type: "table_body",
-    children: [{ type: "table_row", children: nodes }],
+    children: [
+      {
+        type: "table_row",
+        children: selection.extractNodes(
+          Array.from(root.ancestor.closest("tr").childNodes)
+            .filter((node) => node.nodeName === "TD")
+            .filter((_, index) => cols[index])
+        ),
+      },
+    ],
   }
 
   return [
