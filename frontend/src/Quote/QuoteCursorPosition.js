3a6e4546fb51649d34f47a9a65daab74651b096b
rafalp <kontakt@rpiton.com>
2025-05-09 23:20:55 +0200

    Improve selective quote UI positioning

--- frontend/src/Quote/QuoteCursorPosition.js
+++ frontend/src/Quote/QuoteCursorPosition.js
@@ -8,37 +8,54 @@ class QuoteCursorPosition {
     this.shadow.style.opacity = 0
     this.shadow.style.zIndex = -999
     document.body.appendChild(this.shadow)
+
+    this.offsetY = null
   }
 
   getPosition(target, orgRange) {
     this.shadow.replaceChildren()
     this.shadow.setAttribute("class", target.getAttribute("class"))
 
-    const targetRect = target.getBoundingClientRect()
-    this.shadow.style.width = `${targetRect.width}px`
-    this.shadow.style.height = `${targetRect.height}px`
+    const orgRangeRect = orgRange.getBoundingClientRect()
+    this.shadow.style.width = `${orgRangeRect.width}px`
+
+    if (this.offsetY === null) {
+      const padding = window
+        .getComputedStyle(target)
+        .getPropertyValue("padding-top")
+      this.offsetY = parseFloat(padding.substring(0, padding.length - 2))
+    }
 
     const range = orgRange.cloneRange()
     range.setStart(target.childNodes[0], 0)
-    this.shadow.replaceChildren(...range.cloneContents().childNodes)
+    const rangeChildNodes = range.cloneContents().childNodes
 
     const marker = document.createElement("span")
-    this.shadow.querySelector("p:last-child").appendChild(marker)
+    const marketTarget = this.findMarkerTargetNode(rangeChildNodes)
 
+    if (marketTarget) {
+      marketTarget.appendChild(marker)
+      this.shadow.replaceChildren(...rangeChildNodes)
+    } else {
+      this.shadow.replaceChildren(...rangeChildNodes)
+      this.shadow.appendChild(marker)
+    }
+
+    const targetRect = target.getBoundingClientRect()
     const rangeRect = range.getBoundingClientRect()
     const shadowRect = this.shadow.getBoundingClientRect()
     const markerRect = marker.getBoundingClientRect()
-    const markerPosition = markerRect.x - shadowRect.x
+    const markerX = markerRect.x - shadowRect.x
 
     const rect = {
-      bottom: targetRect.top + rangeRect.height,
+      bottom: targetRect.top + rangeRect.height + this.offsetY,
       height: rangeRect.height,
-      left: targetRect.left + markerPosition - 1,
-      right: targetRect.left + markerPosition + 1,
-      top: targetRect.top + 10,
+      left: targetRect.left + markerX - 1,
+      right: targetRect.left + markerX + 1,
+      top: targetRect.top + this.offsetY,
       width: 2,
-      x: targetRect.x + markerPosition - 1,
-      y: targetRect.y + 10,
+      x: targetRect.x + markerX - 1,
+      y: targetRect.y,
     }
 
     return {
@@ -46,6 +63,16 @@ class QuoteCursorPosition {
         return rect
       },
     }
+  }
+
+  findMarkerTargetNode(nodes) {
+    const node = nodes[nodes.length - 1]
+    if (node.nodeType === Node.TEXT_NODE) {
+      return null
+    } else if (node.childNodes && node.childNodes.length) {
+      return this.findMarkerTargetNode(node.childNodes) || node
+    }
+    return node
   }
 }
 
