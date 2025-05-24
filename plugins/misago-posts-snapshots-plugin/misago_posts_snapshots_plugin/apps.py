afe5b53d66abee868c5538fcb7aa796d64988eb5
rafalp <kontakt@rpiton.com>
2025-05-25 01:44:51 +0200

    WIP posts snapshots plugin

---
+++ plugins/misago-posts-snapshots-plugin/misago_posts_snapshots_plugin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MisagoPostsSnaphostsPlugin(AppConfig):
+    name = "misago_posts_snapshots_plugin"
---
+++ plugins/misago-posts-snapshots-plugin/misago_posts_snapshots_plugin/misago_plugin.py
@@ -0,0 +1,17 @@
+from misago import MisagoPlugin
+
+
+manifest = MisagoPlugin(
+    name="Posts Snapshots",
+    description="Creates snapshots of posts contents.",
+    license="GNU GPL v2",
+    icon="fas fa-clone",
+    color="#b5179e",
+    version="1.0",
+    author="Rafał Pitoń",
+    homepage="https://misago-project.org",
+    sponsor="https://github.com/sponsors/rafalp",
+    help="https://misago-project.org/c/support/30/",
+    bugs="https://misago-project.org/c/bug-reports/29/",
+    repo="https://github.com/rafalp/misago",
+)
