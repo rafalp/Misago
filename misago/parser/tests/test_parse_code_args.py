019df1581fcdf91b1d37567c3f06f0e365ccd621
rafalp <kontakt@rpiton.com>
2025-06-03 21:33:28 +0200

    Unescape code args

--- misago/parser/tests/test_parse_code_args.py
+++ misago/parser/tests/test_parse_code_args.py
@@ -11,6 +11,11 @@ def test_parse_code_args_returns_info_for_custom_str():
     assert args == {"info": "lorem ipsum"}
 
 
+def test_parse_code_args_returns_unescaped_info_for_custom_str():
+    args = parse_code_args("lorem ipsum\\!")
+    assert args == {"info": "lorem ipsum!"}
+
+
 def test_parse_code_args_returns_syntax_for_pygments_syntax_str():
     args = parse_code_args("Python")
     assert args == {"syntax": "python"}
@@ -21,6 +26,11 @@ def test_parse_code_args_returns_syntax_from_prefix_str():
     assert args == {"syntax": "php"}
 
 
+def test_parse_code_args_returns_syntax_from_escaped_prefix_str():
+    args = parse_code_args("syntax: php\\!")
+    assert args == {"syntax": "php!"}
+
+
 def test_parse_code_args_returns_none_for_empty_syntax():
     args = parse_code_args("syntax:")
     assert args is None
@@ -29,3 +39,13 @@ def test_parse_code_args_returns_none_for_empty_syntax():
 def test_parse_code_args_returns_info_and_syntax():
     args = parse_code_args("lorem ipsum ; syntax: php")
     assert args == {"info": "lorem ipsum", "syntax": "php"}
+
+
+def test_parse_code_args_returns_unescaped_info_and_syntax():
+    args = parse_code_args("lorem ipsum \\; syntax\\: php")
+    assert args == {"info": "lorem ipsum ; syntax: php"}
+
+
+def test_parse_code_args_returns_unescaped_info_and_unescaped_syntax():
+    args = parse_code_args("lorem\\!ipsum ; syntax: php\\!")
+    assert args == {"info": "lorem!ipsum", "syntax": "php!"}
