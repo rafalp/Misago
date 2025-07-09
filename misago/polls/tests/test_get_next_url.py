08ea83e9328a5fc2bfe15561467b98e480b0e37e
rafalp <kontakt@rpiton.com>
2025-07-10 00:01:29 +0200

    Add more tests to get next url

--- misago/polls/tests/test_get_next_url.py
+++ misago/polls/tests/test_get_next_url.py
@@ -1,4 +1,4 @@
-from django.urls import resolve
+from django.urls import resolve, reverse
 
 from ..nexturl import get_next_url
 
@@ -19,3 +19,15 @@ def test_get_next_url_returns_thread_url_from_request_resolve_match_with_page(
 
     next_url = get_next_url(request, thread)
     assert next_url == "/t/lorem-ipsum/123/8/"
+
+
+def test_get_next_url_returns_thread_url_if_request_resolve_match_is_not_thread(
+    rf, thread
+):
+    request = rf.get("/u/john/123/")
+    request.resolver_match = resolve("/u/john/123/")
+
+    next_url = get_next_url(request, thread)
+    assert next_url == reverse(
+        "misago:thread", kwargs={"slug": thread.slug, "id": thread.id}
+    )
