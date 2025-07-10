f9bf09ca9767f96f930a008e71c911c98569c2e3
rafalp <kontakt@rpiton.com>
2025-07-10 22:45:15 +0200

    Add tests for poll next url

--- misago/polls/tests/test_get_next_url.py
+++ misago/polls/tests/test_get_next_url.py
@@ -31,3 +31,67 @@ def test_get_next_url_returns_thread_url_if_request_resolve_match_is_not_thread(
     assert next_url == reverse(
         "misago:thread", kwargs={"slug": thread.slug, "id": thread.id}
     )
+
+
+def test_get_next_url_returns_thread_url_from_request_post_next_value(rf, thread):
+    thread_url = reverse(
+        "misago:thread",
+        kwargs={
+            "slug": thread.slug,
+            "id": thread.id,
+        },
+    )
+
+    request = rf.post("/u/john/123/", {"next": thread_url})
+    request.resolver_match = resolve("/u/john/123/")
+
+    next_url = get_next_url(request, thread)
+    assert next_url == thread_url
+
+
+def test_get_next_url_returns_thread_page_url_from_request_post_next_value(rf, thread):
+    thread_url = reverse(
+        "misago:thread",
+        kwargs={
+            "slug": thread.slug,
+            "id": thread.id,
+            "page": 21,
+        },
+    )
+
+    request = rf.post("/u/john/123/", {"next": thread_url})
+    request.resolver_match = resolve("/u/john/123/")
+
+    next_url = get_next_url(request, thread)
+    assert next_url == thread_url
+
+
+def test_get_next_url_returns_thread_url_from_resolver_match_if_post_next_value_is_not_thread_url(
+    rf, thread
+):
+    thread_url = reverse(
+        "misago:thread",
+        kwargs={
+            "slug": thread.slug,
+            "id": thread.id,
+            "page": 21,
+        },
+    )
+
+    request = rf.post(thread_url, {"next": "/c/test/123"})
+    request.resolver_match = resolve(thread_url)
+
+    next_url = get_next_url(request, thread)
+    assert next_url == thread_url
+
+
+def test_get_next_url_returns_thread_url_if_resolver_match_and_post_next_value_is_not_thread_url(
+    rf, thread
+):
+    request = rf.post("/u/john/123/", {"next": "/c/test/123"})
+    request.resolver_match = resolve("/u/john/123/")
+
+    next_url = get_next_url(request, thread)
+    assert next_url == reverse(
+        "misago:thread", kwargs={"slug": thread.slug, "id": thread.id}
+    )
