87514dcc7339abfde804b2232b69cf5d59863800
rafalp <kontakt@rpiton.com>
2025-08-09 14:40:46 +0200

    Backport auth sync silent errors fix

--- frontend/src/initializers/auth-sync.js
+++ frontend/src/initializers/auth-sync.js
@@ -1,7 +1,6 @@
 import misago from "misago/index"
 import { patch } from "misago/reducers/auth"
 import ajax from "misago/services/ajax"
-import snackbar from "misago/services/snackbar"
 import store from "misago/services/store"
 
 const AUTH_SYNC_RATE = 45 // sync user with backend every 45 seconds
@@ -9,14 +8,9 @@ const AUTH_SYNC_RATE = 45 // sync user with backend every 45 seconds
 export default function initializer(context) {
   if (context.get("isAuthenticated")) {
     window.setInterval(function () {
-      ajax.get(context.get("AUTH_API")).then(
-        function (data) {
-          store.dispatch(patch(data))
-        },
-        function (rejection) {
-          snackbar.apiError(rejection)
-        }
-      )
+      ajax.get(context.get("AUTH_API")).then(function (data) {
+        store.dispatch(patch(data))
+      })
     }, AUTH_SYNC_RATE * 1000)
   }
 }
