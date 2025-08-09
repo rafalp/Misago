44a02a67766f1ebdc3eb98c511987d583d9e7b4b
rafalp <kontakt@rpiton.com>
2025-08-10 01:07:48 +0200

    Move readtracker to members

--- misago/readtracker/tests/test_get_unread_private_threads.py
+++ misago/readtracker/tests/test_get_unread_private_threads.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 from ...categories.proxy import CategoriesProxy
 from ...permissions.proxy import UserPermissionsProxy
-from ...threads.models import ThreadParticipant
+from ...privatethreadmembers.models import PrivateThreadMember
 from ...threads.test import post_thread, reply_thread
 from ..models import ReadThread
 from ..privatethreads import get_unread_private_threads
@@ -32,7 +32,7 @@ def test_get_unread_private_threads_returns_unread_thread(
     user.save()
 
     thread = post_thread(private_threads_category)
-    ThreadParticipant.objects.create(user=user, thread=thread)
+    PrivateThreadMember.objects.create(user=user, thread=thread)
 
     private_threads_category.last_post_on = thread.last_post_on
     private_threads_category.save()
@@ -59,7 +59,7 @@ def test_get_unread_private_threads_excludes_unread_thread_older_than_tracking_p
     thread = post_thread(
         private_threads_category, started_on=timezone.now().replace(year=2012)
     )
-    ThreadParticipant.objects.create(user=user, thread=thread)
+    PrivateThreadMember.objects.create(user=user, thread=thread)
 
     private_threads_category.last_post_on = thread.last_post_on
     private_threads_category.save()
@@ -81,7 +81,7 @@ def test_get_unread_private_threads_excludes_unread_thread_older_than_user(
     thread = post_thread(
         private_threads_category, started_on=timezone.now() - timedelta(minutes=30)
     )
-    ThreadParticipant.objects.create(user=user, thread=thread)
+    PrivateThreadMember.objects.create(user=user, thread=thread)
 
     private_threads_category.last_post_on = thread.last_post_on
     private_threads_category.save()
@@ -104,7 +104,7 @@ def test_get_unread_private_threads_excludes_read_thread(
     user.save()
 
     thread = post_thread(private_threads_category)
-    ThreadParticipant.objects.create(user=user, thread=thread)
+    PrivateThreadMember.objects.create(user=user, thread=thread)
 
     ReadThread.objects.create(
         user=user,
@@ -134,7 +134,7 @@ def test_get_unread_private_threads_includes_read_thread_with_unread_reply(
     user.save()
 
     thread = post_thread(private_threads_category)
-    ThreadParticipant.objects.create(user=user, thread=thread)
+    PrivateThreadMember.objects.create(user=user, thread=thread)
 
     ReadThread.objects.create(
         user=user,
@@ -168,7 +168,7 @@ def test_get_unread_private_threads_excludes_thread_in_read_category(
     user.save()
 
     thread = post_thread(private_threads_category)
-    ThreadParticipant.objects.create(user=user, thread=thread)
+    PrivateThreadMember.objects.create(user=user, thread=thread)
 
     private_threads_category.last_post_on = thread.last_post_on
     private_threads_category.save()
@@ -193,7 +193,7 @@ def test_get_unread_private_threads_includes_thread_in_read_category_with_unread
     user.save()
 
     thread = post_thread(private_threads_category)
-    ThreadParticipant.objects.create(user=user, thread=thread)
+    PrivateThreadMember.objects.create(user=user, thread=thread)
 
     reply = reply_thread(thread)
 
