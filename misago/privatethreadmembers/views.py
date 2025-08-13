de5d9b9ab37c2c9508df21e0d1d3a2a5937caca0
rafalp <kontakt@rpiton.com>
2025-08-13 19:29:03 +0200

    Add members grid and basic footer to the private threads members component

--- misago/privatethreadmembers/views.py
+++ misago/privatethreadmembers/views.py
@@ -14,7 +14,12 @@ def get_private_thread_members_context_data(
     owner: Optional["User"],
     members: list["User"],
 ) -> dict:
+    moderation = bool(request.user_permissions.is_private_threads_moderator)
+    manage = moderation or request.user.id == owner.id if owner else None
+
     return {
+        "manage": manage,
+        "moderation": moderation,
         "thread": thread,
         "owner": owner,
         "members": members,
