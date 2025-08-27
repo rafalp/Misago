9eb707e90e222c4a078859b238534ba5eefe51c1
rafalp <kontakt@rpiton.com>
2025-08-27 23:12:45 +0200

    More tests for change private thread owner view

--- misago/privatethreadmembers/tests/test_private_thread_owner_change_view.py
+++ misago/privatethreadmembers/tests/test_private_thread_owner_change_view.py
@@ -1,6 +1,28 @@
 from django.urls import reverse
 
+from ...test import assert_contains
+from ...threadupdates.enums import ThreadUpdateActionName
+from ...threadupdates.models import ThreadUpdate
 from ..members import get_private_thread_members
+
+
+def test_private_thread_owner_change_view_displays_confirm_page_on_get(
+    user_client, other_user, user_private_thread
+):
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-owner-change",
+            kwargs={
+                "id": user_private_thread.id,
+                "slug": user_private_thread.slug,
+                "user_id": other_user.id,
+            },
+        )
+    )
+    assert_contains(
+        response,
+        "Are you sure you want to make <strong>Other_User</strong> the new thread owner?",
+    )
 
 
 def test_private_thread_owner_change_view_changes_thread_owner(
@@ -25,3 +47,37 @@ def test_private_thread_owner_change_view_changes_thread_owner(
     owner, members = get_private_thread_members(user_private_thread)
     assert owner == other_user
     assert members == [user, other_user, moderator]
+
+    ThreadUpdate.objects.get(
+        actor=user,
+        thread=user_private_thread,
+        action=ThreadUpdateActionName.CHANGED_OWNER,
+    )
+
+
+def test_private_thread_owner_change_view_changes_thread_owner_in_htmx(
+    user_client, user, other_user, moderator, user_private_thread
+):
+    response = user_client.post(
+        reverse(
+            "misago:private-thread-owner-change",
+            kwargs={
+                "id": user_private_thread.id,
+                "slug": user_private_thread.slug,
+                "user_id": other_user.id,
+            },
+        ),
+        headers={"hx-request": "true"},
+    )
+
+    assert_contains(response, "3 members")
+
+    owner, members = get_private_thread_members(user_private_thread)
+    assert owner == other_user
+    assert members == [user, other_user, moderator]
+
+    ThreadUpdate.objects.get(
+        actor=user,
+        thread=user_private_thread,
+        action=ThreadUpdateActionName.CHANGED_OWNER,
+    )
