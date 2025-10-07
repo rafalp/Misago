5ed7043d200a08af2532691c74fb3e755bb5318a
rafalp <kontakt@rpiton.com>
2025-10-07 19:37:12 +0200

    WIP more thread tests

--- misago/threads/tests/test_threads_lists_start_thread.py
+++ misago/threads/tests/test_threads_lists_start_thread.py
@@ -54,13 +54,6 @@ def test_category_threads_list_displays_start_thread_button_to_user_with_permiss
     )
 
 
-def test_private_threads_list_displays_start_thread_button_to_user_with_permission(
-    user_client,
-):
-    response = user_client.get(reverse("misago:private-thread-list"))
-    assert_contains(response, reverse("misago:private-thread-start"))
-
-
 @override_dynamic_settings(index_view="categories")
 def test_threads_list_hides_start_thread_button_from_guest_without_permission(
     client, guests_group
@@ -125,16 +118,6 @@ def test_category_threads_list_hides_start_thread_button_from_user_without_permi
         )
     )
     assert_not_contains(response, reverse("misago:thread-start"))
-
-
-def test_private_threads_list_hides_start_thread_button_from_user_without_permission(
-    user, user_client
-):
-    user.group.can_start_private_threads = False
-    user.group.save()
-
-    response = user_client.get(reverse("misago:private-thread-list"))
-    assert_not_contains(response, reverse("misago:private-thread-start"))
 
 
 def test_closed_category_threads_list_hides_start_thread_button_from_user_without_permission(
