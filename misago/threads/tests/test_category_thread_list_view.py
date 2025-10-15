96f18f30a4f706249fff582af0cb66f08c32cf0b
rafalp <kontakt@rpiton.com>
2025-10-15 18:58:35 +0200

    Move vanilla category tests to category thread list tests

--- misago/threads/tests/test_category_thread_list_view.py
+++ misago/threads/tests/test_category_thread_list_view.py
@@ -246,6 +246,52 @@ def test_category_thread_list_view_returns_redirect_to_valid_url_if_slug_is_inva
     )
 
 
+def test_category_thread_list_view_returns_error_404_for_top_level_vanilla_category_without_children(
+    default_category, client
+):
+    default_category.is_vanilla = True
+    default_category.list_children_threads = False
+    default_category.save()
+
+    response = client.get(default_category.get_absolute_url())
+    assert response.status_code == 404
+
+
+def test_category_thread_list_view_returns_error_404_for_top_level_vanilla_category_with_invisible_children(
+    default_category, child_category, client
+):
+    default_category.is_vanilla = True
+    default_category.list_children_threads = False
+    default_category.save()
+
+    CategoryGroupPermission.objects.filter(category=child_category).delete()
+
+    response = client.get(default_category.get_absolute_url())
+    assert response.status_code == 404
+
+
+def test_category_thread_list_view_renders_for_top_level_vanilla_category_with_children(
+    default_category, child_category, client
+):
+    default_category.is_vanilla = True
+    default_category.list_children_threads = False
+    default_category.save()
+
+    response = client.get(default_category.get_absolute_url())
+    assert response.status_code == 200
+
+
+def test_category_thread_list_view_renders_for_nested_vanilla_category_without_children(
+    child_category, client
+):
+    child_category.is_vanilla = True
+    child_category.list_children_threads = False
+    child_category.save()
+
+    response = client.get(child_category.get_absolute_url())
+    assert response.status_code == 200
+
+
 def test_category_thread_list_view_renders_full_subcategories_component(
     client, guests_group, default_category
 ):
