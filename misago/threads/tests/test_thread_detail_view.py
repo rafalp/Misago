cc7be4bb0548be2cc77648c0b1cfabf6a4a98e3f
rafalp <kontakt@rpiton.com>
2025-10-22 19:49:34 +0200

    Add tests for show_started_only in thread detail view

--- misago/threads/tests/test_thread_detail_view.py
+++ misago/threads/tests/test_thread_detail_view.py
@@ -104,6 +104,70 @@ def test_thread_detail_view_shows_error_404_to_users_who_cant_see_thread(
     assert response.status_code == 404
 
 
+def test_thread_detail_view_shows_anonymous_user_deleted_user_thread(client, thread):
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_anonymous_user_user_thread(client, user_thread):
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": user_thread.id,
+                "slug": user_thread.slug,
+            },
+        )
+    )
+    assert_contains(response, user_thread.title)
+
+
+def test_thread_detail_view_shows_anonymous_user_category_moderator_thread(
+    client, user, user_thread
+):
+    Moderator.objects.create(
+        user=user,
+        is_global=False,
+        categories=[user_thread.category_id],
+    )
+
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": user_thread.id,
+                "slug": user_thread.slug,
+            },
+        )
+    )
+    assert_contains(response, user_thread.title)
+
+
+def test_thread_detail_view_shows_anonymous_user_global_moderator_thread(
+    client, user, user_thread
+):
+    Moderator.objects.create(user=user)
+
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": user_thread.id,
+                "slug": user_thread.slug,
+            },
+        )
+    )
+    assert_contains(response, user_thread.title)
+
+
 def test_thread_detail_view_shows_user_their_thread(user_client, user_thread):
     response = user_client.get(
         reverse(
@@ -120,6 +184,44 @@ def test_thread_detail_view_shows_user_their_thread(user_client, user_thread):
 def test_thread_detail_view_shows_user_other_user_thread(
     user_client, other_user_thread
 ):
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": other_user_thread.id,
+                "slug": other_user_thread.slug,
+            },
+        )
+    )
+    assert_contains(response, other_user_thread.title)
+
+
+def test_thread_detail_view_shows_user_category_moderator_thread(
+    user_client, other_user, other_user_thread
+):
+    Moderator.objects.create(
+        user=other_user,
+        is_global=False,
+        categories=[other_user_thread.category_id],
+    )
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": other_user_thread.id,
+                "slug": other_user_thread.slug,
+            },
+        )
+    )
+    assert_contains(response, other_user_thread.title)
+
+
+def test_thread_detail_view_shows_user_global_moderator_thread(
+    user_client, other_user, other_user_thread
+):
+    Moderator.objects.create(user=other_user)
+
     response = user_client.get(
         reverse(
             "misago:thread",
@@ -217,6 +319,78 @@ def test_thread_detail_view_shows_global_moderator_deleted_user_thread(
     assert_contains(response, thread.title)
 
 
+def test_thread_detail_view_shows_anonymous_user_deleted_user_thread_in_htmx(
+    client, thread
+):
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        ),
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_anonymous_user_user_thread_in_htmx(
+    client, user_thread
+):
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": user_thread.id,
+                "slug": user_thread.slug,
+            },
+        ),
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, user_thread.title)
+
+
+def test_thread_detail_view_shows_anonymous_user_category_moderator_thread_in_htmx(
+    client, user, user_thread
+):
+    Moderator.objects.create(
+        user=user,
+        is_global=False,
+        categories=[user_thread.category_id],
+    )
+
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": user_thread.id,
+                "slug": user_thread.slug,
+            },
+        ),
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, user_thread.title)
+
+
+def test_thread_detail_view_shows_anonymous_user_global_moderator_thread_in_htmx(
+    client, user, user_thread
+):
+    Moderator.objects.create(user=user)
+
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": user_thread.id,
+                "slug": user_thread.slug,
+            },
+        ),
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, user_thread.title)
+
+
 def test_thread_detail_view_shows_user_their_thread_in_htmx(user_client, user_thread):
     response = user_client.get(
         reverse(
@@ -234,6 +408,46 @@ def test_thread_detail_view_shows_user_their_thread_in_htmx(user_client, user_th
 def test_thread_detail_view_shows_user_other_user_thread_in_htmx(
     user_client, other_user_thread
 ):
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": other_user_thread.id,
+                "slug": other_user_thread.slug,
+            },
+        ),
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, other_user_thread.title)
+
+
+def test_thread_detail_view_shows_user_category_moderator_thread_in_htmx(
+    user_client, other_user, other_user_thread
+):
+    Moderator.objects.create(
+        user=other_user,
+        is_global=False,
+        categories=[other_user_thread.category_id],
+    )
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": other_user_thread.id,
+                "slug": other_user_thread.slug,
+            },
+        ),
+        headers={"hx-request": "true"},
+    )
+    assert_contains(response, other_user_thread.title)
+
+
+def test_thread_detail_view_shows_user_global_moderator_thread_in_htmx(
+    user_client, other_user, other_user_thread
+):
+    Moderator.objects.create(user=other_user)
+
     response = user_client.get(
         reverse(
             "misago:thread",
@@ -337,6 +551,304 @@ def test_thread_detail_view_shows_global_moderator_deleted_user_thread_in_htmx(
         headers={"hx-request": "true"},
     )
     assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_error_404_to_anonymous_user_for_deleted_user_thread_in_show_started_only(
+    client, default_category, thread
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert response.status_code == 404
+
+
+def test_thread_detail_view_shows_error_404_to_anonymous_user_for_user_thread_in_show_started_only(
+    client, default_category, other_user_thread
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": other_user_thread.id,
+                "slug": other_user_thread.slug,
+            },
+        )
+    )
+    assert response.status_code == 404
+
+
+def test_thread_detail_view_shows_deleted_user_category_pinned_thread_to_anonymous_user_in_show_started_only(
+    thread_factory, client, default_category
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    thread = thread_factory(default_category, weight=1)
+
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_deleted_user_globally_pinned_thread_to_anonymous_user_in_show_started_only(
+    thread_factory, client, default_category
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    thread = thread_factory(default_category, weight=2)
+
+    response = client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_error_404_to_user_for_deleted_user_thread_in_show_started_only(
+    user_client, default_category, thread
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert response.status_code == 404
+
+
+def test_thread_detail_view_shows_error_404_to_user_for_other_user_thread_in_show_started_only(
+    user_client, default_category, other_user_thread
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": other_user_thread.id,
+                "slug": other_user_thread.slug,
+            },
+        )
+    )
+    assert response.status_code == 404
+
+
+def test_thread_detail_view_shows_user_own_thread_in_show_started_only(
+    user_client, default_category, user_thread
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": user_thread.id,
+                "slug": user_thread.slug,
+            },
+        )
+    )
+    assert_contains(response, user_thread.title)
+
+
+def test_thread_detail_view_shows_deleted_user_category_pinned_thread_to_user_in_show_started_only(
+    thread_factory, user_client, default_category
+):
+    thread = thread_factory(default_category, weight=1)
+
+    default_category.show_started_only = True
+    default_category.set_last_thread(thread)
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_deleted_user_globally_pinned_thread_to_user_in_show_started_only(
+    thread_factory, user_client, default_category
+):
+    thread = thread_factory(default_category, weight=2)
+
+    default_category.show_started_only = True
+    default_category.set_last_thread(thread)
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_other_user_category_pinned_thread_to_user_in_show_started_only(
+    thread_factory, user_client, other_user, default_category
+):
+    thread = thread_factory(default_category, starter=other_user, weight=1)
+
+    default_category.show_started_only = True
+    default_category.set_last_thread(thread)
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_other_user_globally_pinned_thread_to_user_in_show_started_only(
+    thread_factory, user_client, other_user, default_category
+):
+    thread = thread_factory(default_category, starter=other_user, weight=2)
+
+    default_category.show_started_only = True
+    default_category.set_last_thread(thread)
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_category_moderator_deleted_user_thread_in_show_started_only(
+    user_client, user, default_category, thread
+):
+    Moderator.objects.create(
+        user=user,
+        is_global=False,
+        categories=[thread.category_id],
+    )
+
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_global_moderator_deleted_user_thread_in_show_started_only(
+    moderator_client, default_category, thread
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = moderator_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+            },
+        )
+    )
+    assert_contains(response, thread.title)
+
+
+def test_thread_detail_view_shows_category_moderator_other_user_thread_in_show_started_only(
+    user_client, user, default_category, other_user_thread
+):
+    Moderator.objects.create(
+        user=user,
+        is_global=False,
+        categories=[other_user_thread.category_id],
+    )
+
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = user_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": other_user_thread.id,
+                "slug": other_user_thread.slug,
+            },
+        )
+    )
+    assert_contains(response, other_user_thread.title)
+
+
+def test_thread_detail_view_shows_global_moderator_other_user_thread_in_show_started_only(
+    moderator_client, default_category, other_user_thread
+):
+    default_category.show_started_only = True
+    default_category.save()
+
+    response = moderator_client.get(
+        reverse(
+            "misago:thread",
+            kwargs={
+                "thread_id": other_user_thread.id,
+                "slug": other_user_thread.slug,
+            },
+        )
+    )
+    assert_contains(response, other_user_thread.title)
 
 
 def test_thread_detail_view_returns_redirect_to_valid_url_if_slug_is_invalid(
