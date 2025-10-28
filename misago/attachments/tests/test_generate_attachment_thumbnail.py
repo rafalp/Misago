1076801b72cd697e739a9a0a127229c9c77fe4f3
raydeal <alnw@interia.eu>
2025-10-28 17:09:42 +0000

    Added image_format parameter to generate_attachment_thumbnail (#1976)

--- misago/attachments/tests/test_generate_attachment_thumbnail.py
+++ misago/attachments/tests/test_generate_attachment_thumbnail.py
@@ -7,7 +7,7 @@ def test_generate_attachment_thumbnail_generates_thumbnail_image_and_updates_att
     image_attachment, image_large, teardown_attachments
 ):
     image = Image.open(image_large)
-    generate_attachment_thumbnail(image_attachment, image, 400, 300)
+    generate_attachment_thumbnail(image_attachment, image, image.format, 400, 300)
 
     assert image_attachment.thumbnail
     assert image_attachment.thumbnail.url
