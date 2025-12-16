3cfbe63aa83c94403d9d8362c2c9799a24d39136
rafalp <kontakt@rpiton.com>
2025-12-16 18:19:12 +0100

    Private thread post likes view tests

--- misago/likes/tests/test_private_thread_post_likes_view.py
+++ misago/likes/tests/test_private_thread_post_likes_view.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 
+from ...permissions.enums import CanSeePostLikes
 from ...test import assert_contains, assert_not_contains
 from ..like import like_post
 
@@ -263,3 +264,150 @@ def test_private_thread_post_likes_view_displays_last_page_from_out_of_range_las
         headers={"hx-request": "true"},
     )
     assert_contains(response, "DeletedUser")
+
+
+def test_private_thread_post_likes_view_returns_error_403_if_user_has_no_private_threads_permission(
+    user_client, members_group, user_private_thread
+):
+    members_group.can_use_private_threads = False
+    members_group.save()
+
+    post = user_private_thread.first_post
+
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-likes",
+            kwargs={
+                "thread_id": user_private_thread.id,
+                "slug": user_private_thread.slug,
+                "post_id": post.id,
+            },
+        ),
+    )
+    assert_contains(response, "You can&#x27;t use private threads.", status_code=403)
+
+
+def test_private_thread_post_likes_view_returns_error_404_if_thread_doesnt_exist(
+    user_client,
+):
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-likes",
+            kwargs={"thread_id": 100, "slug": "not-found", "post_id": 1},
+        )
+    )
+    assert response.status_code == 404
+
+
+def test_private_thread_post_likes_view_returns_error_404_if_post_doesnt_exist_in_thread(
+    user_client, user_private_thread, other_user_private_thread
+):
+    post = other_user_private_thread.first_post
+
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-likes",
+            kwargs={
+                "thread_id": user_private_thread.id,
+                "slug": user_private_thread.slug,
+                "post_id": post.id,
+            },
+        )
+    )
+    assert response.status_code == 404
+
+
+def test_private_thread_post_likes_view_returns_error_404_if_user_has_no_private_thread_access(
+    user_client, private_thread
+):
+    post = private_thread.first_post
+
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-likes",
+            kwargs={
+                "thread_id": private_thread.id,
+                "slug": private_thread.slug,
+                "post_id": post.id,
+            },
+        ),
+    )
+    assert response.status_code == 404
+
+
+def test_private_thread_post_likes_view_returns_error_403_if_user_is_anonymous(
+    client, private_thread
+):
+    post = private_thread.first_post
+
+    response = client.get(
+        reverse(
+            "misago:private-thread-post-likes",
+            kwargs={
+                "thread_id": private_thread.id,
+                "slug": private_thread.slug,
+                "post_id": post.id,
+            },
+        )
+    )
+    assert_contains(
+        response, "You must be signed in to use private threads.", status_code=403
+    )
+
+
+def test_private_thread_post_likes_view_returns_error_403_if_user_cant_see_posts_likes(
+    user_client, members_group, other_user_private_thread
+):
+    members_group.can_see_others_posts_likes = CanSeePostLikes.NEVER
+    members_group.save()
+
+    post = other_user_private_thread.first_post
+
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-likes",
+            kwargs={
+                "thread_id": other_user_private_thread.id,
+                "slug": other_user_private_thread.slug,
+                "post_id": post.id,
+            },
+        ),
+    )
+    assert_contains(
+        response, "You can&#x27;t see this post&#x27;s likes.", status_code=403
+    )
+
+
+def test_private_thread_post_likes_view_returns_error_403_if_user_can_see_posts_likes_count_only(
+    user_client, members_group, other_user_private_thread
+):
+    members_group.can_see_others_posts_likes = CanSeePostLikes.COUNT
+    members_group.save()
+
+    post = other_user_private_thread.first_post
+
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-likes",
+            kwargs={
+                "thread_id": other_user_private_thread.id,
+                "slug": other_user_private_thread.slug,
+                "post_id": post.id,
+            },
+        ),
+    )
+    assert_contains(
+        response, "You can&#x27;t see this post&#x27;s likes.", status_code=403
+    )
+
+
+def test_private_thread_post_likes_view_returns_error_404_if_post_is_in_thread(
+    user_client, thread, post
+):
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-likes",
+            kwargs={"thread_id": thread.id, "slug": thread.slug, "post_id": post.id},
+        )
+    )
+    assert response.status_code == 404
