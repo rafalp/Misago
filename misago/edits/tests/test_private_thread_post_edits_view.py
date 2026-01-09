3b3f2846e2678f279162e3c7d7aeadc57e4d5c0b
rafalp <kontakt@rpiton.com>
2026-01-09 20:33:32 +0100

    Test edits view redirect to first edit

--- misago/edits/tests/test_private_thread_post_edits_view.py
+++ misago/edits/tests/test_private_thread_post_edits_view.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 
 from ...test import assert_contains
+from ..create import create_post_edit
 
 
 def test_private_thread_post_edits_view_shows_empty(
@@ -58,3 +59,94 @@ def test_private_thread_post_edits_view_shows_empty_in_modal(
         headers={"hx-request": "true"},
     )
     assert_contains(response, "This post has no edit history.")
+
+
+def test_private_thread_post_edits_view_redirects_to_first_edit(
+    user_client, other_user_private_thread, post
+):
+    post = other_user_private_thread.first_post
+    create_post_edit(post=post, user="Moderator")
+
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-edits",
+            kwargs={
+                "thread_id": other_user_private_thread.id,
+                "slug": other_user_private_thread.slug,
+                "post_id": post.id,
+            },
+        )
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:private-thread-post-edits",
+        kwargs={
+            "thread_id": other_user_private_thread.id,
+            "slug": other_user_private_thread.slug,
+            "post_id": post.id,
+            "page": 1,
+        },
+    )
+
+
+def test_private_thread_post_edits_view_redirects_to_first_edit_in_htmx(
+    user_client, other_user_private_thread, post
+):
+    post = other_user_private_thread.first_post
+    create_post_edit(post=post, user="Moderator")
+
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-edits",
+            kwargs={
+                "thread_id": other_user_private_thread.id,
+                "slug": other_user_private_thread.slug,
+                "post_id": post.id,
+            },
+        ),
+        headers={"hx-request": "true"},
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:private-thread-post-edits",
+        kwargs={
+            "thread_id": other_user_private_thread.id,
+            "slug": other_user_private_thread.slug,
+            "post_id": post.id,
+            "page": 1,
+        },
+    )
+
+
+def test_private_thread_post_edits_view_redirects_to_first_edit_in_modal(
+    user_client, other_user_private_thread, post
+):
+    post = other_user_private_thread.first_post
+    create_post_edit(post=post, user="Moderator")
+
+    response = user_client.get(
+        reverse(
+            "misago:private-thread-post-edits",
+            kwargs={
+                "thread_id": other_user_private_thread.id,
+                "slug": other_user_private_thread.slug,
+                "post_id": post.id,
+            },
+        )
+        + "?modal=true",
+        headers={"hx-request": "true"},
+    )
+    assert response.status_code == 302
+    assert (
+        response["location"]
+        == reverse(
+            "misago:private-thread-post-edits",
+            kwargs={
+                "thread_id": other_user_private_thread.id,
+                "slug": other_user_private_thread.slug,
+                "post_id": post.id,
+                "page": 1,
+            },
+        )
+        + "?modal=true"
+    )
--- misago/edits/tests/test_thread_post_edits_view.py
+++ misago/edits/tests/test_thread_post_edits_view.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 
 from ...test import assert_contains
+from ..create import create_post_edit
 
 
 def test_thread_post_edits_view_shows_empty(user_client, thread, post):
@@ -71,3 +72,77 @@ def test_thread_post_edits_view_shows_empty_to_anonymous_user_in_modal(
         headers={"hx-request": "true"},
     )
     assert_contains(response, "This post has no edit history.")
+
+
+def test_thread_post_edits_view_redirects_to_first_edit(user_client, thread, post):
+    create_post_edit(post=post, user="Moderator")
+
+    response = user_client.get(
+        reverse(
+            "misago:thread-post-edits",
+            kwargs={"thread_id": thread.id, "slug": thread.slug, "post_id": post.id},
+        )
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:thread-post-edits",
+        kwargs={
+            "thread_id": thread.id,
+            "slug": thread.slug,
+            "post_id": post.id,
+            "page": 1,
+        },
+    )
+
+
+def test_thread_post_edits_view_redirects_to_first_edit_in_htmx(
+    user_client, thread, post
+):
+    create_post_edit(post=post, user="Moderator")
+
+    response = user_client.get(
+        reverse(
+            "misago:thread-post-edits",
+            kwargs={"thread_id": thread.id, "slug": thread.slug, "post_id": post.id},
+        ),
+        headers={"hx-request": "true"},
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:thread-post-edits",
+        kwargs={
+            "thread_id": thread.id,
+            "slug": thread.slug,
+            "post_id": post.id,
+            "page": 1,
+        },
+    )
+
+
+def test_thread_post_edits_view_redirects_to_first_edit_in_modal(
+    user_client, thread, post
+):
+    create_post_edit(post=post, user="Moderator")
+
+    response = user_client.get(
+        reverse(
+            "misago:thread-post-edits",
+            kwargs={"thread_id": thread.id, "slug": thread.slug, "post_id": post.id},
+        )
+        + "?modal=true",
+        headers={"hx-request": "true"},
+    )
+    assert response.status_code == 302
+    assert (
+        response["location"]
+        == reverse(
+            "misago:thread-post-edits",
+            kwargs={
+                "thread_id": thread.id,
+                "slug": thread.slug,
+                "post_id": post.id,
+                "page": 1,
+            },
+        )
+        + "?modal=true"
+    )
