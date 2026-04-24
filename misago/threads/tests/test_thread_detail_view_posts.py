3a7136cffe03e2cbfdaa99c4c9ff1af062e97836
rafalp <kontakt@rpiton.com>
2026-04-24 19:21:40 +0200

    Add tests for locked post status bar

--- misago/threads/tests/test_thread_detail_view_posts.py
+++ misago/threads/tests/test_thread_detail_view_posts.py
@@ -260,6 +260,91 @@ def test_thread_detail_view_shows_user_post_to_moderator(
     assert_contains(response, post.original)
 
 
+def test_thread_detail_view_doesnt_show_deleted_user_post_locked_status_bar_to_anonymous_user(
+    thread_reply_factory, client, thread
+):
+    post = thread_reply_factory(thread, original=get_random_string(12), is_locked=True)
+    response = client.get(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug})
+    )
+    assert_contains(response, post.get_absolute_url())
+    assert_not_contains(response, "This post is only editable by moderators.")
+
+
+def test_thread_detail_view_doesnt_show_user_post_locked_status_bar_to_anonymous_user(
+    thread_reply_factory, client, user, thread
+):
+    post = thread_reply_factory(
+        thread, poster=user, original=get_random_string(12), is_locked=True
+    )
+    response = client.get(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug})
+    )
+    assert_contains(response, post.get_absolute_url())
+    assert_not_contains(response, "This post is only editable by moderators.")
+
+
+def test_thread_detail_view_doesnt_show_deleted_user_post_locked_status_bar_to_user(
+    thread_reply_factory, user_client, thread
+):
+    post = thread_reply_factory(thread, original=get_random_string(12), is_locked=True)
+    response = user_client.get(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug})
+    )
+    assert_contains(response, post.get_absolute_url())
+    assert_not_contains(response, "This post is only editable by moderators.")
+
+
+def test_thread_detail_view_doesnt_show_other_user_post_locked_status_bar_to_user(
+    thread_reply_factory, user_client, other_user, thread
+):
+    post = thread_reply_factory(
+        thread, poster=other_user, original=get_random_string(12), is_locked=True
+    )
+    response = user_client.get(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug})
+    )
+    assert_contains(response, post.get_absolute_url())
+    assert_not_contains(response, "This post is only editable by moderators.")
+
+
+def test_thread_detail_view_shows_user_post_locked_status_bar_to_user(
+    thread_reply_factory, user_client, user, thread
+):
+    post = thread_reply_factory(
+        thread, poster=user, original=get_random_string(12), is_locked=True
+    )
+    response = user_client.get(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug})
+    )
+    assert_contains(response, post.get_absolute_url())
+    assert_contains(response, "This post is only editable by moderators.")
+
+
+def test_thread_detail_view_shows_deleted_user_post_locked_status_bar_to_moderator(
+    thread_reply_factory, moderator_client, thread
+):
+    post = thread_reply_factory(thread, original=get_random_string(12), is_locked=True)
+    response = moderator_client.get(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug})
+    )
+    assert_contains(response, post.get_absolute_url())
+    assert_contains(response, "This post is only editable by moderators.")
+
+
+def test_thread_detail_view_shows_user_post_locked_status_bar_to_moderator(
+    thread_reply_factory, moderator_client, user, thread
+):
+    post = thread_reply_factory(
+        thread, poster=user, original=get_random_string(12), is_locked=True
+    )
+    response = moderator_client.get(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug})
+    )
+    assert_contains(response, post.get_absolute_url())
+    assert_contains(response, "This post is only editable by moderators.")
+
+
 def test_thread_detail_view_doesnt_show_unapproved_post_to_anonymous_user(
     thread_reply_factory, client, thread
 ):
