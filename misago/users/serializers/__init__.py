0f44616acd45a1cf5e19ade747539f425ae86b85
rafalp <kontakt@rpiton.com>
2026-06-01 23:08:54 +0200

    Fix circular import

--- misago/users/serializers/__init__.py
+++ misago/users/serializers/__init__.py
@@ -1,7 +1,8 @@
+# noqa
 from .ban import *
 from .moderation import *
 from .rank import *
 from .user import *
+from .auth import *
 from .username import ChangeUsernameSerializer
 from .usernamechange import *
-from .auth import *  # noqa
