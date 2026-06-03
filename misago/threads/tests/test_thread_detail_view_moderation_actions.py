9a76f090359b2c8bc2a7f28597035708e7c7f1f2
rafalp <kontakt@rpiton.com>
2026-06-03 21:36:01 +0200

    Test pin/unpin thread moderation actions

---
+++ misago/threads/tests/test_thread_detail_view_moderation_actions.py
@@ -0,0 +1,99 @@
+from django.urls import reverse
+
+from ...threadupdates.enums import ThreadUpdateActionName
+from ...threadupdates.models import ThreadUpdate
+from ..enums import ThreadPinned
+
+
+def test_thread_detail_view_executes_pin_everywhere_thread_moderation_action(
+    moderator_client, thread
+):
+    response = moderator_client.post(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}),
+        {"thread_moderation": "pin_everywhere"},
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}
+    )
+
+    thread.refresh_from_db()
+    assert thread.pinned == ThreadPinned.EVERYWHERE
+    assert thread.has_updates
+
+    ThreadUpdate.objects.get(
+        thread=thread,
+        action=ThreadUpdateActionName.PINNED_EVERYWHERE,
+    )
+
+
+def test_thread_detail_view_executes_pin_category_thread_moderation_action(
+    moderator_client, thread
+):
+    response = moderator_client.post(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}),
+        {"thread_moderation": "pin_category"},
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}
+    )
+
+    thread.refresh_from_db()
+    assert thread.pinned == ThreadPinned.CATEGORY
+    assert thread.has_updates
+
+    ThreadUpdate.objects.get(
+        thread=thread,
+        action=ThreadUpdateActionName.PINNED_CATEGORY,
+    )
+
+
+def test_thread_detail_view_executes_unpin_pinned_everywhere_thread_moderation_action(
+    moderator_client, thread
+):
+    thread.pinned = ThreadPinned.EVERYWHERE
+    thread.save()
+
+    response = moderator_client.post(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}),
+        {"thread_moderation": "unpin"},
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}
+    )
+
+    thread.refresh_from_db()
+    assert thread.pinned == ThreadPinned.NONE
+    assert thread.has_updates
+
+    ThreadUpdate.objects.get(
+        thread=thread,
+        action=ThreadUpdateActionName.UNPINNED,
+    )
+
+
+def test_thread_detail_view_executes_unpin_pinned_category_thread_moderation_action(
+    moderator_client, thread
+):
+    thread.pinned = ThreadPinned.CATEGORY
+    thread.save()
+
+    response = moderator_client.post(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}),
+        {"thread_moderation": "unpin"},
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}
+    )
+
+    thread.refresh_from_db()
+    assert thread.pinned == ThreadPinned.NONE
+    assert thread.has_updates
+
+    ThreadUpdate.objects.get(
+        thread=thread,
+        action=ThreadUpdateActionName.UNPINNED,
+    )
