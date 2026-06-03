ff393c352fcfec159f44366d1ce7dc1b09860df8
Rafał Pitoń <rafio.xudb@gmail.com>
2026-06-03 22:46:01 +0200

    Lock/unlock thread moderation actions (#2094)

--- misago/threads/tests/test_thread_detail_view_moderation_actions.py
+++ misago/threads/tests/test_thread_detail_view_moderation_actions.py
@@ -97,3 +97,50 @@ def test_thread_detail_view_executes_unpin_pinned_category_thread_moderation_act
         thread=thread,
         action=ThreadUpdateActionName.UNPINNED,
     )
+
+
+def test_thread_detail_view_executes_lock_thread_moderation_action(
+    moderator_client, thread
+):
+    response = moderator_client.post(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}),
+        {"thread_moderation": "lock"},
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}
+    )
+
+    thread.refresh_from_db()
+    assert thread.is_locked
+    assert thread.has_updates
+
+    ThreadUpdate.objects.get(
+        thread=thread,
+        action=ThreadUpdateActionName.LOCKED,
+    )
+
+
+def test_thread_detail_view_executes_unlock_thread_moderation_action(
+    moderator_client, thread
+):
+    thread.is_locked = True
+    thread.save()
+
+    response = moderator_client.post(
+        reverse("misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}),
+        {"thread_moderation": "unlock"},
+    )
+    assert response.status_code == 302
+    assert response["location"] == reverse(
+        "misago:thread", kwargs={"thread_id": thread.id, "slug": thread.slug}
+    )
+
+    thread.refresh_from_db()
+    assert not thread.is_locked
+    assert thread.has_updates
+
+    ThreadUpdate.objects.get(
+        thread=thread,
+        action=ThreadUpdateActionName.UNLOCKED,
+    )
