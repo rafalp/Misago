d5a2d304892cf092c2b83c6262cedac758ba1f1f
raydeal <alnw@interia.eu>
2026-06-18 16:56:23 +0100

    Added No option to CanSeePostLikes

--- misago/permissions/rules.py
+++ misago/permissions/rules.py
@@ -1,6 +1,10 @@
 from typing import Any, Iterable
 
+<<<<<<< HEAD
 from .enums import CanHideOwnPostEdits, CanSeePostEdits, PermissionValue
+=======
+from .enums import CanSeePostEdits, CanSeePostLikes, PermissionValue
+>>>>>>> ce60412c3 (Added No option to CanSeePostLikes)
 
 
 def yes_no_never(permissions: Iterable[PermissionValue]) -> bool:
@@ -26,4 +30,9 @@ def can_see_post_edits(permissions: Iterable[CanSeePostEdits]) -> int:
 def can_hide_own_post_edits(permissions: Iterable[CanHideOwnPostEdits]) -> int:
     if CanHideOwnPostEdits.NEVER in permissions:
         return CanHideOwnPostEdits.NO
+
+
+def can_see_post_likes(permissions: Iterable[CanSeePostLikes]) -> int:
+    if CanSeePostLikes.NEVER in permissions:
+        return CanSeePostLikes.NO
     return max(permissions)
