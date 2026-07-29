c8eb353afe3b3afc8c5118ffd0720c66e7b8f735
Rafał Pitoń <rafio.xudb@gmail.com>
2026-07-29 19:01:49 +0200

    Finalize thread header design (#2181)

--- misago/formats/templatetags/misago_formats.py
+++ misago/formats/templatetags/misago_formats.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 from ..daterelative import (
     date_relative,
@@ -11,3 +12,16 @@ register = template.Library()
 register.filter(is_safe=False, expects_localtime=True)(date_relative)
 register.filter(is_safe=False, expects_localtime=True)(date_relative_in_sentence)
 register.filter(is_safe=False, expects_localtime=True)(date_relative_short)
+
+
+@register.simple_tag
+def strformat(value, **kwargs):
+    try:
+        return value % kwargs
+    except (TypeError, ValueError, KeyError):
+        return value
+
+
+@register.simple_tag
+def safestrformat(value, **kwargs):
+    return mark_safe(strformat(value, **kwargs))
--- misago/metadata/__init__.py
+++ misago/metadata/__init__.py
@@ -1,11 +1,60 @@
 from dataclasses import dataclass
+from datetime import datetime
+from typing import TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from ..users.models import User
 
 
 @dataclass
-class TextMetaData:
+class TextMetadata:
     type = "text"
 
     id: str
     text: str
     icon: str | None = None
-    title: str | None = None
+    url: str | None = None
+    aria_label: str | None = None
+
+
+@dataclass
+class DatetimeMetadata:
+    type = "datetime"
+
+    id: str
+    text: str
+    datetime: datetime
+    is_sentence: bool = False
+    url: str | None = None
+    icon: str | None = None
+    aria_label: str | None = None
+
+
+@dataclass
+class NumberMetadata:
+    type = "number"
+
+    id: str
+    text: str
+    number: int
+    icon: str | None = None
+    aria_label: str | None = None
+
+
+@dataclass
+class UserDatetimeMetadata:
+    type = "user_datetime"
+
+    id: str
+    user: Union["User", str]
+    datetime: datetime
+    url: str | None = None
+    aria_label: str | None = None
+
+    @property
+    def is_anonymous_user(self) -> bool:
+        return isinstance(self.user, str)
+
+    @property
+    def is_registered_user(self) -> bool:
+        return not isinstance(self.user, str)
